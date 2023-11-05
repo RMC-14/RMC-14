82449885145b5666226c7d9aea30795216ebc9bc
DrSmugleaf <drsmugleaf@gmail.com>
2023-11-04 17:07:04 -0700

    Add checkbox to show/hide non-cm decals in the menu

---
+++ Content.Shared/.CM14/Decals/DecalPrototype.cs
@@ -0,0 +1,20 @@
+﻿using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
+
+// ReSharper disable CheckNamespace
+namespace Content.Shared.Decals;
+// ReSharper restore CheckNamespace
+
+public sealed partial class DecalPrototype : IInheritingPrototype
+{
+    [ParentDataField(typeof(AbstractPrototypeIdArraySerializer<DepartmentPrototype>))]
+    public string[]? Parents { get; }
+
+    [NeverPushInheritance]
+    [AbstractDataField]
+    public bool Abstract { get; }
+
+    [DataField]
+    public bool IsCM;
+}
