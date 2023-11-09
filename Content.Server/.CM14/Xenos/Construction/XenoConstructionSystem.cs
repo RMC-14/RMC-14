9c9d7e6c21c112ac9da1faf67b79deb14c48bf4e
DrSmugleaf <drsmugleaf@gmail.com>
2023-11-09 10:26:24 -0800

    Clarification

--- Content.Server/.CM14/Xenos/Construction/XenoConstructionSystem.cs
+++ Content.Server/.CM14/Xenos/Construction/XenoConstructionSystem.cs
@@ -24,8 +24,8 @@ public sealed class XenoConstructionSystem : SharedXenoConstructionSystem
         // TODO CM14
         // There is an edge case right now where existing weeds can block new weeds
         // from expanding further. If this is the case then the weeds should reassign
-        // their source to this one if it is closer to them than their original source
-        // and only if it is still within range
+        // their source to this one and reactivate if it is closer to them than their
+        // original source and only if it is still within range
         if (args.NeighborFreeTiles.Count <= 0 ||
             !Exists(source) ||
             !TryComp(source, out TransformComponent? transform) ||
