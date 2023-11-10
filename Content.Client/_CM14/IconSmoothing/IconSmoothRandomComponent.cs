637b4a2957f72aa318d023c697418d583424c4cf
DrSmugleaf <drsmugleaf@gmail.com>
2023-11-10 00:33:40 -0800

    Add smoothing for weeds

---
+++ Content.Client/_CM14/IconSmoothing/IconSmoothRandomComponent.cs
@@ -0,0 +1,13 @@
+﻿using Content.Shared.Sprite;
+
+namespace Content.Client._CM14.IconSmoothing;
+
+[RegisterComponent]
+public sealed partial class IconSmoothRandomComponent : Component
+{
+    /// <summary>
+    ///     Which states to override with the ones from <see cref="RandomSpriteComponent"/>
+    /// </summary>
+    [DataField]
+    public HashSet<string> Overrides = new();
+}
