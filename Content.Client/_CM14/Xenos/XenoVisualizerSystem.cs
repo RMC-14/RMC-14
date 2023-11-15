268c7aded7b06b7e1cc08158c6b742170ab63aa2
DrSmugleaf <drsmugleaf@gmail.com>
2023-11-15 02:23:35 -0800

    Add facehugger and running animation

--- Content.Client/_CM14/Xenos/XenoVisualizerSystem.cs
+++ Content.Client/_CM14/Xenos/XenoVisualizerSystem.cs
@@ -1,8 +1,11 @@
 ﻿using Content.Shared._CM14.Xenos;
+using Content.Shared._CM14.Xenos.Movement;
 using Content.Shared._CM14.Xenos.Rest;
 using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
+using Content.Shared.Movement.Components;
+using Content.Shared.Movement.Systems;
 using Robust.Client.GameObjects;
 using DrawDepth = Content.Shared.DrawDepth.DrawDepth;
 using XenoComponent = Content.Shared._CM14.Xenos.XenoComponent;
@@ -70,6 +73,58 @@ public sealed class XenoVisualizerSystem : VisualizerSystem<XenoComponent>
         UpdateDrawDepth((uid, sprite));
     }
 
+    private void UpdateSprite(Entity<SpriteComponent?, MobStateComponent?, AppearanceComponent?, InputMoverComponent> entity)
+    {
+        var (_, sprite, mobState, appearance, input) = entity;
+        if (!Resolve(entity, ref sprite, ref appearance))
+            return;
+
+        var state = MobState.Alive;
+        if (Resolve(entity, ref mobState, false))
+        {
+            state = mobState.CurrentState;
+        }
+
+        Resolve(entity, ref input, false);
+
+        if (sprite is not { BaseRSI: { } rsi } ||
+            !sprite.LayerMapTryGet(XenoVisualLayers.Base, out var layer))
+        {
+            return;
+        }
+
+        switch (state)
+        {
+            case MobState.Critical:
+                if (rsi.TryGetState("crit", out _))
+                    sprite.LayerSetState(layer, "crit");
+                break;
+            case MobState.Dead:
+                if (rsi.TryGetState("dead", out _))
+                    sprite.LayerSetState(layer, "dead");
+                break;
+            default:
+                if (AppearanceSystem.TryGetData(entity, XenoVisualLayers.Base, out XenoRestState resting, appearance) &&
+                    resting == XenoRestState.Resting)
+                {
+                    if (rsi.TryGetState("sleeping", out _))
+                        sprite.LayerSetState(layer, "sleeping");
+                    break;
+                }
+
+                if (input?.HeldMoveButtons > MoveButtons.None &&
+                    rsi.TryGetState("running", out _))
+                {
+                    sprite.LayerSetState(layer, "running");
+                    break;
+                }
+
+                if (rsi.TryGetState("alive", out _))
+                    sprite.LayerSetState(layer, "alive");
+                break;
+        }
+    }
+
     public void UpdateDrawDepth(Entity<SpriteComponent?> xeno)
     {
         if (!Resolve(xeno, ref xeno.Comp))
@@ -79,5 +134,17 @@ public sealed class XenoVisualizerSystem : VisualizerSystem<XenoComponent>
         RaiseLocalEvent(xeno, ref ev);
 
         xeno.Comp.DrawDepth = (int) ev.DrawDepth;
+    }
+
+    public override void FrameUpdate(float frameTime)
+    {
+        var query = EntityQueryEnumerator<XenoAnimateMovementComponent, InputMoverComponent, MobStateComponent, SpriteComponent>();
+        while (query.MoveNext(out var uid, out _, out var input, out var mobState, out var sprite))
+        {
+            if (mobState.CurrentState == MobState.Alive)
+            {
+                UpdateSprite((uid, sprite, mobState, null, input));
+            }
+        }
     }
 }
