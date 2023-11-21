5deb8fb6b27c038af45cac3b96b36b6983584a25
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2023-11-21 01:22:37 -0800

    Add xeno armor, defender toggle crest defense ability (#611)

---
+++ Content.Shared/_CM14/Xenos/Armor/XenoArmorComponent.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Xenos.Armor;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(XenoArmorSystem))]
+public sealed partial class XenoArmorComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    [ViewVariables(VVAccess.ReadWrite)]
+    public int Armor;
+}
