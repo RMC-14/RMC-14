3d9b7fd1a7a3aab7b35b4455c7e1cd31cdca06b3
SandSurfer <andyn5068@gmail.com>
2023-11-25 19:53:35 -0800

    Tips... but this time in the CM14 Folder

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -464,7 +464,7 @@ namespace Content.Shared.CCVar
         ///     The dataset prototype to use when selecting a random tip.
         /// </summary>
         public static readonly CVarDef<string> TipsDataset =
-            CVarDef.Create("tips.dataset", "Tips");
+            CVarDef.Create("tips.dataset", "CM_Tips");
 
         /// <summary>
         ///     The number of seconds between each tip being displayed when the round is not actively going
