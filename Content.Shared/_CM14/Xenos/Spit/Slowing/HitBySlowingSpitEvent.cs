9610f8e1d154eec580ecdca0199b403f17536b2a
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2023-11-26 09:21:00 -0800

    Add slowing spit and scattered spit xeno abilities (#679)

---
+++ Content.Shared/_CM14/Xenos/Spit/Slowing/HitBySlowingSpitEvent.cs
@@ -0,0 +1,6 @@
+﻿using Content.Shared.Inventory;
+
+namespace Content.Shared._CM14.Xenos.Spit.Slowing;
+
+[ByRefEvent]
+public record struct HitBySlowingSpitEvent(SlotFlags TargetSlots, bool Cancelled = false) : IInventoryRelayEvent;
