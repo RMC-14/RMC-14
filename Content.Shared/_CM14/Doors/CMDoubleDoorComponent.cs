489a9944f0ae8504ab136ec40898505fe267cb3e
DrSmugleaf <drsmugleaf@gmail.com>
2023-12-27 01:55:12 -0800

    Add double doors and script to split double door .rsis

---
+++ Content.Shared/_CM14/Doors/CMDoubleDoorComponent.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._CM14.Doors;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class CMDoubleDoorComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public TimeSpan LastOpeningAt;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan LastClosingAt;
+}
