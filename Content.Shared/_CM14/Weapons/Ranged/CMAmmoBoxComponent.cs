bab1fbdc89ca92ea95c41943eb506b03dde74ac0
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2023-12-30 00:54:40 -0800

    Add buckshot, slugs, incendiary, beanbag, flechette and incendiary buckshot (#771)

---
+++ Content.Shared/_CM14/Weapons/Ranged/CMAmmoBoxComponent.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._CM14.Weapons.Ranged;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CMAmmoBoxComponent : Component
+{
+    [DataField]
+    public Enum AmmoLayer = CMAmmoBoxLayers.Ammo;
+}
+
+[Serializable, NetSerializable]
+public enum CMAmmoBoxLayers
+{
+    Ammo
+}
