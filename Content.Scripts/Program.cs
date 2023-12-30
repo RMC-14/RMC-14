f67a1466d6236b6a752ae921540afa2ebaf144c3
DrSmugleaf <drsmugleaf@gmail.com>
2023-12-29 20:47:11 -0800

    Add M37A2 shotgun

---
+++ Content.Scripts/Program.cs
@@ -0,0 +1,27 @@
+﻿using Content.Scripts;
+
+string program;
+if (args.Length == 0)
+{
+    Console.WriteLine("Which program to run? [doorsplitter, metafixer]");
+    program = Console.ReadLine() ?? string.Empty;
+}
+else
+{
+    program = args[1];
+}
+
+program = program.ToLower().Trim();
+
+if ("doorsplitter".Contains(program))
+{
+    DoorSplitter.Run();
+}
+else if ("metafixer".Contains(program))
+{
+    MetaFixer.Run();
+}
+else
+{
+    Console.WriteLine("No valid argument given, exiting. Valid arguments: doorsplitter, metafixer");
+}
