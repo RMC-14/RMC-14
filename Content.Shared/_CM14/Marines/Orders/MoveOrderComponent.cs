8ad5c2551b2972d54029629e682547d90e182b13
nikthechampiongr <32041239+nikthechampiongr@users.noreply.github.com>
2024-01-03 09:03:14 +0200

    Marine Orders (#767)

---
+++ Content.Shared/_CM14/Marines/Orders/MoveOrderComponent.cs
@@ -0,0 +1,32 @@
+using Content.Shared.FixedPoint;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Utility;
+using static Robust.Shared.Utility.SpriteSpecifier;
+
+namespace Content.Shared._CM14.Marines.Orders;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class MoveOrderComponent : Component, IOrderComponent
+{
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier Icon = new Rsi(new ResPath("/Textures/_CM14/Interface/marine_orders.rsi"), "move");
+
+    [DataField, AutoNetworkedField]
+    public FixedPoint2 MoveSpeedModifier = 0.1;
+
+    // CM14 TODO Actually make this do something once we got melee dodging
+    [DataField, AutoNetworkedField]
+    public FixedPoint2 DodgeModifier = 0.1;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField]
+    public TimeSpan Duration { get; set; }
+
+    public void AssignMultiplier(FixedPoint2 multiplier)
+    {
+        MoveSpeedModifier *= multiplier;
+        DodgeModifier *= multiplier;
+    }
+
+    public override bool SessionSpecific => true;
+}
