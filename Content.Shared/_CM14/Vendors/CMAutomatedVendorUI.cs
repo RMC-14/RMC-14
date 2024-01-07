c3e32990517fe28a0ac5a01e9511ea07e87a7f24
DrSmugleaf <drsmugleaf@gmail.com>
2024-01-07 03:20:45 -0800

    Add new UI for ColMarTech automated vendors

---
+++ Content.Shared/_CM14/Vendors/CMAutomatedVendorUI.cs
@@ -0,0 +1,27 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._CM14.Vendors;
+
+[Serializable, NetSerializable]
+public enum CMAutomatedVendorUI : byte
+{
+    Key
+}
+
+[Serializable, NetSerializable]
+public sealed class CMVendorVendBuiMessage : BoundUserInterfaceMessage
+{
+    public readonly int Section;
+    public readonly int Entry;
+
+    public CMVendorVendBuiMessage(int section, int entry)
+    {
+        Section = section;
+        Entry = entry;
+    }
+}
+
+[Serializable, NetSerializable]
+public sealed class CMVendorRefreshBuiMessage : BoundUserInterfaceMessage
+{
+}
