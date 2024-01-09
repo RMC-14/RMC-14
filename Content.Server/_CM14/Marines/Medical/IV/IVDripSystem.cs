66a6f8c04242c8dd0f6aedd9acab893fa99a0a62
gus <august.eymann@gmail.com>
2024-01-09 07:46:58 -0500

    Porting of medical (#595)
    Co-authored-by: Whisper <121047731+QuietlyWhisper@users.noreply.github.com>
    Co-authored-by: QuietlyWhisper <chrisracer.cloud@gmail.com>
    Co-authored-by: DrSmugleaf <drsmugleaf@gmail.com>

---
+++ Content.Server/_CM14/Marines/Medical/IV/IVDripSystem.cs
@@ -0,0 +1,61 @@
+using Content.Server.Body.Components;
+using Content.Shared._CM14.Marines.Medical.IV;
+using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Containers.ItemSlots;
+using Robust.Shared.Timing;
+
+namespace Content.Server._CM14.Marines.Medical.IV;
+
+public sealed class IVDripSystem : SharedIVDripSystem
+{
+    [Dependency] private readonly ItemSlotsSystem _itemSlots = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly SolutionContainerSystem _solutionContainer = default!;
+
+    public override void Update(float frameTime)
+    {
+        var time = _timing.CurTime;
+        var query = EntityQueryEnumerator<IVDripComponent>();
+        while (query.MoveNext(out var ivId, out var ivComp))
+        {
+            if (ivComp.AttachedTo == default)
+                continue;
+
+            if (!InRange((ivId, ivComp), ivComp.AttachedTo))
+                Detach((ivId, ivComp), true, false);
+
+            if (_itemSlots.GetItemOrNull(ivId, ivComp.Slot) is not { } pack)
+                continue;
+
+            if (!TryComp(pack, out BloodPackComponent? packComponent))
+                continue;
+
+            if (!_solutionContainer.TryGetSolution(pack, packComponent.Solution, out var packSolution))
+                continue;
+
+            if (!TryComp(ivComp.AttachedTo, out BloodstreamComponent? targetBloodstream))
+                continue;
+
+            if (time < ivComp.TransferAt)
+                continue;
+
+            if (ivComp.Injecting)
+            {
+                if (targetBloodstream.BloodSolution.Volume < targetBloodstream.BloodSolution.MaxVolume)
+                {
+                    _solutionContainer.TryTransferSolution(pack, ivComp.AttachedTo, packSolution, targetBloodstream.BloodSolution, ivComp.TransferAmount);
+                }
+            }
+            else
+            {
+                if (packSolution.Volume < packSolution.MaxVolume)
+                {
+                    _solutionContainer.TryTransferSolution(ivComp.AttachedTo, pack, targetBloodstream.BloodSolution, packSolution, ivComp.TransferAmount);
+                }
+            }
+
+            ivComp.TransferAt = time + ivComp.TransferDelay;
+            Dirty(ivId, ivComp);
+        }
+    }
+}
