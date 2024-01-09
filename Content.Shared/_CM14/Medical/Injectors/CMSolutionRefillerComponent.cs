70f8cab0e6aff70f7bc2e1a8615eb1435340e8da
DrSmugleaf <drsmugleaf@gmail.com>
2024-01-09 05:23:02 -0800

    Add refilling auto injectors with the Wey-Med Plus

---
+++ Content.Shared/_CM14/Medical/Injectors/CMSolutionRefillerComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Medical.Injectors;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(CMRefillableSolutionSystem))]
+public sealed partial class CMSolutionRefillerComponent : Component
+{
+}
