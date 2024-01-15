8553918cccd437f39b7dde2cb7a71d19f728bb14
DrSmugleaf <drsmugleaf@gmail.com>
2024-01-15 11:19:59 -0800

    Add points system to automated vendors

--- Content.Server/_CM14/Vendors/CMAutomatedVendorSystem.cs
+++ Content.Server/_CM14/Vendors/CMAutomatedVendorSystem.cs
@@ -1,7 +1,17 @@
 ﻿using Content.Shared._CM14.Vendors;
+using Robust.Server.GameObjects;
 
 namespace Content.Server._CM14.Vendors;
 
 public sealed class CMAutomatedVendorSystem : SharedCMAutomatedVendorSystem
 {
+    [Dependency] private readonly UserInterfaceSystem _ui = default!;
+
+    protected override void OnVendBui(Entity<CMAutomatedVendorComponent> vendor, ref CMVendorVendBuiMessage args)
+    {
+        base.OnVendBui(vendor, ref args);
+
+        var msg = new CMVendorRefreshBuiMessage();
+        _ui.TrySendUiMessage(vendor, args.UiKey, msg, args.Session);
+    }
 }
