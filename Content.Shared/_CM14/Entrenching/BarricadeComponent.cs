596d535e31f20bdc25ac4fa0590c8b0657b417bf
thymol-blue <53154853+thymol-blue@users.noreply.github.com>
2024-01-18 00:15:25 -0800

    Add entrenching tool, filling sandbags and building sandbag barricades (#925)
    Co-authored-by: DrSmugleaf <drsmugleaf@gmail.com>

---
+++ Content.Shared/_CM14/Entrenching/BarricadeComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Entrenching;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(BarricadeComponent))]
+public sealed partial class BarricadeComponent : Component
+{
+}
---
+++ Content.Shared/_CM14/Entrenching/EntrenchingToolComponent.cs
@@ -0,0 +1,33 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Map;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._CM14.Entrenching;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+[Access(typeof(EntrenchingToolSystem))]
+public sealed partial class EntrenchingToolComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public TimeSpan DigDelay = TimeSpan.FromSeconds(5);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan FillDelay = TimeSpan.FromSeconds(1);
+
+    [DataField, AutoNetworkedField]
+    public int LayersPerDig = 5;
+
+    [DataField, AutoNetworkedField]
+    public int TotalLayers;
+
+    [AutoNetworkedField]
+    public EntityCoordinates LastDigLocation;
+}
+
+[Serializable, NetSerializable]
+public enum EntrenchingToolComponentVisualLayers
+{
+    Base,
+    Folded,
+    Dirt
+}
