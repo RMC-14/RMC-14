72c4731fb06922f21ddfc8c040e8985a94a53fcb
DrSmugleaf <drsmugleaf@gmail.com>
2024-01-29 13:01:39 -0800

    Add requisitions

---
+++ Content.Shared/_CM14/Requisitions/RequisitionsEntry.cs
@@ -0,0 +1,21 @@
+﻿using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._CM14.Requisitions;
+
+[DataDefinition]
+[Serializable, NetSerializable]
+public sealed partial class RequisitionsEntry
+{
+    [DataField(required: true)]
+    public string Name = string.Empty;
+
+    [DataField(required: true)]
+    public int Cost;
+
+    [DataField(required: true)]
+    public EntProtoId Crate;
+
+    [DataField(required: true)]
+    public List<EntProtoId> Entities = new();
+}
