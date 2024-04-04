4e90b502eac776dd0813c4c93d5b48998623900e
DrSmugleaf <drsmugleaf@gmail.com>
2024-04-03 17:23:25 -0700

    Dirty on expand

--- Content.Shared/_CM14/Storage/CMStorageSystem.cs
+++ Content.Shared/_CM14/Storage/CMStorageSystem.cs
@@ -21,6 +21,7 @@ public sealed class CMStorageSystem : EntitySystem
         {
             Log.Warning($"Storage {ToPrettyString(storage)} can't fit {ToPrettyString(args.Item)}");
 
+            var modified = false;
             foreach (var shape in _item.GetItemShape((args.Item, args.Item)))
             {
                 var grid = args.Storage.Grid;
@@ -37,7 +38,11 @@ public sealed class CMStorageSystem : EntitySystem
                     expanded.Top = shape.Top;
 
                 grid[^1] = expanded;
+                modified = true;
             }
+
+            if (modified)
+                Dirty(storage);
         }
     }
 }
