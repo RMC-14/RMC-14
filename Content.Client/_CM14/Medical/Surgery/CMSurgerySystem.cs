10be21c701ab6a1c263d000756e188ec78a0e636
DrSmugleaf <drsmugleaf@gmail.com>
2024-04-05 21:55:14 -0700

    Add cm operating table

--- Content.Client/_CM14/Medical/Surgery/CMSurgerySystem.cs
+++ Content.Client/_CM14/Medical/Surgery/CMSurgerySystem.cs
@@ -1,7 +1,4 @@
 ﻿using Content.Shared._CM14.Medical.Surgery;
-using Content.Shared.Hands;
-using Content.Shared.Hands.Components;
-using Content.Shared.Standing;
 
 namespace Content.Client._CM14.Medical.Surgery;
 
@@ -9,17 +6,7 @@ public sealed class CMSurgerySystem : SharedCMSurgerySystem
 {
     public event Action? OnRefresh;
 
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<HandsComponent, DidEquipHandEvent>(RefreshUI);
-        SubscribeLocalEvent<HandsComponent, DidUnequipHandEvent>(RefreshUI);
-        SubscribeLocalEvent<CMSurgeryTargetComponent, DownedEvent>(RefreshUI);
-        SubscribeLocalEvent<CMSurgeryTargetComponent, StoodEvent>(RefreshUI);
-    }
-
-    private void RefreshUI<TComp, TEvent>(Entity<TComp> hands, ref TEvent args) where TComp : IComponent?
+    public override void Update(float frameTime)
     {
         OnRefresh?.Invoke();
     }
