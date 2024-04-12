f8132d56652302a4094ba57c348256e67519da63
nikthechampiongr <nikvog4@gmail.com>
2024-04-13 00:40:02 +0300

    Correctly apply modifiers. Standardize how modifiers are applied for marine orders.

--- Content.Shared/_CM14/Marines/Orders/MoveOrderComponent.cs
+++ Content.Shared/_CM14/Marines/Orders/MoveOrderComponent.cs
@@ -13,11 +13,11 @@ public sealed partial class MoveOrderComponent : Component, IOrderComponent
     public SpriteSpecifier Icon = new Rsi(new ResPath("/Textures/_CM14/Interface/marine_orders.rsi"), "move");
 
     [DataField, AutoNetworkedField]
-    public FixedPoint2 MoveSpeedModifier = 0.1;
+    public FixedPoint2 MoveSpeedModifier = 1.1;
 
     // CM14 TODO Actually make this do something once we got melee dodging
     [DataField, AutoNetworkedField]
-    public FixedPoint2 DodgeModifier = 0.1;
+    public FixedPoint2 DodgeModifier = 1.1;
 
     [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField]
     public TimeSpan Duration { get; set; }
