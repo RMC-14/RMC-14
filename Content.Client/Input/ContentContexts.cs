7089e151d8488a0638d9dd21f700f11f57e80d6c
Whisper <121047731+QuietlyWhisper@users.noreply.github.com>
2024-04-16 06:45:59 -0400

    Update ContentContexts.cs

--- Content.Client/Input/ContentContexts.cs
+++ Content.Client/Input/ContentContexts.cs
@@ -120,6 +120,7 @@ namespace Content.Client.Input
             common.AddFunction(ContentKeyFunctions.OpenAdminMenu);
             common.AddFunction(ContentKeyFunctions.OpenGuidebook);
 
+
             CMFunctions(contexts);
         }
 
