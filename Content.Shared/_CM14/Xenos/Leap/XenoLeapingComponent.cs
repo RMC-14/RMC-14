56576429b8d66ebd9293ea183fe0ac16ee637c67
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-04-19 22:01:30 -0700

    Fix inconsistent range for runner and hugger leaps (#1257)

---
+++ Content.Shared/_CM14/Xenos/Leap/XenoLeapingComponent.cs
@@ -0,0 +1,18 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared._CM14.Xenos.Leap;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class XenoLeapingComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public TimeSpan KnockdownTime;
+
+    [DataField, AutoNetworkedField]
+    public SoundSpecifier? HitSound;
+
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField]
+    public TimeSpan LeapEndTime;
+}
