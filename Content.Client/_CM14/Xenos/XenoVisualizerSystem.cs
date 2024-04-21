654501118554d78c1ccbbccc4c5680f85fdd7b92
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-04-20 21:30:56 -0700

    Make the ravager and queen immune to catching fire, add resisting fire to xenos (#1291)

--- Content.Client/_CM14/Xenos/XenoVisualizerSystem.cs
+++ Content.Client/_CM14/Xenos/XenoVisualizerSystem.cs
@@ -8,6 +8,8 @@ using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Systems;
+using Content.Shared.StatusEffect;
+using Content.Shared.Stunnable;
 using Content.Shared.Throwing;
 using Robust.Client.GameObjects;
 using DrawDepth = Content.Shared.DrawDepth.DrawDepth;
@@ -18,11 +20,27 @@ public sealed class XenoVisualizerSystem : VisualizerSystem<XenoComponent>
 {
     [Dependency] private readonly MobStateSystem _mobState = default!;
 
+    private EntityQuery<XenoAnimateMovementComponent> _animateQuery;
+
     public override void Initialize()
     {
         base.Initialize();
 
+        SubscribeLocalEvent<XenoComponent, KnockedDownEvent>(OnXenoKnockedDown);
+        SubscribeLocalEvent<XenoComponent, StatusEffectEndedEvent>(OnXenoStatusEffectEnded);
         SubscribeLocalEvent<XenoComponent, GetDrawDepthEvent>(OnXenoGetDrawDepth);
+
+        _animateQuery = GetEntityQuery<XenoAnimateMovementComponent>();
+    }
+
+    private void OnXenoKnockedDown(Entity<XenoComponent> xeno, ref KnockedDownEvent args)
+    {
+        UpdateSprite(xeno.Owner);
+    }
+
+    private void OnXenoStatusEffectEnded(Entity<XenoComponent> xeno, ref StatusEffectEndedEvent args)
+    {
+        UpdateSprite(xeno.Owner);
     }
 
     private void OnXenoGetDrawDepth(Entity<XenoComponent> ent, ref GetDrawDepthEvent args)
@@ -43,9 +61,9 @@ public sealed class XenoVisualizerSystem : VisualizerSystem<XenoComponent>
         UpdateDrawDepth((uid, sprite));
     }
 
-    public void UpdateSprite(Entity<SpriteComponent?, MobStateComponent?, AppearanceComponent?, InputMoverComponent?, ThrownItemComponent?, XenoLeapingComponent?> entity)
+    public void UpdateSprite(Entity<SpriteComponent?, MobStateComponent?, AppearanceComponent?, InputMoverComponent?, ThrownItemComponent?, XenoLeapingComponent?, KnockedDownComponent?> entity)
     {
-        var (_, sprite, mobState, appearance, input, thrown, leaping) = entity;
+        var (_, sprite, mobState, appearance, input, thrown, leaping, knocked) = entity;
         if (!Resolve(entity, ref sprite, ref appearance))
             return;
 
@@ -53,7 +71,9 @@ public sealed class XenoVisualizerSystem : VisualizerSystem<XenoComponent>
         if (Resolve(entity, ref mobState, false))
             state = mobState.CurrentState;
 
-        Resolve(entity, ref input, ref thrown, ref leaping, false);
+        Resolve(entity, ref input, ref thrown, ref leaping, ref knocked, false);
+        if (knocked != null)
+            state = MobState.Critical;
 
         if (sprite is not { BaseRSI: { } rsi } ||
             !sprite.LayerMapTryGet(XenoVisualLayers.Base, out var layer))
@@ -145,21 +165,24 @@ public sealed class XenoVisualizerSystem : VisualizerSystem<XenoComponent>
         xeno.Comp.DrawDepth = (int) ev.DrawDepth;
     }
 
+    public override void Update(float frameTime)
+    {
+        var xenoQuery = EntityQueryEnumerator<XenoComponent>();
+        while (xenoQuery.MoveNext(out var uid, out _))
+        {
+            if (_animateQuery.HasComp(uid))
+                continue;
+
+            UpdateSprite(uid);
+        }
+    }
+
     public override void FrameUpdate(float frameTime)
     {
-        var animateQuery = EntityQueryEnumerator<XenoAnimateMovementComponent, InputMoverComponent, MobStateComponent, SpriteComponent>();
-        while (animateQuery.MoveNext(out var uid, out _, out var input, out var mobState, out var sprite))
+        var animateQuery = EntityQueryEnumerator<XenoAnimateMovementComponent>();
+        while (animateQuery.MoveNext(out var uid, out _))
         {
-            if (mobState.CurrentState == MobState.Alive)
-            {
-                UpdateSprite((uid, sprite, mobState, null, input));
-            }
-        }
-
-        var oviQuery = EntityQueryEnumerator<XenoOvipositorCapableComponent, SpriteComponent, AppearanceComponent>();
-        while (oviQuery.MoveNext(out var uid, out _, out var sprite, out var appearance))
-        {
-            UpdateSprite((uid, sprite, null, appearance));
+            UpdateSprite(uid);
         }
     }
 }
