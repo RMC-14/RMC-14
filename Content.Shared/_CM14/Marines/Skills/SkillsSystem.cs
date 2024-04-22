d9d0e88e47533f18fbc6f9ae48e1f133778588ab
DrSmugleaf <drsmugleaf@gmail.com>
2024-04-21 21:17:25 -0700

    Make medically-unskilled marines have a 3 second do after when using auto-injectors

---
+++ Content.Shared/_CM14/Marines/Skills/SkillsSystem.cs
@@ -0,0 +1,18 @@
+﻿namespace Content.Shared._CM14.Marines.Skills;
+
+public sealed class SkillsSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MedicallyUnskilledDoAfterComponent, AttemptHyposprayUseEvent>(OnAttemptHyposprayUse);
+    }
+
+    private void OnAttemptHyposprayUse(Entity<MedicallyUnskilledDoAfterComponent> ent, ref AttemptHyposprayUseEvent args)
+    {
+        if (!TryComp(args.User, out SkillsComponent? skills) ||
+            skills.Medical < ent.Comp.Min)
+        {
+            args.MaxDoAfter(ent.Comp.DoAfter);
+        }
+    }
+}
