fb76b7d4289fffd02c9179f11df8e326cc4666c3
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-04-23 01:48:31 -0700

    Update gen_build_info.py

--- Tools/gen_build_info.py
+++ Tools/gen_build_info.py
@@ -20,10 +20,10 @@ SERVER_FILES = [
 ]
 
 VERSION = os.environ['GITHUB_SHA']
-FORK_ID = "wizards"
-BUILD_URL = f"https://cdn.centcomm.spacestation14.com/builds/wizards/builds/{{FORK_VERSION}}/{FILE}"
-MANIFEST_URL = f"https://cdn.centcomm.spacestation14.com/cdn/version/{{FORK_VERSION}}/manifest"
-MANIFEST_DOWNLOAD_URL = f"https://cdn.centcomm.spacestation14.com/cdn/version/{{FORK_VERSION}}/download"
+FORK_ID = "cm14"
+BUILD_URL = f"https://cdn.xenomorph.gay/builds/cm14/builds/{{FORK_VERSION}}/{FILE}"
+MANIFEST_URL = f"https://cdn.xenomorph.gay/cdn/version/{{FORK_VERSION}}/manifest"
+MANIFEST_DOWNLOAD_URL = f"https://cdn.xenomorph.gay/cdn/version/{{FORK_VERSION}}/download"
 
 def main() -> None:
     client_file = os.path.join("release", FILE)
