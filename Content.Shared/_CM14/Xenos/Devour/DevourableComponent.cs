943fc118ee4fb4679fb25ee1f3a18d5f4f305512
DrSmugleaf <drsmugleaf@gmail.com>
2024-04-24 20:37:01 -0700

    Fix not being able to devour tackled marines

---
+++ Content.Shared/_CM14/Xenos/Devour/DevourableComponent.cs
@@ -0,0 +1,7 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Xenos.Devour;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(XenoDevourSystem))]
+public sealed partial class DevourableComponent : Component;
--- Content.Shared/_CM14/Xenos/Devour/XenoDevourSystem.cs
+++ Content.Shared/_CM14/Xenos/Devour/XenoDevourSystem.cs
@@ -1,8 +1,10 @@
-﻿using Content.Shared._CM14.Marines;
+﻿using System.Diagnostics.CodeAnalysis;
+using Content.Shared._CM14.Marines;
 using Content.Shared.DoAfter;
 using Content.Shared.DragDrop;
 using Content.Shared.Mobs.Systems;
 using Content.Shared.Popups;
+using Content.Shared.Standing;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
 
@@ -15,6 +17,7 @@ public sealed class XenoDevourSystem : EntitySystem
     [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
     [Dependency] private readonly MobStateSystem _mobState = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly StandingStateSystem _standing = default!;
 
     public override void Initialize()
     {
@@ -23,47 +26,58 @@ public sealed class XenoDevourSystem : EntitySystem
         SubscribeLocalEvent<XenoDevourComponent, CanDropTargetEvent>(OnXenoCanDropTarget);
         SubscribeLocalEvent<MarineComponent, CanDropDraggedEvent>(OnMarineCanDropDragged);
         SubscribeLocalEvent<MarineComponent, DragDropDraggedEvent>(OnMarineDragDropDragged);
+        SubscribeLocalEvent<XenoDevourComponent, DoAfterAttemptEvent<XenoDevourDoAfterEvent>>(OnXenoDevourDoAfterAttempt);
         SubscribeLocalEvent<XenoDevourComponent, XenoDevourDoAfterEvent>(OnXenoDevourDoAfter);
         SubscribeLocalEvent<XenoDevourComponent, XenoRegurgitateActionEvent>(OnXenoRegurgitateAction);
     }
 
     private void OnXenoCanDropTarget(Entity<XenoDevourComponent> xeno, ref CanDropTargetEvent args)
     {
-        args.CanDrop |= args.User == xeno.Owner &&
-                        HasComp<MarineComponent>(args.Dragged);
+        if (CanDevour(xeno, args.Dragged, out _))
+            args.CanDrop = true;
 
         args.Handled = true;
     }
 
     private void OnMarineCanDropDragged(Entity<MarineComponent> marine, ref CanDropDraggedEvent args)
     {
-        args.CanDrop |= args.Target == args.User &&
-                        HasComp<XenoComponent>(args.User) &&
-                        HasComp<MarineComponent>(args.Target) &&
-                        !_mobState.IsDead(args.Target);
-
-        if (args.CanDrop)
+        if (CanDevour(args.User, marine, out _))
+        {
+            args.CanDrop = true;
             args.Handled = true;
+        }
     }
 
     private void OnMarineDragDropDragged(Entity<MarineComponent> marine, ref DragDropDraggedEvent args)
     {
-        if (args.Handled || args.Target != args.User)
+        if (args.Target != args.User)
             return;
 
-        if (!TryComp(args.User, out XenoDevourComponent? xeno))
+        if (!CanDevour(args.User, marine, out var devour))
         {
             _popup.PopupClient(Loc.GetString("cm-xeno-cant-devour", ("target", args.Target)), marine, marine);
             return;
         }
 
-        StartDevour((args.User, xeno), marine, xeno.DevourDelay);
+        StartDevour((args.User, devour), marine, devour.DevourDelay);
         args.Handled = true;
+    }
+
+    private void OnXenoDevourDoAfterAttempt(Entity<XenoDevourComponent> ent, ref DoAfterAttemptEvent<XenoDevourDoAfterEvent> args)
+    {
+        if (args.DoAfter.Args.Target is not { } target ||
+            !CanDevour(ent, target, out _))
+        {
+            args.Cancel();
+        }
     }
 
     private void OnXenoDevourDoAfter(Entity<XenoDevourComponent> xeno, ref XenoDevourDoAfterEvent args)
     {
         if (args.Handled || args.Cancelled || args.Target is not { } target)
+            return;
+
+        if (!CanDevour(xeno, target, out _))
             return;
 
         args.Handled = true;
@@ -89,11 +103,33 @@ public sealed class XenoDevourSystem : EntitySystem
         _audio.PlayPredicted(xeno.Comp.RegurgitateSound, xeno, xeno);
     }
 
+    private bool CanDevour(EntityUid xeno, EntityUid victim, [NotNullWhen(true)] out XenoDevourComponent? devour)
+    {
+        devour = default;
+        if (xeno == victim)
+            return false;
+
+        if (HasComp<XenoComponent>(victim))
+            return false;
+
+        if (_mobState.IsDead(victim) || !_standing.IsDown(victim))
+            return false;
+
+        if (_mobState.IsIncapacitated(xeno))
+            return false;
+
+        if (!HasComp<DevourableComponent>(victim))
+            return false;
+
+        return TryComp(xeno, out devour);
+    }
+
     private void StartDevour(Entity<XenoDevourComponent> xeno, Entity<MarineComponent> target, TimeSpan delay)
     {
         var doAfter = new DoAfterArgs(EntityManager, xeno, delay, new XenoDevourDoAfterEvent(), xeno, target)
         {
-            BreakOnMove = true
+            BreakOnMove = true,
+            AttemptFrequency = AttemptFrequency.EveryTick
         };
 
         _doAfter.TryStartDoAfter(doAfter);
