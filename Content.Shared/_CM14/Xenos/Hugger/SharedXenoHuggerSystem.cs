adaba7f89e8be1fced45d97690c3fde4a2ec08ec
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-04-27 17:56:19 -0700

    Remove chestbursters from dead marines when they try to burst (#2036)

--- Content.Shared/_CM14/Xenos/Hugger/SharedXenoHuggerSystem.cs
+++ Content.Shared/_CM14/Xenos/Hugger/SharedXenoHuggerSystem.cs
@@ -273,6 +273,12 @@ public abstract class SharedXenoHuggerSystem : EntitySystem
                 continue;
             }
 
+            if (_mobState.IsDead(uid))
+            {
+                RemCompDeferred<VictimHuggedComponent>(uid);
+                return;
+            }
+
             RemCompDeferred<VictimHuggedComponent>(uid);
             Spawn(hugged.BurstSpawn, xform.Coordinates);
             EnsureComp<VictimBurstComponent>(uid);
