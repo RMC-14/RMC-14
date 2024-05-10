398739ab64e814432fc877ed64f64d636a7eeac1
DrSmugleaf <drsmugleaf@gmail.com>
2024-05-09 20:01:33 -0700

    Make xenos other than larva require a live Queen to evolve Fixes https://github.com/CM-14/CM-14/issues/1176

--- Content.Shared/_CM14/Xenos/Evolution/XenoEvolutionComponent.cs
+++ Content.Shared/_CM14/Xenos/Evolution/XenoEvolutionComponent.cs
@@ -8,8 +8,13 @@ namespace Content.Shared._CM14.Xenos.Evolution;
 public sealed partial class XenoEvolutionComponent : Component
 {
     [DataField, AutoNetworkedField]
+    public bool RequiresGranter = true;
+
+    [DataField, AutoNetworkedField]
     public List<EntProtoId> EvolvesTo = new();
 
     [DataField, AutoNetworkedField]
     public TimeSpan EvolutionDelay = TimeSpan.FromSeconds(3);
+
+    public readonly List<EntityUid> EvolutionActions = new();
 }
