7fe25b686c9d228b066d47bd8d88d4f842150e9f
DrSmugleaf <drsmugleaf@gmail.com>
2024-05-10 23:58:05 -0700

    Fix showing "no id" description on all entities

---
+++ Content.Shared/_CM14/Examine/ShortExamineComponent.cs
@@ -0,0 +1,7 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Examine;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(CMExamineSystem))]
+public sealed partial class ShortExamineComponent : Component;
