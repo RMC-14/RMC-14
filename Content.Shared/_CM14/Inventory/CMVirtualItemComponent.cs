494be170d3a68c53cdc506192a9bf96adadd9e45
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-05-13 18:45:54 -0700

    Make xenos able to click themselves to devour a pulled marine, regurgitate on death (#2140)

---
+++ Content.Shared/_CM14/Inventory/CMVirtualItemComponent.cs
@@ -0,0 +1,6 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Inventory;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CMVirtualItemComponent : Component;
