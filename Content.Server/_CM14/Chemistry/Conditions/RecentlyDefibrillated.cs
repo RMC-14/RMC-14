aa94979b3069b20eb0869c9aff60b03f046f92db
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-05-21 17:40:24 -0700

    Fill out wey-med plus, port effects for existing medicines (#2163)

---
+++ Content.Server/_CM14/Chemistry/Conditions/RecentlyDefibrillated.cs
@@ -0,0 +1,18 @@
+﻿using Content.Shared._CM14.Medical.Defibrillator;
+using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._CM14.Chemistry.Conditions;
+
+public sealed partial class RecentlyDefibrillated : ReagentEffectCondition
+{
+    public override bool Condition(ReagentEffectArgs args)
+    {
+        return args.EntityManager.HasComponent<CMRecentlyDefibrillatedComponent>(args.SolutionEntity);
+    }
+
+    public override string GuidebookExplanation(IPrototypeManager prototype)
+    {
+        return "on defibrillation";
+    }
+}
---
+++ Content.Shared/Medical/TargetDefibrillatedEvent.cs
@@ -0,0 +1,4 @@
+﻿namespace Content.Shared.Medical;
+
+[ByRefEvent]
+public readonly record struct TargetDefibrillatedEvent(EntityUid User, Entity<DefibrillatorComponent> Defibrillator);
