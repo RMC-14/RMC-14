7ed0a9fd837d83b4d359d161757b0b3af3e020d5
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-05-21 17:40:24 -0700

    Fill out wey-med plus, port effects for existing medicines (#2163)

--- Content.Shared/_CM14/Medical/Injectors/CMRefillableSolutionSystem.cs
+++ Content.Shared/_CM14/Medical/Injectors/CMRefillableSolutionSystem.cs
@@ -1,10 +1,12 @@
 ﻿using Content.Shared.Chemistry.EntitySystems;
 using Content.Shared.Interaction;
+using Content.Shared.Popups;
 
 namespace Content.Shared._CM14.Medical.Injectors;
 
 public sealed class CMRefillableSolutionSystem : EntitySystem
 {
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly SharedSolutionContainerSystem _solution = default!;
 
     public override void Initialize()
@@ -16,17 +18,22 @@ public sealed class CMRefillableSolutionSystem : EntitySystem
 
     private void OnRefillerInteractUsing(Entity<CMSolutionRefillerComponent> ent, ref InteractUsingEvent args)
     {
+        args.Handled = true;
         if (!TryComp(args.Used, out CMRefillableSolutionComponent? refillable))
+        {
+            _popup.PopupClient($"The {Name(ent)} cannot refill the {Name(args.Used)}.", args.User, args.User, PopupType.SmallCaution);
             return;
+        }
 
         if (!_solution.TryGetSolution(args.Used, refillable.Solution, out var solution))
             return;
+
 
         foreach (var (reagent, amount) in refillable.Reagents)
         {
             _solution.TryAddReagent(solution.Value, reagent, amount);
         }
 
-        args.Handled = true;
+        _popup.PopupClient($"The {Name(ent)} makes a whirring noise as it refills your {Name(args.Used)}.", args.User, args.User);
     }
 }
