5e01396440d3efcffb0da9535ba6f246c87e10ce
DrSmugleaf <drsmugleaf@gmail.com>
2024-05-23 22:29:48 -0700

    Make the wey-med and wey-chem have an auto refilling tank of reagents Make it possible to filter which reagents are refilled

--- Content.Shared/_CM14/Medical/Injectors/CMSolutionRefillerComponent.cs
+++
@@ -1,7 +0,0 @@
-﻿using Robust.Shared.GameStates;
-
-namespace Content.Shared._CM14.Medical.Injectors;
-
-[RegisterComponent, NetworkedComponent]
-[Access(typeof(CMRefillableSolutionSystem))]
-public sealed partial class CMSolutionRefillerComponent : Component;
