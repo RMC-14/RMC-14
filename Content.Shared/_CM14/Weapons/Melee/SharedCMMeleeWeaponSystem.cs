83d63db250c9386cec8f8b1c6a29a47738589b53
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-03 00:21:42 -0700

    Make xeno weeds able to be attacked with melee weapons Includes the changes in https://github.com/space-wizards/space-station-14/pull/28548 Fixes https://github.com/CM-14/CM-14/issues/2208

---
+++ Content.Shared/_CM14/Weapons/Melee/SharedCMMeleeWeaponSystem.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.Interaction.Events;
+
+namespace Content.Shared._CM14.Weapons.Melee;
+
+public abstract class SharedCMMeleeWeaponSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ImmuneToUnarmedComponent, GettingAttackedAttemptEvent>(OnImmuneToUnarmedGettingAttacked);
+    }
+
+    private void OnImmuneToUnarmedGettingAttacked(Entity<ImmuneToUnarmedComponent> ent, ref GettingAttackedAttemptEvent args)
+    {
+        if (args.Attacker == args.Weapon)
+            args.Cancelled = true;
+    }
+}
