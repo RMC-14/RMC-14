c18b649038cf183efca1c2c49429e3c0f6ff217f
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-03 21:05:36 -0700

    Make the M42A sniper ignore friendly fire Fixes https://github.com/CM-14/CM-14/issues/2232

---
+++ Content.Shared/_CM14/Weapons/Ranged/IFF/IFFFactionComponent.cs
@@ -0,0 +1,7 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Weapons.Ranged.IFF;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(GunIFFSystem))]
+public sealed partial class IFFFactionComponent : Component;
