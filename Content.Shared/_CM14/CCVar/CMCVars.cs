9bea52d4c1fcb56ddf007e053d69e363c020a1de
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-04 15:32:09 -0700

    Add cvars for xeno damage, resistance and speed

---
+++ Content.Shared/_CM14/CCVar/CMCVars.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Shared._CM14.CCVar;
+
+[CVarDefs]
+public sealed class CMCVars : CVars
+{
+    public static readonly CVarDef<float> CMXenoDamageDealtMultiplier =
+        CVarDef.Create("cm.xeno_damage_dealt_multiplier", 1f, CVar.REPLICATED | CVar.SERVER);
+
+    public static readonly CVarDef<float> CMXenoDamageReceivedMultiplier =
+        CVarDef.Create("cm.xeno_damage_received_multiplier", 1f, CVar.REPLICATED | CVar.SERVER);
+
+    public static readonly CVarDef<float> CMXenoSpeedMultiplier =
+        CVarDef.Create("cm.xeno_speed_multiplier", 1f, CVar.REPLICATED | CVar.SERVER);
+}
