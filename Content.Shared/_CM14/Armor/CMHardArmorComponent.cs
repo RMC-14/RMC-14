40392e0d3271d11538a36d86dda2b68897f48ba7
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-06 04:16:21 -0700

    Fix armor not preventing spit stun and surgery

---
+++ Content.Shared/_CM14/Armor/CMHardArmorComponent.cs
@@ -0,0 +1,7 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Armor;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(CMArmorSystem))]
+public sealed partial class CMHardArmorComponent : Component;
