f31d5b928ea3069b4859963936c63c623c2acaf9
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-06 22:22:33 -0700

    Implement grenades, effects, sounds, voicelines

--- Content.Shared/_CM14/CCVar/CMCVars.cs
+++ Content.Shared/_CM14/CCVar/CMCVars.cs
@@ -14,4 +14,7 @@ public sealed class CMCVars : CVars
 
     public static readonly CVarDef<float> CMXenoSpeedMultiplier =
         CVarDef.Create("cm.xeno_speed_multiplier", 1f, CVar.REPLICATED | CVar.SERVER);
+
+    public static readonly CVarDef<bool> CMPlayHumanoidVoicelines =
+        CVarDef.Create("cm.play_humanoid_voicelines", true, CVar.REPLICATED | CVar.CLIENT);
 }
