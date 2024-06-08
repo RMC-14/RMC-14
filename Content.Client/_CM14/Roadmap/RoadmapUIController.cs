6492936580c2cd6cc0af7292c2a7574aa7e704bc
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-08 03:12:05 -0700

    Fix roadmap showing while the rules popup is open

--- Content.Client/_CM14/Roadmap/RoadmapUIController.cs
+++ Content.Client/_CM14/Roadmap/RoadmapUIController.cs
@@ -1,19 +1,37 @@
 ﻿using Content.Client.Lobby;
+using Content.Client.UserInterface.Systems.Info;
 using Robust.Client.UserInterface.Controllers;
 
 namespace Content.Client._CM14.Roadmap;
 
 public sealed class RoadmapUIController : UIController, IOnStateEntered<LobbyState>
 {
+    [Dependency] private readonly InfoUIController _infoUIController = default!;
+
     private RoadmapWindow? _window;
     private bool _shown;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        _infoUIController.Accepted += OnAccepted;
+    }
 
     public void OnStateEntered(LobbyState state)
     {
         if (_shown || _window != null)
             return;
 
+        if (_infoUIController.RulesPopup != null)
+            return;
+
         ToggleRoadmap();
+    }
+
+    private void OnAccepted()
+    {
+        if (!_shown)
+            ToggleRoadmap();
     }
 
     public void ToggleRoadmap()
