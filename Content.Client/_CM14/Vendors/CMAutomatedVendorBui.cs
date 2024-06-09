f43bee8e691579b8c0ee4dcf4f249666c2d2c86f
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-09 02:19:58 -0700

    Fix hovered color for mandatory/recommended vendor entries

--- Content.Client/_CM14/Vendors/CMAutomatedVendorBui.cs
+++ Content.Client/_CM14/Vendors/CMAutomatedVendorBui.cs
@@ -58,26 +58,33 @@ public sealed class CMAutomatedVendorBui : BoundUserInterface
                         var name = entity.Name;
                         var color = CMAutomatedVendorPanel.DefaultColor;
                         var borderColor = CMAutomatedVendorPanel.DefaultBorderColor;
+                        var hoverColor = CMAutomatedVendorPanel.DefaultBorderColor;
                         if (section.TakeAll != null)
                         {
                             name = $"Mandatory: {name}";
                             color = Color.FromHex("#251A0C");
                             borderColor = Color.FromHex("#805300");
+                            hoverColor = Color.FromHex("#805300");
                         }
                         else if (entry.Recommended)
                         {
                             name = $"Recommended: {name}";
                             color = Color.FromHex("#102919");
                             borderColor = Color.FromHex("#3A9B52");
+                            hoverColor = Color.FromHex("#3A9B52");
                         }
 
                         uiEntry.Panel.Color = color;
                         uiEntry.Panel.BorderColor = borderColor;
+                        uiEntry.Panel.HoveredColor = hoverColor;
 
                         var msg = new FormattedMessage();
                         msg.AddText(name);
                         msg.PushNewline();
-                        msg.AddText(entity.Description);
+
+                        if (!string.IsNullOrWhiteSpace(entity.Description))
+                            msg.AddText(entity.Description);
+
                         var tooltip = new Tooltip();
                         tooltip.SetMessage(msg);
 
