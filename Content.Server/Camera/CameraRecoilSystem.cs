ff06f14be7c5275906b15c4e79c41ef672113201
geraeumig <alfenos@proton.me>
2024-06-13 20:11:00 +0200

    Revert upstream changes

--- Content.Server/Camera/CameraRecoilSystem.cs
+++ Content.Server/Camera/CameraRecoilSystem.cs
@@ -1,29 +1,16 @@
 using System.Numerics;
 using Content.Shared.Camera;
+using Robust.Shared.Player;
 
 namespace Content.Server.Camera;
 
 public sealed class CameraRecoilSystem : SharedCameraRecoilSystem
 {
-    [Dependency] private readonly SharedEyeSystem _eye = default!;
-
     public override void KickCamera(EntityUid euid, Vector2 kickback, CameraRecoilComponent? component = null)
     {
         if (!Resolve(euid, ref component, false))
             return;
 
         RaiseNetworkEvent(new CameraKickEvent(GetNetEntity(euid), kickback), euid);
-    }
-
-    public override void Update(float frameTime)
-    {
-        base.Update(frameTime);
-
-        var query = AllEntityQuery<EyeComponent, CameraRecoilComponent>();
-        while (query.MoveNext(out var uid, out var eye, out var recoil))
-        {
-            // apparently this is required for aim to work properly
-            _eye.SetOffset(uid, recoil.BaseOffset, eye);
-        }
     }
 }
--- Content.Shared/Camera/CameraRecoilComponent.cs
+++ Content.Shared/Camera/CameraRecoilComponent.cs
@@ -5,7 +5,6 @@ namespace Content.Shared.Camera;
 
 [RegisterComponent]
 [NetworkedComponent]
-[AutoGenerateComponentState]
 public sealed partial class CameraRecoilComponent : Component
 {
     public Vector2 CurrentKick { get; set; }
@@ -14,6 +13,5 @@ public sealed partial class CameraRecoilComponent : Component
     /// <summary>
     ///     Basically I needed a way to chain this effect for the attack lunge animation. Sorry!
     /// </summary>
-    [ViewVariables, AutoNetworkedField]
     public Vector2 BaseOffset { get; set; }
 }
