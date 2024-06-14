04b0ee6b2b0ee51c9746e92223d362aa6c9938c3
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-13 19:05:05 -0700

    Increase default gun projectile speed from 25 to 41 Fixes https://github.com/CM-14/CM-14/issues/2452

--- Content.Shared/Weapons/Ranged/Components/GunComponent.cs
+++ Content.Shared/Weapons/Ranged/Components/GunComponent.cs
@@ -187,7 +187,7 @@ public sealed partial class GunComponent : Component
     /// The base value for how fast the projectile moves.
     /// </summary>
     [DataField]
-    public float ProjectileSpeed = 25f;
+    public float ProjectileSpeed = 41f;
 
     /// <summary>
     /// How fast the projectile moves.
