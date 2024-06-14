069f0913fa715a19cac287a15c7f2c2dc105e6d2
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-14 01:05:58 -0700

    Make marines trained in firearms have no camera recoil when firing wielded guns

---
+++ Content.Shared/_CM14/Weapons/Ranged/GunSkilledRecoilComponent.cs
@@ -0,0 +1,18 @@
+﻿using Content.Shared._CM14.Marines.Skills;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._CM14.Weapons.Ranged;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SkillsSystem))]
+public sealed partial class GunSkilledRecoilComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public float SetRecoil;
+
+    [DataField(required: true), AutoNetworkedField]
+    public Skills Skills;
+
+    [DataField, AutoNetworkedField]
+    public bool MustBeWielded = true;
+}
