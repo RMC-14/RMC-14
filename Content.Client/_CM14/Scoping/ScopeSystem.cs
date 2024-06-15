a3e7866fbf81109de3e5fe168a48ca941318cd71
geraeumig <alfenos@proton.me>
2024-06-15 14:48:27 +0200

    Refactor to utilize SharedEyeSystem.SetTarget

--- Content.Client/_CM14/Scoping/ScopeSystem.cs
+++ Content.Client/_CM14/Scoping/ScopeSystem.cs
@@ -1,27 +1,13 @@
-﻿using System.Numerics;
-using Content.Client.Movement.Systems;
+﻿using Content.Client.Movement.Systems;
 using Content.Shared._CM14.Scoping;
-using Content.Shared.Camera;
+using Robust.Client.GameObjects;
 
 namespace Content.Client._CM14.Scoping;
 
 public sealed class ScopeSystem : SharedScopeSystem
 {
-    [Dependency] private readonly ContentEyeSystem _eye = default!;
-
-    public override void Initialize()
-    {
-        base.Initialize();
-        SubscribeNetworkEvent<CMScopeToggleEvent>(OnScopeToggle);
-    }
-
-    private void OnScopeToggle(CMScopeToggleEvent args)
-    {
-        if (TryComp(GetEntity(args.User), out CameraRecoilComponent? cameraRecoilComp))
-        {
-            cameraRecoilComp.BaseOffset = args.EyeOffset;
-        }
-    }
+    [Dependency] private readonly ContentEyeSystem _contentEye = default!;
+    [Dependency] private readonly EyeSystem _eye = default!;
 
     protected override void StartScopingCamera(EntityUid user, ScopeComponent scopeComponent)
     {
@@ -29,10 +15,7 @@ public sealed class ScopeSystem : SharedScopeSystem
 
     protected override void StopScopingCamera(EntityUid user, ScopeComponent scopeComponent)
     {
-        if (TryComp(user, out CameraRecoilComponent? cameraRecoilComponent))
-        {
-            _eye.ResetZoom(user);
-            cameraRecoilComponent.BaseOffset = Vector2.Zero;
-        }
+        _contentEye.ResetZoom(user);
+        _eye.SetTarget(user, null);
     }
 }
--- Content.Server/_CM14/Scoping/ScopeSystem.cs
+++ Content.Server/_CM14/Scoping/ScopeSystem.cs
@@ -1,7 +1,6 @@
 ﻿using System.Numerics;
 using Content.Server.Movement.Systems;
 using Content.Shared._CM14.Scoping;
-using Content.Shared.Camera;
 using Robust.Server.GameObjects;
 using Robust.Shared.Player;
 
@@ -17,39 +16,28 @@ public sealed class ScopeSystem : SharedScopeSystem
     protected override void StartScopingCamera(EntityUid user, ScopeComponent scopeComponent)
     {
         var xform = Transform(user);
-        if (!HasComp<CameraRecoilComponent>(user))
-            return;
 
         const float smallestViewpointSize = 15;
 
         var cardinalVector = xform.LocalRotation.GetCardinalDir().ToVec();
         var targetOffset = cardinalVector * ((smallestViewpointSize * scopeComponent.Zoom - 1) / 2);
 
-        _eye.SetOffset(user, targetOffset); // apparently this is required for aim to work properly
         _contentEye.SetZoom(user, Vector2.One * scopeComponent.Zoom, true);
-
-        var scopeToggleEvent = new CMScopeToggleEvent(GetNetEntity(user), targetOffset);
-        RaiseNetworkEvent(scopeToggleEvent, user);
 
         if (TryComp(user, out ActorComponent? actorComp))
         {
             // add cardinal vector, until better pvs handling is introduced here
-            var loaderId = Spawn(scopeComponent.PvsLoaderProto, _transformSystem.GetMapCoordinates(xform).Offset(targetOffset + cardinalVector * 2));
+            var loaderId = Spawn(scopeComponent.PvsLoaderProto, _transformSystem.GetMapCoordinates(xform).Offset(targetOffset));
             scopeComponent.PvsLoader = loaderId;
             _viewSubscriber.AddViewSubscriber(loaderId, actorComp.PlayerSession);
+            _eye.SetTarget(user, loaderId);
         }
     }
 
     protected override void StopScopingCamera(EntityUid user, ScopeComponent scopeComponent)
     {
-        if (!HasComp<CameraRecoilComponent>(user))
-            return;
-
-        _eye.SetOffset(user, Vector2.Zero);
         _contentEye.ResetZoom(user);
-
-        var scopeToggleEvent = new CMScopeToggleEvent(GetNetEntity(user), Vector2.Zero);
-        RaiseNetworkEvent(scopeToggleEvent, user);
+        _eye.SetTarget(user, null);
 
         Del(scopeComponent.PvsLoader);
         scopeComponent.PvsLoader = null;
