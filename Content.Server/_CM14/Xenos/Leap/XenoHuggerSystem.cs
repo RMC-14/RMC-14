31d6156ea545ae178335d015c7d7ad8a072612f3
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-16 01:12:56 -0700

    Make the hugger's observer spawn on top of where they were Fixes https://github.com/CM-14/CM-14/issues/2462

--- Content.Server/_CM14/Xenos/Leap/XenoHuggerSystem.cs
+++ Content.Server/_CM14/Xenos/Leap/XenoHuggerSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Server.GameTicking;
 using Content.Shared._CM14.Xenos.Hugger;
+using Content.Shared.Coordinates;
 using Robust.Shared.Player;
 
 namespace Content.Server._CM14.Xenos.Leap;
@@ -7,10 +8,19 @@ namespace Content.Server._CM14.Xenos.Leap;
 public sealed class XenoHuggerSystem : SharedXenoHuggerSystem
 {
     [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
 
     protected override void HuggerLeapHit(Entity<XenoHuggerComponent> hugger)
     {
-        if (TryComp(hugger, out ActorComponent? actor))
-            _gameTicker.SpawnObserver(actor.PlayerSession);
+        if (!TryComp(hugger, out ActorComponent? actor))
+            return;
+
+        var session = actor.PlayerSession;
+        _gameTicker.SpawnObserver(session);
+        if (session.AttachedEntity is not { } entity)
+            return;
+
+        _transform.SetCoordinates(entity, hugger.Owner.ToCoordinates());
+        _transform.AttachToGridOrMap(entity);
     }
 }
