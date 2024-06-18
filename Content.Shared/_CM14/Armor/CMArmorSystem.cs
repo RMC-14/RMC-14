f57815783c6089e1df089bb3a91e65d0256473a0
DrSmugleaf <drsmugleaf@gmail.com>
2024-06-18 14:40:42 -0700

    Fix the defender's armor applying to marines when attacking them

--- Content.Shared/_CM14/Armor/CMArmorSystem.cs
+++ Content.Shared/_CM14/Armor/CMArmorSystem.cs
@@ -40,7 +40,7 @@ public sealed class CMArmorSystem : EntitySystem
 
         SubscribeLocalEvent<CMArmorUserComponent, DamageModifyEvent>(OnUserDamageModify);
 
-        SubscribeLocalEvent<CMArmorPiercingComponent, CMGetArmorEvent>(OnPiercingGetArmor);
+        SubscribeLocalEvent<CMArmorPiercingComponent, CMGetArmorPiercingEvent>(OnPiercingGetArmor);
 
         SubscribeLocalEvent<InventoryComponent, CMGetArmorEvent>(_inventory.RelayEvent);
     }
@@ -105,9 +105,9 @@ public sealed class CMArmorSystem : EntitySystem
         ModifyDamage(ent, ref args);
     }
 
-    private void OnPiercingGetArmor(Entity<CMArmorPiercingComponent> piercing, ref CMGetArmorEvent args)
+    private void OnPiercingGetArmor(Entity<CMArmorPiercingComponent> piercing, ref CMGetArmorPiercingEvent args)
     {
-        args.Armor -= piercing.Comp.Amount;
+        args.Piercing += piercing.Comp.Amount;
     }
 
     private void ModifyDamage(EntityUid ent, ref DamageModifyEvent args)
@@ -117,7 +117,11 @@ public sealed class CMArmorSystem : EntitySystem
         RaiseLocalEvent(ent, ref ev);
 
         if (args.Tool != null)
-            RaiseLocalEvent(args.Tool.Value, ref ev);
+        {
+            var piercingEv = new CMGetArmorPiercingEvent();
+            RaiseLocalEvent(args.Tool.Value, ref piercingEv);
+            ev.Armor -= piercingEv.Piercing;
+        }
 
         var armor = Math.Max(ev.Armor, 0);
         if (args.Origin is { } origin)
