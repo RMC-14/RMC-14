696111598e142f4ee841b74cd38b4a63dbe657a0
Whisper <121047731+QuietlyWhisper@users.noreply.github.com>
2024-06-19 19:19:27 -0400

    Discard changes to Content.Client/StatusIcon/StatusIconSystem.cs

--- Content.Client/StatusIcon/StatusIconSystem.cs
+++ Content.Client/StatusIcon/StatusIconSystem.cs
@@ -71,6 +71,7 @@ public sealed class StatusIconSystem : SharedStatusIconSystem
     public bool IsVisible(Entity<MetaDataComponent> ent, StatusIconData data)
     {
         var viewer = _playerManager.LocalSession?.AttachedEntity;
+
         // Always show our icons to our entity
         if (viewer == ent.Owner)
             return true;
