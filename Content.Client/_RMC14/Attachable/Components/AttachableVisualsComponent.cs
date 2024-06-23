fcc2aec6346225879676023bbe8b459f9dd433e3
Sigil <84070966+Siigiil@users.noreply.github.com>
2024-06-23 23:01:26 +0700

    Fixed UBS offset, fixed collapsible stock visuals.

--- Content.Client/_RMC14/Attachable/Components/AttachableVisualsComponent.cs
+++ Content.Client/_RMC14/Attachable/Components/AttachableVisualsComponent.cs
@@ -18,8 +18,8 @@ public sealed partial class AttachableVisualsComponent : Component
     ///     Optional, only used if the item's own state should not be used.
     ///     This prefix is added to the name of the slot the attachable is installed in.
     ///     The prefix must be in kebab-case and end with a dash, like so: example-prefix-
-    ///     The RSI must contain a state for every slot the attachable fits into, named in
-    ///     this pattern: prefix-slot-name
+    ///     The RSI must contain a state for every slot the attachable fits into.
+    ///     If the attachment only fits into one slot, it should be named as follows: normal-state-a.
     ///     The slot names can be found in AttachableHolderComponent.cs
     /// </summary>
     [DataField, AutoNetworkedField]
@@ -29,8 +29,8 @@ public sealed partial class AttachableVisualsComponent : Component
     ///     Optional, only used if the item's own state should not be used.
     ///     This prefix is added to the name of the slot the attachable is installed in.
     ///     The prefix must be in kebab-case and end with a dash, like so: example-prefix-
-    ///     The RSI must contain a state for every slot the attachable fits into, named in
-    ///     this pattern: prefix-slot-name
+    ///     The RSI must contain a state for every slot the attachable fits into.
+    ///     If the attachment only fits into one slot, it should be named as follows: normal-state-suffix.
     ///     The slot names can be found in AttachableHolderComponent.cs
     /// </summary>
     [DataField, AutoNetworkedField]
@@ -39,13 +39,15 @@ public sealed partial class AttachableVisualsComponent : Component
     /// <summary>
     ///     If true, will include the holder's slot name to find this attachment's state
     ///     in its RSI.
+    ///     In this case, there must be a separate state for each slot the attachment fits into.
+    ///     The states should be named as follows: prefix-slot-name-suffix.
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool IncludeSlotName;
 
     /// <summary>
     ///     If this is toggled on and the item has an AttachableToggleableComponent, then the visualisation system will try to show a different sprite when it's active.
-    ///     Each active state must have "-active" appended to the end of its name.
+    ///     Each active state must have "-on" appended to the end of its name.
     /// </summary>
     [DataField, AutoNetworkedField]
     public bool ShowActive;
