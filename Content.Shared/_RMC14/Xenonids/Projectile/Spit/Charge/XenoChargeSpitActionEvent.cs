ca790b5db3e9767db8ff938bc20003e96454d8ca
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-06-23 04:13:15 -0700

    Add Lurker and Spitter abilities, enable spitter, lurker and warrior evolutions (#2566)

---
+++ Content.Shared/_RMC14/Xenonids/Projectile/Spit/Charge/XenoChargeSpitActionEvent.cs
@@ -0,0 +1,5 @@
+﻿using Content.Shared.Actions;
+
+namespace Content.Shared._RMC14.Xenonids.Projectile.Spit.Charge;
+
+public sealed partial class XenoChargeSpitActionEvent : InstantActionEvent;
---
+++ Content.Shared/_RMC14/Xenonids/Projectile/Spit/Charge/XenoChargeSpitComponent.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Damage;
+using Content.Shared.FixedPoint;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Xenonids.Projectile.Spit.Charge;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(XenoSpitSystem))]
+public sealed partial class XenoChargeSpitComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public FixedPoint2 PlasmaCost = 50;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan Duration = TimeSpan.FromSeconds(4);
+
+    [DataField]
+    public DamageSpecifier Damage = new();
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan Lifetime = TimeSpan.FromSeconds(0.18);
+}
