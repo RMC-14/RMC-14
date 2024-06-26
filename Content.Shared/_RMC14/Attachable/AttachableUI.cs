9bd9085d152c5db44ebb68fa58739c8d45174d4f
Sigil <84070966+Siigiil@users.noreply.github.com>
2024-06-27 06:37:10 +0700

    Adds integrated attachments, fixes a few bugs with attachments. (#2571)

--- Content.Shared/_RMC14/Attachable/AttachableUI.cs
+++ Content.Shared/_RMC14/Attachable/AttachableUI.cs
@@ -1,13 +1,12 @@
-using Content.Shared.FixedPoint;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared._RMC14.Attachable;
 
 [Serializable, NetSerializable]
-public sealed class AttachableHolderStripUserInterfaceState(Dictionary<string, string?> attachableSlots)
+public sealed class AttachableHolderStripUserInterfaceState(Dictionary<string, (string?, bool)> attachableSlots)
     : BoundUserInterfaceState
 {
-    public Dictionary<string, string?> AttachableSlots = attachableSlots;
+    public Dictionary<string, (string?, bool)> AttachableSlots = attachableSlots;
 }
 
 [Serializable, NetSerializable]
@@ -26,43 +25,6 @@ public sealed class AttachableHolderDetachMessage(string slot) : BoundUserInterf
 public sealed class AttachableHolderAttachToSlotMessage(string slot) : BoundUserInterfaceMessage
 {
     public readonly string Slot = slot;
-}
-
-[Serializable, NetSerializable, DataDefinition]
-public sealed partial class AttachableWeaponMeleeModifierSet;
-
-[Serializable, NetSerializable, DataDefinition]
-public sealed partial class AttachableWeaponRangedModifierSet
-{
-    [DataField]
-    public int ShotsPerBurst;
-
-    [DataField]
-    public FixedPoint2 DamageFlat = FixedPoint2.Zero;
-
-    [DataField]
-    public float RecoilFlat;
-
-    [DataField]
-    public double AngleIncrease = 1.0;
-
-    [DataField]
-    public double AngleDecay = 1.0;
-
-    [DataField]
-    public double MaxAngle = 1.0;
-
-    [DataField]
-    public double MinAngle = 1.0;
-
-    [DataField]
-    public float FireRate = 1.0f;
-
-    [DataField]
-    public float ProjectileSpeedFlat = 0;
-
-    [DataField]
-    public float ProjectileSpeedMultiplier = 1.0f;
 }
 
 [Serializable, NetSerializable]
