eaea39d6dd476d3a688ee81407d6a1bdc5dcb756
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-06-26 22:04:10 -0700

    Replace succumb, last words and fake death with ghost action and a warning (#2644)

---
+++ Content.Shared/_RMC14/Mobs/CMMobStateSystem.cs
@@ -0,0 +1,60 @@
+﻿using Content.Shared.Mobs.Components;
+using Content.Shared.Mobs.Systems;
+using Robust.Shared.Console;
+using Robust.Shared.Network;
+using Robust.Shared.Player;
+
+namespace Content.Shared._RMC14.Mobs;
+
+public sealed class CMMobStateSystem : EntitySystem
+{
+    [Dependency] private readonly IConsoleHost _host = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<MobStateActionsComponent, CMGhostActionEvent>(OnMobStateActionsGhost);
+
+        Subs.BuiEvents<MobStateActionsComponent>(CMMobStateActionsUI.Key,
+            sub =>
+            {
+                sub.Event<CMGhostActionBuiMsg>(OnGhostActionBuiMsg);
+            });
+    }
+
+    private void OnMobStateActionsGhost(Entity<MobStateActionsComponent> ent, ref CMGhostActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (_mobState.IsDead(ent))
+        {
+            if (_net.IsServer && TryComp(ent, out ActorComponent? actor))
+                _host.ExecuteCommand(actor.PlayerSession, "ghost");
+
+            return;
+        }
+
+        args.Handled = true;
+        _ui.OpenUi(ent.Owner, CMMobStateActionsUI.Key, ent);
+    }
+
+    private void OnGhostActionBuiMsg(Entity<MobStateActionsComponent> ent, ref CMGhostActionBuiMsg args)
+    {
+        if (!_mobState.IsIncapacitated(ent))
+            return;
+
+        if (!TryGetEntity(args.Entity, out var entity) ||
+            entity != args.Actor)
+        {
+            return;
+        }
+
+        _ui.CloseUi(ent.Owner, CMMobStateActionsUI.Key);
+
+        if (_net.IsServer && TryComp(args.Actor, out ActorComponent? actor))
+            _host.ExecuteCommand(actor.PlayerSession, "ghost");
+    }
+}
