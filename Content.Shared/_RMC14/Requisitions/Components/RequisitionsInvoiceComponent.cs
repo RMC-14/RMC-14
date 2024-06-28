ad2ecd77cb343ca8b63d395fcbaf472cc881f295
Noisrevni <152494932+noisrevni-dev@users.noreply.github.com>
2024-06-28 09:32:44 +0800

    Requisitions paperwork reward (#2409)

---
+++ Content.Shared/_RMC14/Requisitions/Components/RequisitionsInvoiceComponent.cs
@@ -0,0 +1,18 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RMC14.Requisitions.Components;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SharedRequisitionsSystem))]
+public sealed partial class RequisitionsInvoiceComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public int Reward = 300;
+
+    [DataField]
+    public EntProtoId PaperOutput = "RMCPaperRequisitionInvoice";
+
+    [DataField]
+    public string? RequiredStamp = "paper_stamp-approve";
+}
