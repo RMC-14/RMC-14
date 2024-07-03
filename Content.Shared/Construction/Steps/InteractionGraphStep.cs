053de273820e8420e99c17768c63b89ec907bc77
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-07-03 15:05:51 -0700

    Add InteractionGraphStep examine and guide strings

--- Content.Shared/Construction/Steps/InteractionGraphStep.cs
+++ Content.Shared/Construction/Steps/InteractionGraphStep.cs
@@ -1,16 +1,34 @@
 ﻿using Content.Shared.Examine;
+using ConstructionInteractionVerbComponent = Content.Shared.Construction.Components.ConstructionInteractionVerbComponent;
 
 namespace Content.Shared.Construction.Steps;
 
 public sealed partial class InteractionGraphStep : ConstructionGraphStep
 {
+    /// <summary>
+    /// A localization string used for the guidebook.
+    /// </summary>
+    [DataField]
+    public LocId GuideString = "construction-interact-entity-guide";
+
     public override void DoExamine(ExaminedEvent examinedEvent)
     {
-        throw new NotImplementedException();
+        var target = examinedEvent.Examined;
+
+        var entMan = IoCManager.Resolve<EntityManager>();
+        if (entMan.TryGetComponent<ConstructionInteractionVerbComponent>(target, out var verbComp))
+        {
+            examinedEvent.PushMarkup(Loc.GetString("construction-interact-entity-verb",
+                ("target", target),
+                ("verbName", verbComp.VerbText)));
+        }
     }
 
     public override ConstructionGuideEntry GenerateGuideEntry()
     {
-        throw new NotImplementedException();
+        return new ConstructionGuideEntry
+        {
+            Localization = GuideString,
+        };
     }
 }
---
+++ Resources/Locale/en-US/construction/steps/interaction-construction-graph-step.ftl
@@ -0,0 +1,2 @@
+﻿construction-interact-entity-verb = Next, interact with {THE($target)} using the verb {$verbName}.
+construction-interact-entity-guide = The entity must be interacted with.
