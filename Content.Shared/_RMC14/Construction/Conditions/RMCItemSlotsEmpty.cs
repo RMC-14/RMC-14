a3926253251d53cde0ca468e9dddf0ef9b6c0ab5
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-07-03 12:20:39 -0700

    Add empty check for cardboard deconstruction

---
+++ Content.Shared/_RMC14/Construction/Conditions/RMCItemSlotsEmpty.cs
@@ -0,0 +1,36 @@
+﻿using Content.Shared._RMC14.Inventory;
+using Content.Shared.Construction;
+using Content.Shared.Examine;
+using JetBrains.Annotations;
+
+namespace Content.Shared._RMC14.Construction.Conditions;
+
+[UsedImplicitly]
+[DataDefinition]
+public sealed partial class RMCItemSlotsEmpty : IGraphCondition
+{
+    public bool Condition(EntityUid uid, IEntityManager entityManager)
+    {
+        return entityManager.System<SharedCMInventorySystem>().GetItemSlotsFilled(uid).Filled == 0;
+    }
+
+    public bool DoExamine(ExaminedEvent args)
+    {
+        var entMan = IoCManager.Resolve<IEntityManager>();
+        var inventorySys = entMan.System<SharedCMInventorySystem>();
+
+        if (inventorySys.GetItemSlotsFilled(args.Examined).Filled == 0)
+            return false;
+
+        args.PushMarkup(Loc.GetString("rmc-construction-slots-examine-empty"));
+        return true;
+    }
+
+    public IEnumerable<ConstructionGuideEntry> GenerateGuideEntry()
+    {
+        yield return new ConstructionGuideEntry
+        {
+            Localization = "rmc-construction-slots-guide-empty",
+        };
+    }
+}
---
+++ Resources/Locale/en-US/_RMC14/construction/item-slots-empty.ftl
@@ -0,0 +1,2 @@
+﻿rmc-construction-slots-examine-empty = First, empty the contents.
+rmc-construction-slots-guide-empty = Empty the contents.
