4ec74fa62da5adaabbd0833d8cf9b368dbda0bc4
DrSmugleaf <drsmugleaf@gmail.com>
2024-07-04 07:06:01 -0700

    Make medbay personnel unable to wear non-medbay armor, increase their gun spread from +10 to +75, reduce their crit health from 125 to 75

---
+++ Content.Shared/_RMC14/Marines/Armor/RMCBulkyArmorSystem.cs
@@ -0,0 +1,28 @@
+﻿using Content.Shared.Inventory.Events;
+using Content.Shared.Popups;
+
+namespace Content.Shared._RMC14.Marines.Armor;
+
+public sealed class RMCBulkyArmorSystem : EntitySystem
+{
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<RMCBulkyArmorComponent, BeingEquippedAttemptEvent>(OnBeingEquippedAttempt);
+    }
+
+    private void OnBeingEquippedAttempt(Entity<RMCBulkyArmorComponent> armor, ref BeingEquippedAttemptEvent args)
+    {
+        if (!armor.Comp.IsBulky || !HasComp<RMCUserBulkyArmorIncapableComponent>(args.EquipTarget))
+            return;
+
+        if (args.EquipTarget == args.Equipee)
+            _popup.PopupClient(Loc.GetString("rmc-bulky-armor-user-unable", ("armor", armor)), args.Equipee, args.Equipee, PopupType.MediumCaution);
+        else
+            _popup.PopupClient(Loc.GetString("rmc-bulky-armor-target-unable", ("target", args.EquipTarget), ("armor", armor)), args.Equipee, args.Equipee, PopupType.MediumCaution);
+
+        args.Cancel();
+    }
+}
---
+++ Resources/Locale/en-US/_RMC14/armor/rmc-bulky-armor.ftl
@@ -0,0 +1,2 @@
+﻿rmc-bulky-armor-target-unable = {$target} is too weak to equip {$armor}!
+rmc-bulky-armor-user-unable = You are too weak to equip {$armor}!
