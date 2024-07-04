01f3dadd747f2c47a6d024e5b825d9f987310e8b
DrSmugleaf <drsmugleaf@gmail.com>
2024-07-04 07:06:01 -0700

    Make medbay personnel unable to wear non-medbay armor, increase their gun spread from +10 to +75, reduce their crit health from 125 to 75

---
+++ Content.Shared/_RMC14/Marines/Armor/RMCUserBulkyArmorIncapableComponent.cs
@@ -0,0 +1,7 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Marines.Armor;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(RMCBulkyArmorSystem))]
+public sealed partial class RMCUserBulkyArmorIncapableComponent : Component;
