a50b477fbb8c694611f9dd7069ea39a3e78b7d5a
krose124 <126707777+krose124@users.noreply.github.com>
2024-07-04 09:45:36 -0500

    Create GunAPStacksSystem.cs

---
+++ Content.Shared/_RMC14/Weapons/Ranged/GunAPStacksSystem.cs
@@ -0,0 +1,15 @@
+﻿using Content.Shared._RMC14.Weapons.Common;
+using Content.Shared.Weapons.Ranged.Events;
+using Content.Shared.Weapons.Ranged.Systems;
+
+namespace Content.Shared._RMC14.Weapons.Ranged;
+
+public abstract class SharedPumpActionSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<PumpActionComponent, ExaminedEvent>(OnExamined, before: [typeof(SharedGunSystem)]);
+        SubscribeLocalEvent<PumpActionComponent, AttemptShootEvent>(OnAttemptShoot);
+    }
+
+}
