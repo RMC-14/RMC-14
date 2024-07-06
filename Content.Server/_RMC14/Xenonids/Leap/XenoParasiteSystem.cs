04fe5900393edbeafa4794d1ffbc211657cb5b3f
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-07-06 10:30:37 -0700

    Refactor parasite ghosting

--- Content.Server/_RMC14/Xenonids/Leap/XenoParasiteSystem.cs
+++ Content.Server/_RMC14/Xenonids/Leap/XenoParasiteSystem.cs
@@ -1,15 +1,16 @@
-﻿using Content.Server.GameTicking;
-using Content.Server.Ghost.Roles.Components;
+﻿using Content.Server.Ghost.Roles.Components;
+using Content.Server.Ghost;
+using Content.Server.Mind;
 using Content.Shared._RMC14.Xenonids.Parasite;
-using Content.Shared.Coordinates;
+using Content.Shared.Mind;
 using Robust.Shared.Player;
 
 namespace Content.Server._RMC14.Xenonids.Leap;
 
 public sealed class XenoParasiteSystem : SharedXenoParasiteSystem
 {
-    [Dependency] private readonly GameTicker _gameTicker = default!;
-    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly GhostSystem _ghostSystem = default!;
+    [Dependency] private readonly MindSystem _mind = default!;
 
     protected override void ParasiteLeapHit(Entity<XenoParasiteComponent> parasite)
     {
@@ -19,11 +20,13 @@ public sealed class XenoParasiteSystem : SharedXenoParasiteSystem
         RemComp<GhostTakeoverAvailableComponent>(parasite);
 
         var session = actor.PlayerSession;
-        _gameTicker.SpawnObserver(session);
-        if (session.AttachedEntity is not { } entity)
-            return;
 
-        _transform.SetCoordinates(entity, parasite.Owner.ToCoordinates());
-        _transform.AttachToGridOrMap(entity);
+        Entity<MindComponent> mind;
+        if (_mind.TryGetMind(session, out var mindId, out var mindComp))
+            mind = (mindId, mindComp);
+        else
+            mind = _mind.CreateMind(session.UserId);
+
+        _ghostSystem.SpawnGhost((mind.Owner, mind.Comp), parasite);
     }
 }
