cb997c69cc1c37384c9bc3de05b5be3c9641aa1d
Verm <vermidiathesnake@gmail.com>
2024-07-06 16:50:44 -0500

    Add embryo, make it on component remove

--- Content.Shared/_RMC14/Xenonids/Parasite/SharedXenoParasiteSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Parasite/SharedXenoParasiteSystem.cs
@@ -202,6 +202,8 @@ public abstract class SharedXenoParasiteSystem : EntitySystem
     {
         _blindable.UpdateIsBlind(victim.Owner);
         _standing.Stand(victim);
+        if (_net.IsServer && TryComp<TransformComponent>(victim, out var xform))
+            SpawnAtPosition("RMCXenoEmbryo", xform.Coordinates);
     }
 
     private void OnVictimInfectedCancel<T>(Entity<VictimInfectedComponent> victim, ref T args) where T : CancellableEntityEventArgs
