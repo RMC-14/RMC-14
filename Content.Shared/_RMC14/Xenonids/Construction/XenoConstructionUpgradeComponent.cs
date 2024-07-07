e7eda424db58fefba37ee70d00799018165777c4
ShadowCommander <10494922+ShadowCommander@users.noreply.github.com>
2024-07-07 06:07:18 -0700

    Add summary to upgrade component

--- Content.Shared/_RMC14/Xenonids/Construction/XenoConstructionUpgradeComponent.cs
+++ Content.Shared/_RMC14/Xenonids/Construction/XenoConstructionUpgradeComponent.cs
@@ -1,11 +1,16 @@
-﻿using Content.Shared.FixedPoint;
-using Robust.Shared.Prototypes;
+﻿using Robust.Shared.Prototypes;
 
 namespace Content.Shared._RMC14.Xenonids.Construction;
 
+/// <summary>
+/// Used for upgrading a resin structure to a thicker version.:
+/// </summary>
 [RegisterComponent]
 public sealed partial class XenoConstructionUpgradeComponent : Component
 {
+    /// <summary>
+    /// The prototypeId that is allowed to replace this entity.
+    /// </summary>
     [DataField]
     public EntProtoId Proto;
 }
