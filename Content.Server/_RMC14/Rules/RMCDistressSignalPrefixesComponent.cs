ff5f100486cf0a5628da5f9acf71c556fb033cde
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-07-08 16:49:37 -0700

    Add operation names, make Discord round start and end messages include the operation name and planet map chosen (#2917)

---
+++ Content.Server/_RMC14/Rules/RMCDistressSignalPrefixesComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Server._RMC14.Rules;
+
+[RegisterComponent]
+[Access(typeof(CMDistressSignalRuleSystem))]
+public sealed partial class RMCDistressSignalPrefixesComponent : Component
+{
+    [DataField]
+    public HashSet<string> Prefixes = new();
+}
