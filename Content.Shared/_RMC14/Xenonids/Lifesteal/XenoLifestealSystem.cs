9412391e8833c55fe370cb80f624dc6a7fe149f5
DrSmugleaf <drsmugleaf@gmail.com>
2024-07-08 03:32:55 -0700

    Popup when hitting max

--- Content.Shared/_RMC14/Xenonids/Lifesteal/XenoLifestealSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Lifesteal/XenoLifestealSystem.cs
@@ -3,7 +3,9 @@ using Content.Shared._RMC14.Marines;
 using Content.Shared.Coordinates;
 using Content.Shared.Damage;
 using Content.Shared.FixedPoint;
+using Content.Shared.Popups;
 using Content.Shared.Weapons.Melee.Events;
+using Robust.Shared.Player;
 
 namespace Content.Shared._RMC14.Xenonids.Lifesteal;
 
@@ -12,6 +14,7 @@ public sealed class XenoLifestealSystem : EntitySystem
     [Dependency] private readonly CMDamageableSystem _cmDamageable = default!;
     [Dependency] private readonly DamageableSystem _damageable = default!;
     [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
 
     private readonly HashSet<Entity<MarineComponent>> _targets = new();
 
@@ -26,7 +29,7 @@ public sealed class XenoLifestealSystem : EntitySystem
         SubscribeLocalEvent<XenoLifestealComponent, MeleeHitEvent>(OnMeleeHit);
     }
 
-    private void OnMeleeHit(Entity<XenoLifestealComponent> ent, ref MeleeHitEvent args)
+    private void OnMeleeHit(Entity<XenoLifestealComponent> xeno, ref MeleeHitEvent args)
     {
         if (!args.IsHit)
             return;
@@ -44,7 +47,7 @@ public sealed class XenoLifestealSystem : EntitySystem
         if (!found)
             return;
 
-        if (!_damageableQuery.TryComp(ent, out var damageable))
+        if (!_damageableQuery.TryComp(xeno, out var damageable))
             return;
 
         var total = damageable.TotalDamage;
@@ -52,24 +55,34 @@ public sealed class XenoLifestealSystem : EntitySystem
             return;
 
         _targets.Clear();
-        _entityLookup.GetEntitiesInRange(ent.Owner.ToCoordinates(), ent.Comp.TargetRange, _targets);
+        _entityLookup.GetEntitiesInRange(xeno.Owner.ToCoordinates(), xeno.Comp.TargetRange, _targets);
 
-        var lifesteal = ent.Comp.BasePercentage;
+        var lifesteal = xeno.Comp.BasePercentage;
         foreach (var hit in _targets)
         {
             if (!_marineQuery.HasComp(hit))
                 continue;
 
-            lifesteal += ent.Comp.TargetIncreasePercentage;
-            if (lifesteal >= ent.Comp.MaxPercentage)
+            lifesteal += xeno.Comp.TargetIncreasePercentage;
+            if (lifesteal >= xeno.Comp.MaxPercentage)
             {
-                lifesteal = ent.Comp.MaxPercentage;
+                lifesteal = xeno.Comp.MaxPercentage;
                 break;
             }
         }
 
-        var amount = -FixedPoint2.Clamp(total * lifesteal, ent.Comp.MinHeal, ent.Comp.MaxHeal);
-        var heal = _cmDamageable.DistributeTypes(ent.Owner, amount);
-        _damageable.TryChangeDamage(ent, heal, true);
+        var amount = -FixedPoint2.Clamp(total * lifesteal, xeno.Comp.MinHeal, xeno.Comp.MaxHeal);
+        var heal = _cmDamageable.DistributeTypes(xeno.Owner, amount);
+        _damageable.TryChangeDamage(xeno, heal, true);
+
+        if (lifesteal >= xeno.Comp.MaxPercentage)
+        {
+            var marines = Filter.PvsExcept(xeno).RemoveWhereAttachedEntity(e => !_marineQuery.HasComp(e));
+            var marineMsg = Loc.GetString("rmc-lifesteal-more-marine", ("xeno", xeno.Owner));
+            _popup.PopupEntity(marineMsg, xeno, marines, true, PopupType.SmallCaution);
+
+            var selfMsg = Loc.GetString("rmc-lifesteal-more-self");
+            _popup.PopupClient(selfMsg, xeno, xeno);
+        }
     }
 }
