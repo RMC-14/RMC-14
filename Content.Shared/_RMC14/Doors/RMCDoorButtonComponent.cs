9119d87750ddd8d05d1893cbaad40ea78d7e59e2
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-07-08 20:51:56 -0700

    Add pod door buttons (#2922)

---
+++ Content.Shared/_RMC14/Doors/RMCDoorButtonComponent.cs
@@ -0,0 +1,21 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+
+namespace Content.Shared._RMC14.Doors;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState, AutoGenerateComponentPause]
+[Access(typeof(CMDoorSystem))]
+public sealed partial class RMCDoorButtonComponent : Component
+{
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
+    public TimeSpan LastUse;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan Cooldown = TimeSpan.FromSeconds(0.5);
+
+    [DataField, AutoNetworkedField]
+    public string OffState = "doorctrl";
+
+    [DataField, AutoNetworkedField]
+    public string OnState = "doorctrl1";
+}
