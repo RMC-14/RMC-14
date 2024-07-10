8f6c8fe3924f75665e1ea11182f8af1312cea81c
Taran <t.purewal50@gmail.com>
2024-07-10 09:33:16 -0600

    Revert "edits sharedStorageSystem to allow storage of other storage items if in whitelist"
    This reverts commit 7189526de58d6da4cb550782720bcebd41d45cc9.

--- Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
+++ Content.Shared/Storage/EntitySystems/SharedStorageSystem.cs
@@ -917,8 +917,7 @@ public abstract class SharedStorageSystem : EntitySystem
 
         if (TryComp<StorageComponent>(insertEnt, out var insertStorage)
             && GetMaxItemSize((insertEnt, insertStorage)) >= maxSize
-            && !CMStorage.IgnoreItemSize((uid, storageComp), insertEnt)
-            && !_whitelistSystem.IsWhitelistPass(storageComp.Whitelist, insertEnt))
+            && !CMStorage.IgnoreItemSize((uid, storageComp), insertEnt))
         {
             reason = "comp-storage-too-big";
             return false;
