e9e804e3fb0415731146110707b199ae700c79c6
Kaga <103199482+Kaga-404@users.noreply.github.com>
2024-07-09 23:16:44 -0700

    temp merge fix

--- Content.Shared/_RMC14/Weapons/Ranged/CMGunSystem.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/CMGunSystem.cs
@@ -98,7 +98,7 @@ public sealed class CMGunSystem : EntitySystem
         if (direction == Vector2.Zero)
             return;
 
-        var distance = ent.Comp.MaxRange != null ? Math.Min(ent.Comp.MaxRange.Value, direction.Length()) : direction.Length();
+        var distance = ent.Comp.MaxFixedRange != null ? Math.Min(ent.Comp.MaxFixedRange, direction.Length()) : direction.Length();
         var time = _timing.CurTime;
         var normalized = direction.Normalized();
 
@@ -139,7 +139,7 @@ public sealed class CMGunSystem : EntitySystem
                 comp.FlyEndTime = time + TimeSpan.FromSeconds(cappedRange / gun.ProjectileSpeedModified);
             }
             else
-                comp.FlyEndTime = time + TimeSpan.FromSeconds(distance / gun.ProjectileSpeedModified);
+                comp.FlyEndTime = time + TimeSpan.FromSeconds(ammoDistance / gun.ProjectileSpeedModified);
         }
     }
 
