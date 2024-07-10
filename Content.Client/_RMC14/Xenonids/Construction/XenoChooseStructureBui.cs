4d161f57e2d5b7125da0e66d41713db678868240
DrSmugleaf <drsmugleaf@gmail.com>
2024-07-10 01:14:17 -0700

    Update UI

--- Content.Client/_RMC14/Xenonids/Construction/XenoChooseStructureBui.cs
+++ Content.Client/_RMC14/Xenonids/Construction/XenoChooseStructureBui.cs
@@ -2,6 +2,7 @@
 using Content.Shared._RMC14.Xenonids.Construction;
 using JetBrains.Annotations;
 using Robust.Client.GameObjects;
+using Robust.Client.UserInterface.Controls;
 using Robust.Shared.Prototypes;
 
 namespace Content.Client._RMC14.Xenonids.Construction;
@@ -13,6 +14,8 @@ public sealed class XenoChooseStructureBui : BoundUserInterface
 
     private readonly SpriteSystem _sprite;
     private readonly SharedXenoConstructionSystem _xenoConstruction;
+
+    private readonly Dictionary<EntProtoId, XenoChoiceControl> _buttons = new();
 
     [ViewVariables]
     private XenoChooseStructureWindow? _window;
@@ -28,6 +31,8 @@ public sealed class XenoChooseStructureBui : BoundUserInterface
         _window = new XenoChooseStructureWindow();
         _window.OnClose += Close;
 
+        _buttons.Clear();
+        var group = new ButtonGroup();
         if (EntMan.TryGetComponent(Owner, out XenoConstructionComponent? xeno))
         {
             foreach (var structureId in xeno.CanBuild)
@@ -36,17 +41,25 @@ public sealed class XenoChooseStructureBui : BoundUserInterface
                     continue;
 
                 var control = new XenoChoiceControl();
+                control.Button.Group = group;
+
                 var name = structure.Name;
                 if (_xenoConstruction.GetStructurePlasmaCost(structureId) is { } cost)
                     name += $" ({cost} plasma)";
 
                 control.Set(name, _sprite.Frame0(structure));
-                control.Button.OnPressed += _ => SendPredictedMessage(new XenoChooseStructureBuiMsg(structureId));
+                control.Button.OnPressed += _ =>
+                {
+                    SendPredictedMessage(new XenoChooseStructureBuiMsg(structureId));
+                    Refresh();
+                };
 
                 _window.StructureContainer.AddChild(control);
+                _buttons.Add(structureId, control);
             }
         }
 
+        Refresh();
         _window.OpenCentered();
     }
 
@@ -54,5 +67,16 @@ public sealed class XenoChooseStructureBui : BoundUserInterface
     {
         if (disposing)
             _window?.Dispose();
+    }
+
+    public void Refresh()
+    {
+        if (EntMan.GetComponentOrNull<XenoConstructionComponent>(Owner)?.BuildChoice is not { } choice ||
+            !_buttons.TryGetValue(choice, out var button))
+        {
+            return;
+        }
+
+        button.Button.Pressed = true;
     }
 }
