61e9c9f9c5dae1daf3241021ad7072412283b0bd
DrSmugleaf <drsmugleaf@gmail.com>
2024-07-20 00:30:52 -0700

    Fix flashes not requiring police skill, make stun baton require police skill, longer stun duration

---
+++ Content.Shared/_RMC14/Marines/Skills/MeleeRequiresSkillComponent.cs
@@ -0,0 +1,11 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Marines.Skills;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SkillsSystem))]
+public sealed partial class MeleeRequiresSkillComponent : Component
+{
+    [DataField(required: true), AutoNetworkedField]
+    public Skills Skills;
+}
---
+++ Content.Shared/_RMC14/Throwing/ThrowItemAttemptEvent.cs
@@ -0,0 +1,4 @@
+﻿namespace Content.Shared._RMC14.Throwing;
+
+[ByRefEvent]
+public record struct ThrowItemAttemptEvent(EntityUid User, bool Cancelled = false);
