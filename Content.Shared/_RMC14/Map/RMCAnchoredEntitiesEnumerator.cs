6821b28dfa8ffe83f815b81bed47a72c78df39e4
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-07-22 03:48:43 -0700

    Add Spray Acid ability (#3157)

---
+++ Content.Shared/_RMC14/Map/RMCAnchoredEntitiesEnumerator.cs
@@ -0,0 +1,41 @@
+﻿// ReSharper disable StructCanBeMadeReadOnly
+
+using Robust.Shared.Map.Enumerators;
+
+namespace Content.Shared._RMC14.Map;
+
+public struct RMCAnchoredEntitiesEnumerator(
+    SharedTransformSystem transform,
+    AnchoredEntitiesEnumerator enumerator,
+    DirectionFlag facing = DirectionFlag.None
+) : IDisposable
+{
+    // ReSharper disable once CollectionNeverUpdated.Local
+    public static readonly RMCAnchoredEntitiesEnumerator Empty = new(default!, AnchoredEntitiesEnumerator.Empty);
+
+    public bool MoveNext(out EntityUid uid)
+    {
+        while (enumerator.MoveNext(out var uidNullable))
+        {
+            if (facing == DirectionFlag.None)
+            {
+                uid = uidNullable.Value;
+                return true;
+            }
+
+            if ((transform.GetWorldRotation(uidNullable.Value).GetDir().AsFlag() & facing) == 0)
+                continue;
+
+            uid = uidNullable.Value;
+            return true;
+        }
+
+        uid = default;
+        return false;
+    }
+
+    public void Dispose()
+    {
+        enumerator.Dispose();
+    }
+}
---
+++ Content.Shared/_RMC14/Xenonids/Spray/XenoSprayAcidComponent.cs
@@ -0,0 +1,22 @@
+﻿using Content.Shared.Damage;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RMC14.Xenonids.Spray;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(XenoSprayAcidSystem))]
+public sealed partial class XenoSprayAcidComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public EntProtoId Acid = "XenoAcidSprayWeak";
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan Delay = TimeSpan.FromSeconds(0.2);
+
+    [DataField, AutoNetworkedField]
+    public DamageSpecifier BarricadeDamage;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan BarricadeDuration = TimeSpan.FromSeconds(20);
+}
