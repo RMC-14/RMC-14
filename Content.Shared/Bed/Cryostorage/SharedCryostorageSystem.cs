698737c5e0b93799042803900afa66c313e928ac
0xRainy <35612912+0xRainy@users.noreply.github.com>
2024-07-21 19:31:35 -0700

    Xenos can't cryo

--- Content.Shared/Bed/Cryostorage/SharedCryostorageSystem.cs
+++ Content.Shared/Bed/Cryostorage/SharedCryostorageSystem.cs
@@ -5,6 +5,7 @@ using Content.Shared.GameTicking;
 using Content.Shared.Mind;
 using Content.Shared.Mind.Components;
 using Content.Shared.Mobs.Systems;
+using Content.Shared._RMC14.Xenonids;
 using Robust.Shared.Configuration;
 using Robust.Shared.Containers;
 using Robust.Shared.Map;
@@ -82,6 +83,12 @@ public abstract class SharedCryostorageSystem : EntitySystem
         var (_, comp) = ent;
         if (args.Container.ID != comp.ContainerId)
             return;
+
+        if (HasComp<XenoComponent>(args.EntityUid))
+        {
+            args.Cancel();
+            return;
+        }
 
         if (_mobState.IsIncapacitated(args.EntityUid))
         {
