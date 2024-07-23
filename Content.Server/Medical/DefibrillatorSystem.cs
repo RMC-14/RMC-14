18c5c02d8ccfd615971b3c3841428e4054aa901c
Mr. 27 <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2024-07-23 18:18:55 -0400

    Update Content.Server/Medical/DefibrillatorSystem.cs
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>

--- Content.Server/Medical/DefibrillatorSystem.cs
+++ Content.Server/Medical/DefibrillatorSystem.cs
@@ -154,13 +154,13 @@ public sealed class DefibrillatorSystem : EntitySystem
 
         return _doAfter.TryStartDoAfter(new DoAfterArgs(EntityManager, user, component.DoAfterDuration, new DefibrillatorZapDoAfterEvent(),
             uid, target, uid)
-        {
-            BlockDuplicate = true,
-            BreakOnHandChange = true,
-            NeedHand = true,
-            BreakOnMove = !component.AllowDoAfterMovement,
-            DuplicateCondition = DuplicateConditions.SameEvent,
-        });
+            {
+                BlockDuplicate = true,
+                BreakOnHandChange = true,
+                NeedHand = true,
+                BreakOnMove = !component.AllowDoAfterMovement,
+                DuplicateCondition = DuplicateConditions.SameEvent,
+            });
     }
 
     public void Zap(EntityUid uid, EntityUid target, EntityUid user, DefibrillatorComponent? component = null, MobStateComponent? mob = null, MobThresholdsComponent? thresholds = null)
