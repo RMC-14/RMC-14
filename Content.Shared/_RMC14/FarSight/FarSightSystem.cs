c924313dc922e19c5262f27d456fe958f25fef19
Verm <vermidiathesnake@gmail.com>
2024-07-24 14:20:46 -0500

    Fix farsight

--- Content.Shared/_RMC14/FarSight/FarSightSystem.cs
+++ Content.Shared/_RMC14/FarSight/FarSightSystem.cs
@@ -1,4 +1,7 @@
 ﻿using Content.Shared.Actions;
+using Content.Shared.Inventory;
+using Content.Shared.Inventory.Events;
+using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Systems;
 
 namespace Content.Shared._RMC14.FarSight;
@@ -8,16 +11,19 @@ public sealed class FarSightSystem : EntitySystem
     [Dependency] private readonly SharedActionsSystem _actions = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
     [Dependency] private readonly SharedContentEyeSystem _eye = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
 
     public override void Initialize()
     {
         SubscribeLocalEvent<FarSightItemComponent, GetItemActionsEvent>(OnFarSightGetItemActions);
         SubscribeLocalEvent<FarSightItemComponent, FarSightActionEvent>(OnFarSightAction);
+        SubscribeLocalEvent<FarSightItemComponent, GotUnequippedEvent>(OnFarSightUnequipped);
+        SubscribeLocalEvent<FarSightItemComponent, GotEquippedEvent>(OnFarSightEquipped);
     }
 
     private void OnFarSightGetItemActions(Entity<FarSightItemComponent> ent, ref GetItemActionsEvent args)
     {
-        if (args.InHands)
+        if (args.InHands || !_inventory.InSlotWithFlags((ent, null, null), ent.Comp.Slots))
             return;
 
         args.AddAction(ref ent.Comp.Action, ent.Comp.ActionId);
@@ -42,10 +48,47 @@ public sealed class FarSightSystem : EntitySystem
         }
         else
         {
+            if (TryComp<EyeComponent>(user, out var eye))
+                _eye.SetMaxZoom(user, eye.Zoom);
+
             _eye.ResetZoom(user);
         }
 
         _actions.SetToggled(ent.Comp.Action, ent.Comp.Enabled);
         _appearance.SetData(ent, FarSightItemVisuals.Active, ent.Comp.Enabled);
+    }
+
+    private void OnFarSightEquipped(Entity<FarSightItemComponent> ent, ref GotEquippedEvent args)
+    {
+        var user = args.Equipee;
+
+        if (!_inventory.InSlotWithFlags((ent, null, null), ent.Comp.Slots))
+            return;
+
+        if (ent.Comp.Enabled)
+        {
+            _eye.SetMaxZoom(user, ent.Comp.Zoom);
+            _eye.SetZoom(user, ent.Comp.Zoom);
+        }
+        else
+        {
+            if (TryComp<EyeComponent>(user, out var eye))
+                _eye.SetMaxZoom(user, eye.Zoom);
+
+            _eye.ResetZoom(user);
+        }
+    }
+
+    private void OnFarSightUnequipped(Entity<FarSightItemComponent> ent, ref GotUnequippedEvent args)
+    {
+        var user = args.Equipee;
+
+        if (_inventory.InSlotWithFlags((ent, null, null), ent.Comp.Slots))
+            return;
+
+        if (TryComp<EyeComponent>(user, out var eye))
+            _eye.SetMaxZoom(user, eye.Zoom);
+
+        _eye.ResetZoom(user);
     }
 }
