f903a438d1f2dc5173d61b97daa8a50162328ec4
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-07-25 15:09:36 -0700

    Fix the Boiler's bombard not taking plasma to use (#3273)

--- Content.Shared/_RMC14/Xenonids/Bombard/XenoBombardSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Bombard/XenoBombardSystem.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using Content.Shared._RMC14.Actions;
 using Content.Shared._RMC14.Projectiles;
+using Content.Shared._RMC14.Xenonids.Plasma;
 using Content.Shared.DoAfter;
 using Content.Shared.Popups;
 using Content.Shared.Weapons.Ranged.Systems;
@@ -20,6 +21,7 @@ public sealed class XenoBombardSystem : EntitySystem
     [Dependency] private readonly RMCActionsSystem _rmcActions = default!;
     [Dependency] private readonly RMCProjectileSystem _rmcProjectile = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly XenoPlasmaSystem _xenoPlasma = default!;
 
     public override void Initialize()
     {
@@ -36,6 +38,9 @@ public sealed class XenoBombardSystem : EntitySystem
             return;
 
         args.Handled = true;
+
+        if (!_xenoPlasma.HasPlasmaPopup(ent.Owner, ent.Comp.PlasmaCost))
+            return;
 
         var direction = target.Position - source.Position;
         if (direction.Length() > ent.Comp.Range)
@@ -64,6 +69,9 @@ public sealed class XenoBombardSystem : EntitySystem
             return;
 
         args.Handled = true;
+
+        if (!_xenoPlasma.TryRemovePlasmaPopup(ent.Owner, ent.Comp.PlasmaCost))
+            return;
 
         if (_net.IsClient)
             return;
