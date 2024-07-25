d5562624dc47be10d361aab6a135f8c6dc995e35
Alex Pavlenko <oleksa.pavlenko@gmail.com>
2024-07-25 14:55:49 +0300

    don't use trycomp unnecessarily, reduce copypaste

--- Content.Server/_RMC14/Xenonids/Acid/ServerXenoAcidSystem.cs
+++ Content.Server/_RMC14/Xenonids/Acid/ServerXenoAcidSystem.cs
@@ -1,6 +1,5 @@
 ﻿using Content.Server.Light.Components;
 using Content.Shared._RMC14.Xenonids.Acid;
-using Content.Shared.Light.Components;
 
 namespace Content.Server._RMC14.Xenonids.Acid;
 
@@ -16,13 +15,13 @@ public sealed class ServerXenoAcidSystem : EntitySystem
 
     private void OnServerCorrodingEvent(Entity<ExpendableLightComponent> target, ref ServerCorrodingEvent args)
     {
-        if (TryComp<ExpendableLightComponent>(target, out var expendable_light))
-        {
-            expendable_light.StateExpiryTime /= args.ExpendableLightDps + 1;
-            // In case expandable light is activated shortly after being corroded. Or in case we decide to not destroy corrosive lights on timers like the rest of items for whatever the reason.
-            expendable_light.GlowDuration /= args.ExpendableLightDps + 1;
-            expendable_light.FadeOutDuration /= args.ExpendableLightDps + 1;
-        }
+        // Rationale and formula: https://github.com/RMC-14/RMC-14/issues/2952#issuecomment-2227035752
+        var expendable_light = target.Comp;
+        var expendableLightDps = args.ExpendableLightDps + 1;
+        expendable_light.StateExpiryTime /= expendableLightDps;
+        // In case expandable light is activated shortly after being corroded. Or in case we decide to not destroy corrosive lights on timers like the rest of items for whatever the reason.
+        expendable_light.GlowDuration /= expendableLightDps;
+        expendable_light.FadeOutDuration /= expendableLightDps;
     }
 }
 
