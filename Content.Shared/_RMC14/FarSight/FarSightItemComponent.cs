a6e0ba0d7ed6447b88b99a1cad69b457b340c122
Verm <32827189+Vermidia@users.noreply.github.com>
2024-07-25 00:37:55 -0500

    Fix Smartgunner farsight (#3253)

--- Content.Shared/_RMC14/FarSight/FarSightItemComponent.cs
+++ Content.Shared/_RMC14/FarSight/FarSightItemComponent.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using Content.Shared.Inventory;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
 
@@ -6,7 +7,7 @@ namespace Content.Shared._RMC14.FarSight;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 [Access(typeof(FarSightSystem))]
-public sealed partial class FarSightItemComponent : Component
+public sealed partial class FarSightItemComponent : Component, IClothingSlots
 {
     [DataField, AutoNetworkedField]
     public EntProtoId ActionId = "RMCActionToggleFarSight";
@@ -19,4 +20,7 @@ public sealed partial class FarSightItemComponent : Component
 
     [DataField, AutoNetworkedField]
     public Vector2 Zoom = new(1.13f, 1.13f);
+
+    [DataField, AutoNetworkedField]
+    public SlotFlags Slots { get; set; } = SlotFlags.EYES;
 }
