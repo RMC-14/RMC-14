734d40cbe66bf33a56ab26667c1dd65e9b32b397
krose124 <126707777+krose124@users.noreply.github.com>
2024-07-27 17:11:30 -0500

    Include Xeno component

--- Content.Shared/_RMC14/Weapons/Ranged/GunAPStacksSystem.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/GunAPStacksSystem.cs
@@ -1,6 +1,7 @@
 ﻿using Content.Shared._RMC14.Weapons.Common;
 using Content.Shared.Weapons.Ranged.Events;
 using Content.Shared.Weapons.Ranged.Systems;
+using Content.Shared._RMC14.Xenoids.Components;
 
 namespace Content.Shared._RMC14.Weapons.Ranged;
 
