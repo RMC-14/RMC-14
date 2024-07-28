8531a5a9f360fe25668a2c0aa72ae517944c3f50
Verm <vermidiathesnake@gmail.com>
2024-07-28 03:09:05 -0500

    Remove extra comp usings too

--- Content.Shared/_RMC14/Xenonids/IgnoreForHivePopComponent.cs
+++ Content.Shared/_RMC14/Xenonids/IgnoreForHivePopComponent.cs
@@ -1,6 +1,4 @@
-﻿using System.Numerics;
-using Content.Shared.Access;
-namespace Content.Shared._RMC14.Xenonids;
+﻿namespace Content.Shared._RMC14.Xenonids;
 
 [RegisterComponent]
 public sealed partial class IgnoreForHivePopComponent : Component;
