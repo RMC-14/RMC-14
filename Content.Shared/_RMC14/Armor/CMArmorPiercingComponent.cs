031dc01f77d82e2e065d7fdf55f1c1f0b5e9e285
krose124 <126707777+krose124@users.noreply.github.com>
2024-07-29 22:02:04 -0500

    Someone is gonna grind me up for this in review

--- Content.Shared/_RMC14/Armor/CMArmorPiercingComponent.cs
+++ Content.Shared/_RMC14/Armor/CMArmorPiercingComponent.cs
@@ -1,9 +1,10 @@
 ﻿using Robust.Shared.GameStates;
+using Content.Shared._RMC.Weapons.Ranged;
 
 namespace Content.Shared._RMC14.Armor;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-[Access(typeof(CMArmorSystem))]
+[Access(typeof(CMArmorSystem), typeof(GunAPStacksSystem)]
 public sealed partial class CMArmorPiercingComponent : Component
 {
     [DataField, AutoNetworkedField]
