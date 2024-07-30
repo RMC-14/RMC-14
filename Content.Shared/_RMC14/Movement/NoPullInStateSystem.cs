8fdadc7a2bc18499d25a32d121829c16164783bf
deltanedas <@deltanedas:kde.org>
2024-07-30 06:38:08 +0100

    stop pulling when going from crit to alive

--- Content.Shared/_RMC14/Movement/NoPullInStateSystem.cs
+++ Content.Shared/_RMC14/Movement/NoPullInStateSystem.cs
@@ -1,10 +1,15 @@
+using Content.Shared.Mobs;
 using Content.Shared.Mobs.Components;
+using Content.Shared.Movement.Pulling.Components;
 using Content.Shared.Movement.Pulling.Events;
+using Content.Shared.Movement.Pulling.Systems;
 
 namespace Content.Shared._RMC14.Movement;
 
 public sealed class NoPullInStateSystem : EntitySystem
 {
+    [Dependency] private readonly PullingSystem _pulling = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -21,5 +26,12 @@ public sealed class NoPullInStateSystem : EntitySystem
 
         if (TryComp<MobStateComponent>(uid, out var state) && state.CurrentState == comp.State)
             args.Cancelled = true;
+    }
+
+    private void OnStateChanged(Entity<NoPullInStateComponent> ent, ref MobStateChangedEvent args)
+    {
+        var (uid, comp) = ent;
+        if (args.NewMobState == comp.State && TryComp<PullableComponent>(uid, out var pullable))
+            _pulling.TryStopPull(uid, pullable);
     }
 }
