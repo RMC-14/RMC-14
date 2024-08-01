f19b0ba9bdef7244195e4b81c8e26070c3bfab71
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-07-31 22:08:28 -0700

    Add ActiveInputMover (#3435)

---
+++ Content.Shared/_RMC14/Input/RMCInputSystem.cs
@@ -0,0 +1,60 @@
+﻿using Content.Shared._RMC14.CCVar;
+using Content.Shared.Movement.Components;
+using Robust.Shared.Configuration;
+using Robust.Shared.Player;
+
+namespace Content.Shared._RMC14.Input;
+
+public sealed class RMCInputSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _config = default!;
+
+    private bool _activeInputMoverEnabled;
+
+    private EntityQuery<ActorComponent> _actorQuery;
+
+    public override void Initialize()
+    {
+        _actorQuery = GetEntityQuery<ActorComponent>();
+
+        SubscribeLocalEvent<InputMoverComponent, MapInitEvent>(OnInputMapInit);
+
+        SubscribeLocalEvent<ActiveInputMoverComponent, MapInitEvent>(OnActiveMapInit);
+        SubscribeLocalEvent<ActiveInputMoverComponent, PlayerAttachedEvent>(OnActiveAttached);
+        SubscribeLocalEvent<ActiveInputMoverComponent, PlayerDetachedEvent>(OnActiveDetached);
+
+        Subs.CVar(_config, RMCCVars.RMCActiveInputMoverEnabled, v => _activeInputMoverEnabled = v, true);
+    }
+
+    private void OnInputMapInit(Entity<InputMoverComponent> ent, ref MapInitEvent args)
+    {
+        EnsureComp<ActiveInputMoverComponent>(ent);
+    }
+
+    private void OnActiveMapInit(Entity<ActiveInputMoverComponent> ent, ref MapInitEvent args)
+    {
+        if (!_activeInputMoverEnabled)
+            return;
+
+        if (_actorQuery.HasComp(ent))
+            EnsureComp<InputMoverComponent>(ent);
+        else
+            RemCompDeferred<InputMoverComponent>(ent);
+    }
+
+    private void OnActiveAttached(Entity<ActiveInputMoverComponent> ent, ref PlayerAttachedEvent args)
+    {
+        if (!_activeInputMoverEnabled)
+            return;
+
+        EnsureComp<InputMoverComponent>(ent);
+    }
+
+    private void OnActiveDetached(Entity<ActiveInputMoverComponent> ent, ref PlayerDetachedEvent args)
+    {
+        if (!_activeInputMoverEnabled)
+            return;
+
+        RemCompDeferred<InputMoverComponent>(ent);
+    }
+}
