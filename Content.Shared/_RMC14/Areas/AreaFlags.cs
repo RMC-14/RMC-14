976135920d1d7367870d0279272f77aa9f077b24
DrSmugleaf <drsmugleaf@gmail.com>
2024-08-03 05:56:09 -0700

    Add area component, almayer areas, area script

---
+++ Content.Shared/_RMC14/Areas/AreaFlags.cs
@@ -0,0 +1,17 @@
+﻿namespace Content.Shared._RMC14.Areas;
+
+[Flags]
+public enum AreaFlags
+{
+    None = 0,
+    CAS = 1 << 0,
+    Fulton = 1 << 1,
+    Lasing = 1 << 2,
+    Mortar = 1 << 3,
+    Medevac = 1 << 4,
+    OB = 1 << 5,
+    SupplyDrop = 1 << 6,
+    AvoidBioscan = 1 << 7,
+    NoTunnel = 1 << 8,
+    Unweedable = 1 << 9,
+}
