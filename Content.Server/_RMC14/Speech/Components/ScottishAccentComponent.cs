5a69aaf9d296a34dd0572bbd1cc721fb99129ce7
P-MCT <callzeno@gmail.com>
2024-08-06 08:55:47 +0100

    Create ScottishAccentComponent.cs

---
+++ Content.Server/_RMC14/Speech/Components/ScottishAccentComponent.cs
@@ -0,0 +1,8 @@
+using Content.Server.RMC14.Speech.EntitySystems;
+
+namespace Content.Server.RMC14.Speech.Components;
+
+[RegisterComponent]
+[Access(typeof(ScottishAccentSystem))]
+public sealed partial class ScottishAccentComponent : Component
+{ }
