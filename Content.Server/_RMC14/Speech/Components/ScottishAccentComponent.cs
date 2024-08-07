3f187a5acd19c96c54e6187b50ad89561efdefb9
P-MCT <callzeno@gmail.com>
2024-08-07 01:46:09 +0100

    Update Content.Server/_RMC14/Speech/Components/ScottishAccentComponent.cs
    Co-authored-by: Mr. 27 <45323883+Dutch-VanDerLinde@users.noreply.github.com>

--- Content.Server/_RMC14/Speech/Components/ScottishAccentComponent.cs
+++
@@ -1,8 +0,0 @@
-using Content.Server.RMC14.Speech.EntitySystems;
-
-namespace Content.Server.RMC14.Speech.Components;
-
-[RegisterComponent]
-[Access(typeof(ScottishAccentSystem))]
-public sealed partial class ScottishAccentComponent : Component
-{ }
