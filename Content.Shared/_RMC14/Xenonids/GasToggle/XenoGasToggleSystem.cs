55a7973ddb9d9130fe7b434f18d8ad898ec9f7bc
Verm <vermidiathesnake@gmail.com>
2024-08-13 18:45:31 -0500

    Boiler neurotoxin

---
+++ Content.Shared/_RMC14/Xenonids/GasToggle/XenoGasToggleSystem.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.Actions;
+using Robust.Shared.Timing;
+
+namespace Content.Shared._RMC14.Xenonids.GasToggle;
+
+public sealed class XenoGasToggleSystem : EntitySystem
+{
+    [Dependency] private readonly IGameTiming _timing = default!;
+
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<XenoGasToggleComponent, XenoGasToggleActionEvent>(OnToggleType);
+    }
+
+    private void OnToggleType(Entity<XenoGasToggleComponent> xeno, ref XenoGasToggleActionEvent args)
+    {
+        if (!_timing.IsFirstTimePredicted)
+            return;
+        xeno.Comp.IsNeurotoxin = !xeno.Comp.IsNeurotoxin;
+        _actions.SetToggled(args.Action, xeno.Comp.IsNeurotoxin);
+        Dirty(xeno);
+    }
+}
---
+++ Content.Shared/_RMC14/Xenonids/Neurotoxin/NeurotoxinComponent.cs
@@ -0,0 +1,76 @@
+﻿using Content.Shared.Chat.Prototypes;
+using Content.Shared.Damage;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RMC14.Xenonids.Neurotoxin;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class NeurotoxinComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public float NeurotoxinAmount = 0;
+
+    [DataField, AutoNetworkedField]
+    public float DepletionPerSecond = 5;
+
+    [DataField, AutoNetworkedField]
+    public float StaminaDamagePerSecond = 35;
+
+    [DataField, AutoNetworkedField]
+    public float DizzyStrength = 1.2f;
+
+    [DataField, AutoNetworkedField]
+    public float DizzyStrengthOnStumble = 5.5f;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan LastMessage;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan TimeBetweenMessages = TimeSpan.FromSeconds(2);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan AccentTime = TimeSpan.FromSeconds(10);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan JitterTime = TimeSpan.FromSeconds(15);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan StumbleJitterTime = TimeSpan.FromSeconds(25);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan LastStumbleTime;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan SecondsToBlindByOne = TimeSpan.FromSeconds(3);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan MinimumDelayBetweenEvents = TimeSpan.FromSeconds(1);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan LastAccentTime;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan LastBlindTime;
+
+    [DataField, AutoNetworkedField]
+    public DamageSpecifier ToxinDamage = new();
+
+    [DataField, AutoNetworkedField]
+    public DamageSpecifier OxygenDamage = new();
+
+    [DataField, AutoNetworkedField]
+    public DamageSpecifier CoughDamage = new();
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan DazeLength = TimeSpan.FromSeconds(1);
+
+    [DataField, AutoNetworkedField]
+    public ProtoId<EmotePrototype> CoughId = "Cough";
+
+    [DataField, AutoNetworkedField]
+    public ProtoId<EmotePrototype> PainId = "Scream"; // TODO custom pain emote
+
+	[DataField, AutoNetworkedField]
+	public TimeSpan BloodCoughDuration = TimeSpan.FromSeconds(1);
+}
