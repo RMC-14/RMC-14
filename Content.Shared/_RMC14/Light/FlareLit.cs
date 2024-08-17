764401ff600520ea0bb0fdafc7615ddba15f75e2
FungiFellow <151778459+FungiFellow@users.noreply.github.com>
2024-08-16 21:37:50 -0400

    Create FlareLit.cs

---
+++ Content.Shared/_RMC14/Light/FlareLit.cs
@@ -0,0 +1,17 @@
+using Content.Server.SharedExpendableLightComponent.Components;
+using Content.Server.ExpendableLightSystem;
+using Content.Shared.Tag;
+
+namespace Content.Shared._RMC14.Light;
+
+public sealed class FlareLitSystem : ExpendableLightSystem
+{
+  public override void Initialize()
+  {
+    if (ExpendableLightState.Lit) 
+    {
+      _tagSystem.AddTag(ent, "Trash");
+    }
+  }
+}
+/// ExpendableLightSystem Already marks Fading/Spent as Trash, this is just to add it to Lit Flares
