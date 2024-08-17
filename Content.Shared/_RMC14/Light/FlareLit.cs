0d1267534bd68d106ea07daf60fa247b6b30b163
FungiFellow <151778459+FungiFellow@users.noreply.github.com>
2024-08-17 00:44:46 -0400

    Update FlareLit.cs

--- Content.Shared/_RMC14/Light/FlareLit.cs
+++ Content.Shared/_RMC14/Light/FlareLit.cs
@@ -2,6 +2,7 @@ using Content.Server.ExpendableLightSystem;
 using Content.Shared.Light.Components;
 using Content.Server.Light.Components;
 using Content.Client.Light.Components;
+using Content.Shared.Light.Components.SharedExpendableLightComponent;
 using Content.Shared.Tag;
 
 namespace Content.Shared._RMC14.Light;
