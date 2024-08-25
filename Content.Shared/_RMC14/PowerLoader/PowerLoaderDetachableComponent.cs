afdd40ba2978b3ff738158a9811faf64ae3f6d41
blue3212 <luckywill339@gmail.com>
2024-08-25 16:57:50 -0400

    Removed Comment

--- Content.Shared/_RMC14/PowerLoader/PowerLoaderDetachableComponent.cs
+++ Content.Shared/_RMC14/PowerLoader/PowerLoaderDetachableComponent.cs
@@ -7,9 +7,6 @@ using System.Threading.Tasks;
 
 namespace Content.Shared._RMC14.PowerLoader;
 
-// This note is for myself and should not be in the final PR
-// CHECK THE ENTITY PROTOTYPES AND MAKE SURE THE DELAYS ARE CORRECT
-
 /// <summary>
 /// For entities that can be "detached" from another thing via interaction with said entity
 /// with an empty hand of the power loader
