f353437cc305217949f8ed735b55effddd2f1ccb
Verm <vermidiathesnake@gmail.com>
2024-08-25 22:45:14 -0500

    Finish the vial system

--- Content.Shared/_RMC14/Chemistry/RMCHyposprayComponent.cs
+++ Content.Shared/_RMC14/Chemistry/RMCHyposprayComponent.cs
@@ -1,5 +1,9 @@
-﻿using Content.Shared.FixedPoint;
+﻿using Content.Shared.DoAfter;
+using Content.Shared.FixedPoint;
+using Content.Shared._RMC14.Marines.Skills;
 using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+using Robust.Shared.Audio;
 
 namespace Content.Shared._RMC14.Chemistry;
 
@@ -13,22 +17,42 @@ public sealed partial class RMCHyposprayComponent : Component
     public string VialName = "beaker";
 
     [DataField, AutoNetworkedField]
-    public string BaseName = "hypospray";
-
-    [DataField, AutoNetworkedField]
     public string SolutionName = "vial";
 
-    //Syringe stuff below
+    [DataField]
+    public SoundSpecifier InjectSound = new SoundPathSpecifier("/Audio/_RMC14/Medical/hypospray.ogg");
+
+    // Syringe stuff below - due to vials assume this only injects
 
     [DataField]
-    public bool OnlyAffectsMobs;
+    public bool OnlyAffectsMobs = true;
 
     [DataField]
-    public FixedPoint2 MinimumTransferAmount = FixedPoint2.New(5);
-
-    [DataField]
-    public FixedPoint2 MaximumTransferAmount = FixedPoint2.New(30);
+    public FixedPoint2[] TransferAmounts = [FixedPoint2.New(3), FixedPoint2.New(5), FixedPoint2.New(10), FixedPoint2.New(15), FixedPoint2.New(30)];
 
     [DataField, AutoNetworkedField]
     public FixedPoint2 TransferAmount = FixedPoint2.New(5);
+
+    // Doafter stuff
+
+    [DataField]
+    public TimeSpan TacticalReloadTime = TimeSpan.FromSeconds(1.25);
+
+    [DataField, AutoNetworkedField]
+    public SkillWhitelist TacticalSkills;
+
+    [DataField]
+    public bool NeedHand = true;
+
+    [DataField]
+    public bool BreakOnHandChange = true;
+
+    [DataField]
+    public float MovementThreshold = 0.1f;
 }
+
+[Serializable, NetSerializable]
+public sealed partial class TacticalReloadHyposprayDoAfterEvent : SimpleDoAfterEvent
+{
+}
+
--- Content.Shared/_RMC14/Chemistry/RMCSharedHypospraySystem.cs
+++ Content.Shared/_RMC14/Chemistry/RMCSharedHypospraySystem.cs
@@ -1,9 +1,23 @@
 ﻿using Content.Shared.Chemistry;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.DoAfter;
+using Content.Shared.FixedPoint;
+using Content.Shared.Popups;
+using Content.Shared.Verbs;
+using Content.Shared._RMC14.Marines.Skills;
+using Content.Shared._RMC14.Medical.Refill;
+using Content.Shared.Containers.ItemSlots;
+using Content.Shared.Interaction;
+using Content.Shared.Interaction.Events;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Timing;
+using Content.Shared.Whitelist;
 using Robust.Shared.Containers;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Network;
+using Content.Shared.Chemistry.Components.SolutionManager;
+using Content.Shared.Examine;
 
 
 namespace Content.Shared._RMC14.Chemistry;
@@ -15,11 +29,72 @@ public abstract class RMCSharedHypospraySystem : EntitySystem
     [Dependency] protected readonly IPrototypeManager _prototype = default!;
     [Dependency] protected readonly SharedSolutionContainerSystem _solution = default!;
     [Dependency] protected readonly INetManager _net = default!;
+    [Dependency] protected readonly SharedPopupSystem _popup = default!;
+    [Dependency] protected readonly SharedDoAfterSystem _doafter = default!;
+    [Dependency] protected readonly SkillsSystem _skills = default!;
+    [Dependency] protected readonly ItemSlotsSystem _slots = default!;
+    [Dependency] protected readonly EntityWhitelistSystem _whitelist = default!;
+    [Dependency] protected readonly UseDelaySystem _useDelay = default!;
     public override void Initialize()
     {
+        SubscribeLocalEvent<RMCHyposprayComponent, GetVerbsEvent<AlternativeVerb>>(AddSetTransferVerbs);
         SubscribeLocalEvent<RMCHyposprayComponent, ComponentStartup>(OnStartup);
         SubscribeLocalEvent<RMCHyposprayComponent, EntInsertedIntoContainerMessage>(OnInsert);
         SubscribeLocalEvent<RMCHyposprayComponent, EntRemovedFromContainerMessage>(OnRemove);
+        SubscribeLocalEvent<RMCHyposprayComponent, AfterInteractEvent>(OnInteractAfter);
+        SubscribeLocalEvent<RMCHyposprayComponent, UseInHandEvent>(OnUseInHand);
+        SubscribeLocalEvent<RMCHyposprayComponent, ExaminedEvent>(OnExamine);
+        SubscribeLocalEvent<RMCHyposprayComponent, RefilledSolutionEvent>(OnRefilled);
+
+    }
+
+    private void OnRefilled(Entity<RMCHyposprayComponent> ent, ref RefilledSolutionEvent args)
+    {
+        UpdateAppearance(ent);
+    }
+
+    private void OnExamine(Entity<RMCHyposprayComponent> ent, ref ExaminedEvent args)
+    {
+        if (!_container.TryGetContainer(ent, ent.Comp.SlotId, out var container) || container.ContainedEntities.Count == 0)
+            return;
+
+        var vial = container.ContainedEntities[0];
+
+        args.PushText(Loc.GetString("rmc-hypospray-loaded", ("vial", vial)));
+    }
+
+    private void AddSetTransferVerbs(Entity<RMCHyposprayComponent> entity, ref GetVerbsEvent<AlternativeVerb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract || args.Hands == null)
+            return;
+
+        var user = args.User;
+        var (_, component) = entity;
+
+        var priority = 0;
+
+        // Add specific transfer verbs according to the container's size
+        foreach (var amount in entity.Comp.TransferAmounts)
+        {
+            AlternativeVerb verb = new()
+            {
+                Text = Loc.GetString("comp-solution-transfer-verb-amount", ("amount", amount)),
+                Category = VerbCategory.SetTransferAmount,
+                Act = () =>
+                {
+                    component.TransferAmount = amount;
+                    _popup.PopupClient(Loc.GetString("comp-solution-transfer-set-amount", ("amount", amount)), user, user);
+                    Dirty(entity);
+                },
+
+                // we want to sort by size, not alphabetically by the verb text.
+                Priority = priority
+            };
+
+            priority -= 1;
+
+            args.Verbs.Add(verb);
+        }
     }
 
     private void OnStartup(Entity<RMCHyposprayComponent> ent, ref ComponentStartup args)
@@ -46,7 +121,103 @@ public abstract class RMCSharedHypospraySystem : EntitySystem
         UpdateAppearance(ent);
     }
 
-    private void UpdateAppearance(Entity<RMCHyposprayComponent> ent)
+    private void OnUseInHand(Entity<RMCHyposprayComponent> ent, ref UseInHandEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        int index = Array.IndexOf(ent.Comp.TransferAmounts, ent.Comp.TransferAmount) + 1;
+
+        if (index >= ent.Comp.TransferAmounts.Length)
+            index = 0;
+        ent.Comp.TransferAmount = ent.Comp.TransferAmounts[index];
+        _popup.PopupClient(Loc.GetString("rmc-hypospray-amount-change", ("amount", ent.Comp.TransferAmount)), args.User, args.User);
+        Dirty(ent);
+
+        args.Handled = true;
+    }
+
+    private void OnInteractAfter(Entity<RMCHyposprayComponent> ent, ref AfterInteractEvent args)
+    {
+
+        if (args.Handled || !args.CanReach)
+            return;
+
+        if (!_container.TryGetContainer(ent, ent.Comp.SlotId, out var container))
+            return;
+
+        if (args.Target == null)
+            return;
+
+        if (!TryComp<ItemSlotsComponent>(ent, out var slots))
+            return;
+
+        if (_slots.CanInsert(ent, args.Target.Value, args.User, slots.Slots[ent.Comp.SlotId], true))
+        {
+            args.Handled = true;
+            if (!_skills.HasSkills(args.User, ent.Comp.TacticalSkills))
+            {
+                _popup.PopupClient(Loc.GetString("rmc-hypospray-fail-tacreload"), args.Used, args.User);
+                return;
+            }
+            //Tactical reload
+            if (container.ContainedEntities.Count == 0)
+            {
+                _popup.PopupClient(Loc.GetString("rmc-hypospray-load-tacreload", ("hypo", ent)), args.Used, args.User);
+            }
+            else
+            {
+                if (!_slots.TryEjectToHands(ent, slots.Slots[ent.Comp.SlotId], args.User, true))
+                    return;
+                _popup.PopupClient(Loc.GetString("rmc-hypospray-swap-tacreload"), args.Used, args.User);
+            }
+
+            _doafter.TryStartDoAfter(new DoAfterArgs(EntityManager, args.User, ent.Comp.TacticalReloadTime, new TacticalReloadHyposprayDoAfterEvent(), ent, args.Target, ent)
+            {
+                BreakOnMove = true,
+                BreakOnWeightlessMove = false,
+                BreakOnDamage = true,
+                NeedHand = ent.Comp.NeedHand,
+                BreakOnHandChange = ent.Comp.BreakOnHandChange,
+                MovementThreshold = ent.Comp.MovementThreshold,
+            });
+            return;
+        }
+
+        if (container.ContainedEntities.Count == 0)
+        {
+            _popup.PopupClient(Loc.GetString("rmc-hypospray-no-vial"), ent, args.User);
+            args.Handled = true;
+            return;
+        }
+
+        var vial = container.ContainedEntities[0];
+
+        if (HasComp<InjectableSolutionComponent>(args.Target.Value) && (!ent.Comp.OnlyAffectsMobs || HasComp<MobStateComponent>(args.Target.Value)))
+        {
+            // Try to inject
+            args.Handled = true;
+            if (TryComp(ent, out UseDelayComponent? delayComp))
+            {
+                if (_useDelay.IsDelayed((ent, delayComp)))
+                    return;
+            }
+
+            var attemptEv = new AttemptHyposprayUseEvent(args.User, args.Target.Value, TimeSpan.Zero);
+
+            RaiseLocalEvent(ent, ref attemptEv);
+            var doAfter = new HyposprayDoAfterEvent();
+            var argsu = new DoAfterArgs(EntityManager, args.User, attemptEv.DoAfter, doAfter, ent, args.Target, ent)
+            {
+                BreakOnMove = true,
+                BreakOnHandChange = true,
+                NeedHand = true
+            };
+            _doafter.TryStartDoAfter(argsu);
+        }
+    }
+
+    protected void UpdateAppearance(Entity<RMCHyposprayComponent> ent)
     {
         if (!TryComp<AppearanceComponent>(ent, out var appearance))
             return;
