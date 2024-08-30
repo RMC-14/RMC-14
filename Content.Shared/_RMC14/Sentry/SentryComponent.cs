ed0f4a9959343ca981985563265e1500fee5db59
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2024-08-30 04:38:12 -0700

    Mortars HE/Flare and Sentry/Mini-Sentry (#3885)

---
+++ Content.Shared/_RMC14/Sentry/SentryComponent.cs
@@ -0,0 +1,66 @@
+﻿using Content.Shared._RMC14.Marines.Skills;
+using Content.Shared.Weapons.Ranged.Components;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._RMC14.Sentry;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SentrySystem))]
+public sealed partial class SentryComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public SentryMode Mode;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan DeployDelay = TimeSpan.FromSeconds(3);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan UndeployDelay = TimeSpan.FromSeconds(2);
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan MagazineDelay = TimeSpan.FromSeconds(7);
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId<SkillDefinitionComponent> Skill = "RMCSkillEngineer";
+
+    [DataField, AutoNetworkedField]
+    public int SkillLevel = 2;
+
+    [DataField, AutoNetworkedField]
+    public float[] SkillMagazineDelayMultipliers = [1f, 0.75f, 0.5f, 0.375f, 0.25f];
+
+    [DataField, AutoNetworkedField]
+    public SoundSpecifier? MagazineSwapSound = new SoundPathSpecifier("/Audio/_RMC14/Weapons/unload.ogg");
+
+    [DataField, AutoNetworkedField]
+    public string? DeployFixture = "sentry";
+
+    [DataField, AutoNetworkedField]
+    public Angle MaxDeviation = Angle.FromDegrees(75);
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId<BallisticAmmoProviderComponent>? StartingMagazine = "RMCMagazineSentry";
+
+    [DataField, AutoNetworkedField]
+    public string ContainerSlotId = "gun_magazine";
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId[]? Upgrades = ["RMCSentryMini"];
+}
+
+[Serializable, NetSerializable]
+public enum SentryMode
+{
+    Item,
+    Off,
+    On,
+}
+
+[Serializable, NetSerializable]
+public enum SentryLayers
+{
+    Layer,
+}
