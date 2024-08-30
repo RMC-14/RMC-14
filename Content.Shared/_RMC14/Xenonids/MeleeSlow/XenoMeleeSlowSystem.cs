35f4ad0f4f6485db4ff997460a8c0738773cfb25
Verm <vermidiathesnake@gmail.com>
2024-08-30 16:38:12 -0500

    done

--- Content.Shared/_RMC14/Xenonids/MeleeSlow/XenoMeleeSlowSystem.cs
+++ Content.Shared/_RMC14/Xenonids/MeleeSlow/XenoMeleeSlowSystem.cs
@@ -14,6 +14,7 @@ public sealed class XenoMeleeSlowSystem : EntitySystem
     [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly XenoSystem _xeno = default!;
+
     public override void Initialize()
     {
         SubscribeLocalEvent<XenoMeleeSlowComponent, MeleeHitEvent>(OnHit);
@@ -29,7 +30,7 @@ public sealed class XenoMeleeSlowSystem : EntitySystem
 
         foreach (var entity in args.HitEntities)
         {
-            if (!_xeno.CanHitLiving(entity))
+            if (!_xeno.CanHitLiving(xeno, entity))
                 return;
 
             if (xeno.Comp.RequiresKnockDown && !_standing.IsDown(entity))
