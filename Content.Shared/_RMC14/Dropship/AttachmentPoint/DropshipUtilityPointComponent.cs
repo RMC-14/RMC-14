e39227c170615a74616e87a4d114dac0d6b589da
blue3212 <luckywill339@gmail.com>
2024-09-11 11:02:19 -0400

    Remove unused variable "DirOffset"

--- Content.Shared/_RMC14/Dropship/AttachmentPoint/DropshipUtilityPointComponent.cs
+++ Content.Shared/_RMC14/Dropship/AttachmentPoint/DropshipUtilityPointComponent.cs
@@ -10,9 +10,6 @@ public sealed partial class DropshipUtilityPointComponent : Component
 {
     [DataField, AutoNetworkedField]
     public string UtilitySlotId = "rmc_dropship_utility_point_container_slot";
-
-    [DataField, AutoNetworkedField]
-    public string DirOffset;
 }
 
 [Serializable, NetSerializable]
