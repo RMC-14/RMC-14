629921ffeb74e5deb9647fec143a9e25451d3302
Mr. 27 <koolthunder019@gmail.com>
2024-09-13 16:48:00 -0400

    l

--- Content.Server/_RMC14/Xenonids/Egg/XenoEggRoleSystem.cs
+++ Content.Server/_RMC14/Xenonids/Egg/XenoEggRoleSystem.cs
@@ -1,51 +1,21 @@
-using Content.Shared._RMC14.Hands;
-using Content.Shared._RMC14.Marines;
-using Content.Shared._RMC14.Xenonids.Construction;
+using Content.Server.Ghost.Roles;
 using Content.Shared._RMC14.Xenonids.Egg;
-using Content.Shared._RMC14.Xenonids.Parasite;
-using Content.Shared._RMC14.Xenonids.Plasma;
-using Content.Shared._RMC14.Xenonids.Weeds;
-using Content.Shared.Actions;
-using Content.Shared.Buckle.Components;
-using Content.Shared.Database;
-using Content.Shared.DoAfter;
-using Content.Shared.FixedPoint;
 using Content.Shared.Ghost;
-using Content.Shared.Hands.Components;
-using Content.Shared._RMC14.Xenonids.Egg;
-using Content.Shared.Interaction;
-using Content.Shared.Item;
-using Content.Shared.Maps;
-using Content.Shared.Mind;
-using Content.Shared.Mobs;
-using Content.Shared.Mobs.Components;
-using Content.Shared.Mobs.Systems;
 using Content.Shared.Popups;
-using Content.Shared.StepTrigger.Components;
-using Content.Shared.StepTrigger.Systems;
-using Content.Shared.Stunnable;
-using Content.Shared.Tag;
-using Content.Shared.Verbs;
-using Robust.Shared.Map.Components;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Random;
-using Robust.Shared.Serialization;
 using Robust.Shared.Timing;
-using Content.Server.Ghost.Roles;
-using Content.Shared._RMC14.Admin;
 
 namespace Content.Server._RMC14.Xenonids.Egg;
 
-public sealed class XenonidEggSystem : EntitySystem
+public sealed class XenoEggRoleSystem : EntitySystem
 {
     [Dependency] private readonly ActorSystem _actor = default!;
     [Dependency] private readonly XenoEggSystem _eggSystem = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly IGameTiming _gameTiming = default!;
-    [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly GhostRoleSystem _ghostRole = default!;
+    [Dependency] private readonly INetManager _net = default!;
     [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
 
     public override void Initialize()
@@ -69,7 +39,7 @@ public sealed class XenonidEggSystem : EntitySystem
             return;
 
         // Must have been dead for 3 minutes
-        if (_gameTiming.RealTime.Subtract(ghostComp.TimeOfDeath) < TimeSpan.FromMinutes(3))
+        if (_gameTiming.CurTime.Subtract(ghostComp.TimeOfDeath) < TimeSpan.FromMinutes(3))
         {
             _popup.PopupEntity(Loc.GetString("rmc-xeno-egg-ghost-need-time"), user, user, PopupType.MediumCaution);
             return;
