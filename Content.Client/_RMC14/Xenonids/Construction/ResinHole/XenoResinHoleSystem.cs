00ab9416e044ab73341ed98d3a1f9ee33457140f
blue3212 <luckywill339@gmail.com>
2024-09-17 14:07:38 -0400

    Remove resin hole interaction outline component for all

--- Content.Client/_RMC14/Xenonids/Construction/ResinHole/XenoResinHoleSystem.cs
+++ Content.Client/_RMC14/Xenonids/Construction/ResinHole/XenoResinHoleSystem.cs
@@ -1,5 +1,10 @@
 using Content.Client.Interactable.Components;
+using Content.Client.Players;
+using Content.Shared._RMC14.Marines;
 using Content.Shared._RMC14.Xenonids.Construction.ResinHole;
+using Robust.Client.Player;
+using Robust.Shared.Network;
+using Robust.Shared.Player;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +15,5 @@ namespace Content.Client._RMC14.Xenonids.Construction.ResinHole;
 
 public sealed partial class XenoResinHoleSystem : SharedXenoResinHoleSystem
 {
-    public override void Initialize()
-    {
-        base.Initialize();
 
-        SubscribeNetworkEvent<HideResinHoleOutlineMessage>(HideResinHoleOutline);
-    }
-
-    private void HideResinHoleOutline(HideResinHoleOutlineMessage msg)
-    {
-        var resinHole = GetEntity(msg.ResinHole);
-        RemCompDeferred<InteractionOutlineComponent> (resinHole);
-    }
 }
--- Content.Server/_RMC14/Xenonids/Construction/ResinHole/XenoResinHoleSystem.cs
+++ Content.Server/_RMC14/Xenonids/Construction/ResinHole/XenoResinHoleSystem.cs
@@ -12,20 +12,4 @@ namespace Content.Server._RMC14.Xenonids.Construction.ResinHole;
 
 public sealed partial class XenoResinHoleSystem : SharedXenoResinHoleSystem
 {
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<XenoResinHoleComponent, ViewSubscriberAddedEvent>(OnEnterPVS);
-    }
-
-    private void OnEnterPVS(Entity<XenoResinHoleComponent> resinHole, ref ViewSubscriberAddedEvent args)
-    {
-        if (!HasComp<MarineComponent>(args.Subscriber.AttachedEntity))
-        {
-            return;
-        }
-        var ev = new HideResinHoleOutlineMessage(GetNetEntity(resinHole.Owner));
-        RaiseNetworkEvent(ev, args.Subscriber);
-    }
 }
