8e428c5fc7c90a87b2eded38280d860cc106167e
Mr. 27 <koolthunder019@gmail.com>
2024-09-29 09:22:50 -0400

    inital

---
+++ Content.Shared/_RMC14/Weapons/Ranged/RMCFireGroupComponent.cs
@@ -0,0 +1,18 @@
+﻿using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Weapons.Ranged;
+
+/// <summary>
+/// Fire group component which prevents things like shotgun juggling.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SharedFireGroupSystem))]
+public sealed partial class RMCFireGroupComponent : Component
+{
+    /// <summary>
+    /// The fire group of the item
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public string Group = string.Empty;
+}
---
+++ Content.Shared/_RMC14/Weapons/Ranged/SharedFireGroupSystem.cs
@@ -0,0 +1,38 @@
+﻿using Content.Shared.Hands.EntitySystems;
+using Content.Shared.Timing;
+using Content.Shared.Weapons.Ranged.Systems;
+
+namespace Content.Shared._RMC14.Weapons.Ranged;
+
+public sealed class SharedFireGroupSystem : EntitySystem
+{
+    [Dependency] private readonly UseDelaySystem _delay = default!;
+    [Dependency] private readonly SharedHandsSystem _hands = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<RMCFireGroupComponent, GunShotEvent>(OnGunShot);
+    }
+
+    private void OnGunShot(Entity<RMCFireGroupComponent> ent, ref GunShotEvent args)
+    {
+        var weapon = ent.Owner;
+        var comp = ent.Comp;
+        var user = args.User;
+
+        foreach (var item in _hands.EnumerateHeld(user))
+        {
+            if (TryComp<RMCFireGroupComponent>(item, out var fireGroup))
+            {
+                if (item == weapon)
+                    continue;
+
+                if (fireGroup.Group != comp.Group)
+                    continue;
+
+                if (TryComp(item, out UseDelayComponent? useDelay))
+                    _delay.TryResetDelay((item, useDelay));
+            }
+        }
+    }
+}
