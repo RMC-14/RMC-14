27b7c7ffee12ba9552256b6d64b74044fe26aea1
DrSmugleaf <drsmugleaf@gmail.com>
2024-09-29 18:57:51 -0700

    Fix emote cooldown for auto emotes

--- Content.Shared/_RMC14/Emote/SharedRMCEmoteSystem.cs
+++ Content.Shared/_RMC14/Emote/SharedRMCEmoteSystem.cs
@@ -1,4 +1,6 @@
-﻿using Content.Shared.Chat.Prototypes;
+﻿using Content.Shared._RMC14.CCVar;
+using Content.Shared.Chat.Prototypes;
+using Robust.Shared.Configuration;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 
@@ -6,7 +8,15 @@ namespace Content.Shared._RMC14.Emote;
 
 public abstract class SharedRMCEmoteSystem : EntitySystem
 {
+    [Dependency] private readonly IConfigurationManager _config = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
+
+    private TimeSpan _emoteCooldown;
+
+    public override void Initialize()
+    {
+        Subs.CVar(_config, RMCCVars.RMCEmoteCooldownSeconds, v => _emoteCooldown = TimeSpan.FromSeconds(v), true);
+    }
 
     public virtual void TryEmoteWithChat(
         EntityUid source,
@@ -19,12 +29,18 @@ public abstract class SharedRMCEmoteSystem : EntitySystem
     {
     }
 
-    public bool CanEmote(Entity<EmoteCooldownComponent?> cooldown)
+    public bool TryEmote(Entity<EmoteCooldownComponent?> cooldown)
     {
         if (!Resolve(cooldown, ref cooldown.Comp, false))
             return true;
 
-        return _timing.CurTime >= cooldown.Comp.NextEmote;
+        var time = _timing.CurTime;
+        if (time < cooldown.Comp.NextEmote)
+            return false;
+
+        cooldown.Comp.NextEmote = time + _emoteCooldown;
+        Dirty(cooldown);
+        return true;
     }
 
     public void ResetCooldown(Entity<EmoteCooldownComponent?> cooldown)
@@ -32,7 +48,7 @@ public abstract class SharedRMCEmoteSystem : EntitySystem
         if (!Resolve(cooldown, ref cooldown.Comp, false))
             return;
 
-        cooldown.Comp.NextEmote = _timing.CurTime;
+        cooldown.Comp.NextEmote = _timing.CurTime + _emoteCooldown;
         Dirty(cooldown);
     }
 }
