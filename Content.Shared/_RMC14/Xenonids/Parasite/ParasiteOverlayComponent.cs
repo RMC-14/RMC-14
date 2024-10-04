c83ce51f6d90ba55379700210283ddfe87a350c3
Verm <vermidiathesnake@gmail.com>
2024-10-03 23:32:08 -0500

    Sprites + some logic

---
+++ Content.Shared/_RMC14/Xenonids/Parasite/ParasiteOverlayComponent.cs
@@ -0,0 +1,34 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._RMC14.Xenonids.Parasite;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class ParasiteOverlayComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public int NumPositions = 4;
+
+    //Should equal num positions
+    [DataField, AutoNetworkedField]
+    public bool[] VisiblePostitions = [false, false, false, false];
+}
+
+[Serializable, NetSerializable]
+public enum ParasiteOverlayVisuals
+{
+    Resting,
+    Downed,
+    State
+}
+
+[Serializable, NetSerializable]
+public enum ParasiteOverlayLayers
+{
+    RightArm,
+    Head,
+    LeftArm,
+    Back
+}
+
+public sealed class ParasiteOverlayFlags { }
---
+++ Content.Shared/_RMC14/Xenonids/Parasite/RMCXenoParasiteOverlayVisualsSystem.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Content.Shared._RMC14.Xenonids.Parasite;
+
+public sealed class RMCXenoParasiteOverlayVisualsSystem : EntitySystem
+{
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+}
