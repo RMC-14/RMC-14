16bd2b2b581e2c81555e1077fa14c1325baf736f
crystalHex <crystalHex@proton.me>
2024-10-04 09:18:45 +0200

    Remove unnecessary specifiers from base components

--- Content.Shared/_RMC14/Xenonids/Fruit/Components/XenoFruitBaseComponent.cs
+++ Content.Shared/_RMC14/Xenonids/Fruit/Components/XenoFruitBaseComponent.cs
@@ -4,54 +4,49 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 namespace Content.Shared._RMC14.Xenonids.Fruit.Components;
 
 // Base component for fruit effects
-[RegisterComponent, NetworkedComponent]
 [Access(typeof(SharedXenoFruitSystem))]
 public abstract partial class XenoFruitBaseComponent : Component { }
 
 // Base component for fruit effects with a fixed duration
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 [Access(typeof(SharedXenoFruitSystem))]
 public abstract partial class XenoFruitDurationComponent : XenoFruitBaseComponent
 {
     // Duration of effect
-    [DataField, AutoNetworkedField]
+    [DataField]
     public TimeSpan Duration = TimeSpan.FromSeconds(0);
 }
 
 // Base component for fruit effects with a tick-based duration
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 [Access(typeof(SharedXenoFruitSystem))]
 public abstract partial class XenoFruitTickBasedComponent : XenoFruitBaseComponent
 {
     // Time between ticks
-    [DataField, AutoNetworkedField]
+    [DataField]
     public TimeSpan TickPeriod = TimeSpan.FromSeconds(1);
 
     // Total number of ticks
-    [DataField, AutoNetworkedField]
+    [DataField]
     public int TickCount = 0;
 }
 
 
 // Base classes for components applied to xenos under the effects of fruit
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true), AutoGenerateComponentPause]
 [Access(typeof(SharedXenoFruitSystem))]
 public abstract partial class XenoFruitEffectDurationComponent : XenoFruitDurationComponent
 {
     // Effect end time
-    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
     public TimeSpan? EndAt;
 }
 
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true), AutoGenerateComponentPause]
 [Access(typeof(SharedXenoFruitSystem))]
 public abstract partial class XenoFruitEffectTickBasedComponent : XenoFruitTickBasedComponent
 {
     // How many ticks does this effect have left?
-    [DataField, AutoNetworkedField]
+    [DataField]
     public int? TicksLeft;
 
     // Time to apply next regen amount at
-    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField, AutoPausedField]
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer))]
     public TimeSpan? NextTickAt;
 }
