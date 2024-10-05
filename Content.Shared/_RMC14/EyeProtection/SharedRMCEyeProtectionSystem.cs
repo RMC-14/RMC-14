12a0ef377719f90f743f2f866b653a85cf73418f
crystalHex <crystalHex@proton.me>
2024-10-05 22:11:44 +0200

    Temp stash

--- Content.Shared/_RMC14/EyeProtection/SharedRMCEyeProtectionSystem.cs
+++ Content.Shared/_RMC14/EyeProtection/SharedRMCEyeProtectionSystem.cs
@@ -33,75 +33,17 @@ namespace Content.Shared._RMC14.EyeProtection
         public override void Initialize()
         {
             base.Initialize();
-            SubscribeLocalEvent<RMCRequiresEyeProtectionComponent, ToolUseAttemptEvent>(OnUseAttempt);
-            SubscribeLocalEvent<RMCRequiresEyeProtectionComponent, ItemToggledEvent>(OnWelderToggled);
 
-            SubscribeLocalEvent<RMCEyeProtectionItemComponent, GetEyeProtectionEvent>(OnGetProtection);
-            SubscribeLocalEvent<RMCEyeProtectionItemComponent, InventoryRelayedEvent<GetEyeProtectionEvent>>(OnGetRelayedProtection);
-
-            //SubscribeLocalEvent<RMCEyeProtectionItemComponent, GetItemActionsEvent>(OnEyeProtectionItemGetActions);
             SubscribeLocalEvent<RMCEyeProtectionItemComponent, ToggleActionEvent>(OnEyeProtectionItemToggle);
+            SubscribeLocalEvent<RMCEyeProtectionItemComponent, ToggleClothingCheckEvent>(OnEyeProtectionItemToggleCheck);
+
+
             SubscribeLocalEvent<RMCEyeProtectionItemComponent, GotEquippedEvent>(OnEyeProtectionItemGotEquipped);
             SubscribeLocalEvent<RMCEyeProtectionItemComponent, GotUnequippedEvent>(OnEyeProtectionItemGotUnequipped);
             SubscribeLocalEvent<RMCEyeProtectionItemComponent, ActionRemovedEvent>(OnEyeProtectionItemActionRemoved);
             SubscribeLocalEvent<RMCEyeProtectionItemComponent, ComponentRemove>(OnEyeProtectionItemRemove);
             SubscribeLocalEvent<RMCEyeProtectionItemComponent, EntityTerminatingEvent>(OnEyeProtectionItemTerminating);
         }
-
-        private void OnGetRelayedProtection(EntityUid uid, RMCEyeProtectionItemComponent component,
-            InventoryRelayedEvent<GetEyeProtectionEvent> args)
-        {
-            OnGetProtection(uid, component, args.Args);
-        }
-
-        private void OnGetProtection(EntityUid uid, RMCEyeProtectionItemComponent component, GetEyeProtectionEvent args)
-        {
-            if (component.Toggled)
-                args.Protection += component.ProtectionTime;
-        }
-
-        private void OnUseAttempt(EntityUid uid, RMCRequiresEyeProtectionComponent component, ToolUseAttemptEvent args)
-        {
-            if (!component.Toggled)
-                return;
-
-            if (!TryComp<BlindableComponent>(args.User, out var blindable) || blindable.IsBlind)
-                return;
-
-            var ev = new GetEyeProtectionEvent();
-            RaiseLocalEvent(args.User, ev);
-
-            var time = (float) (component.StatusEffectTime - ev.Protection).TotalSeconds;
-            if (time <= 0)
-                return;
-
-            // Add permanent eye damage if they had zero protection, also somewhat scale their temporary blindness by
-            // how much damage they already accumulated.
-            _blindingSystem.AdjustEyeDamage((args.User, blindable), 1);
-            var statusTimeSpan = TimeSpan.FromSeconds(time * MathF.Sqrt(blindable.EyeDamage));
-            _statusEffectsSystem.TryAddStatusEffect(args.User, TemporaryBlindnessSystem.BlindingStatusEffect,
-                statusTimeSpan, false, TemporaryBlindnessSystem.BlindingStatusEffect);
-        }
-
-        private void OnWelderToggled(EntityUid uid, RMCRequiresEyeProtectionComponent component, ItemToggledEvent args)
-        {
-            component.Toggled = args.Activated;
-        }
-
-        /*
-        private void OnEyeProtectionItemGetActions(Entity<RMCEyeProtectionItemComponent> ent, ref GetItemActionsEvent args)
-        {
-            if (args.InHands || !ent.Comp.Toggleable)
-                return;
-
-            // Item not in a position for protecting eyes
-            if (ent.Comp.Slots != args.SlotFlags)
-                return;
-
-            args.AddAction(ref ent.Comp.Action, ent.Comp.ActionId);
-            Dirty(ent);
-        }
-        */
 
         private void OnEyeProtectionItemToggle(Entity<RMCEyeProtectionItemComponent> ent, ref ToggleActionEvent args)
         {
@@ -124,8 +66,6 @@ namespace Content.Shared._RMC14.EyeProtection
             // Display correct sprite, if applicable
             if (ent.Comp.RaisedEquippedPrefix != null)
                 _clothingSystem.SetEquippedPrefix(ent.Owner, ent.Comp.RaisedEquippedPrefix, clothingComp);
-
-            //EnableEyeProtectionItem(ent, args.Equipee);
         }
 
         private void OnEyeProtectionItemGotUnequipped(Entity<RMCEyeProtectionItemComponent> ent, ref GotUnequippedEvent args)
@@ -162,8 +102,7 @@ namespace Content.Shared._RMC14.EyeProtection
             // Check if already enabled
             if (TryComp(user, out RMCSightRestrictionComponent? eyeProt))
             {
-                return; // To prevent multiple items being enabled at once
-                //RemComp<RMCSightRestrictionComponent>(user);
+                return;
             }
 
             item.Comp.User = user;
@@ -197,7 +136,7 @@ namespace Content.Shared._RMC14.EyeProtection
 
             if (!_timing.ApplyingState)
             {
-                eyeProt = EnsureComp<RMCSightRestrictionComponent>(user); // This should happen as late as possible
+                eyeProt = EnsureComp<RMCSightRestrictionComponent>(user);
                 Dirty(user, eyeProt);
             }
         }
