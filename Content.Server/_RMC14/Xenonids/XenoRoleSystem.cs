6e6172fd08e7ff7ff522a13fd440a13fad1b05dd
deltanedas <@deltanedas:kde.org>
2024-10-06 01:57:43 +0100

    network hive entity to its members

--- Content.Server/_RMC14/Xenonids/XenoRoleSystem.cs
+++ Content.Server/_RMC14/Xenonids/XenoRoleSystem.cs
@@ -2,7 +2,9 @@
 using Content.Server.Players.PlayTimeTracking;
 using Content.Server.Roles;
 using Content.Shared._RMC14.Xenonids;
+using Content.Shared._RMC14.Xenonids.Hive;
 using Content.Shared.Roles.Jobs;
+using Robust.Server.GameStates;
 using Robust.Shared.Player;
 
 namespace Content.Server._RMC14.Xenonids;
@@ -11,11 +13,17 @@ public sealed class XenoRoleSystem : EntitySystem
 {
     [Dependency] private readonly MindSystem _mind = default!;
     [Dependency] private readonly PlayTimeTrackingSystem _playTime = default!;
+    [Dependency] private readonly PvsOverrideSystem _pvsOverride = default!;
     [Dependency] private readonly RoleSystem _role = default!;
+    [Dependency] private readonly SharedXenoHiveSystem _hive = default!;
 
     public override void Initialize()
     {
+        base.Initialize();
+
         SubscribeLocalEvent<XenoComponent, PlayerAttachedEvent>(OnPlayerAttached);
+        SubscribeLocalEvent<XenoComponent, PlayerDetachedEvent>(OnPlayerDetached);
+        SubscribeLocalEvent<ActorComponent, HiveChangedEvent>(OnHiveChanged);
     }
 
     private void OnPlayerAttached(Entity<XenoComponent> xeno, ref PlayerAttachedEvent args)
@@ -23,8 +31,29 @@ public sealed class XenoRoleSystem : EntitySystem
         if (!_mind.TryGetMind(args.Player.UserId, out var mind))
             return;
 
+        if (_hive.GetHive(xeno.Owner) is {} hive)
+            _pvsOverride.AddForceSend(hive, args.Player);
+
         _role.MindTryRemoveRole<JobComponent>(mind.Value);
         _role.MindAddRole(mind.Value, new JobComponent { Prototype = xeno.Comp.Role });
         _playTime.PlayerRolesChanged(args.Player);
+    }
+
+    private void OnPlayerDetached(Entity<XenoComponent> xeno, ref PlayerDetachedEvent args)
+    {
+        if (_hive.GetHive(xeno.Owner) is {} hive)
+            _pvsOverride.RemoveForceSend(hive, args.Player);
+    }
+
+    private void OnHiveChanged(Entity<ActorComponent> ent, ref HiveChangedEvent args)
+    {
+        if (ent.Comp.PlayerSession is not {} session)
+            return;
+
+        if (args.OldHive is {} oldHive)
+            _pvsOverride.RemoveForceSend(oldHive, session);
+
+        if (args.Hive is {} newHive)
+            _pvsOverride.AddForceSend(newHive, session);
     }
 }
