d7d62cc2e19d5fe6f2abd0c0b27cf28ded08af5b
Mr. 27 <koolthunder019@gmail.com>
2024-10-14 15:37:41 -0400

    Update SharedXenoParasiteSystem.cs

--- Content.Shared/_RMC14/Xenonids/Parasite/SharedXenoParasiteSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Parasite/SharedXenoParasiteSystem.cs
@@ -688,12 +688,12 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
             shakeFilter.RemoveWhereAttachedEntity(HasComp<XenoComponent>); // not visible to xenos
 
             // Force infection shakes even while dead, bigger popup
-            _popup.PopupEntity(Loc.GetString("rmc-xeno-infection-burst-now-victim"), victim, victim, PopupType.MediumCaution);
+            _popup.PopupClient(Loc.GetString("rmc-xeno-infection-burst-now-victim"), victim, victim, PopupType.MediumCaution);
             _popup.PopupEntity(Loc.GetString("rmc-xeno-infection-burst-soon", ("victim", victim)), victim, shakeFilter, true, PopupType.LargeCaution);
-            _jitter.DoJitter(victim, comp.JitterTime, true, 20f, 2f, true); // violent jitter
+            _jitter.DoJitter(victim, comp.JitterTime / 1.5, true, 14f, 5f, true); // violent jitter
 
             var messageLarva = Loc.GetString("rmc-xeno-infection-burst-now-xeno", ("victim", Identity.Entity(victim, EntityManager)));
-            _popup.PopupEntity(messageLarva, spawnedLarva, spawnedLarva, PopupType.MediumCaution);
+            _popup.PopupClient(messageLarva, spawnedLarva, spawnedLarva, PopupType.MediumCaution);
         }
     }
 
