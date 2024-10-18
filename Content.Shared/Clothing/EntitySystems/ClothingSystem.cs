bef310457d3a12a97a59983b63b7b7a1ca7efca6
Mr. 27 <koolthunder019@gmail.com>
2024-10-18 15:56:50 -0400

    final fix

--- Content.Shared/Clothing/EntitySystems/ClothingSystem.cs
+++ Content.Shared/Clothing/EntitySystems/ClothingSystem.cs
@@ -101,7 +101,7 @@ public abstract class ClothingSystem : EntitySystem
                 {
                     if (comp.Slots.Contains(layer))
                     {
-                        if (TryComp(item, out ClothingComponent? clothing) && clothing.Slots == slot.SlotFlags)
+                        if (TryComp(item, out ClothingComponent? clothing) && clothing.InSlot == slot.Name)
                         {
                             //Checks for mask toggling. TODO: Make a generic system for this
                             if (comp.HideOnToggle && TryComp(item, out MaskComponent? mask))
