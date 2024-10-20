75386d3fca8b6a3e665cc3064e3bc109eb4bb735
crystalHex <crystalHex@proton.me>
2024-10-20 19:49:50 +0200

    Remove unused enum value

--- Content.Shared/_RMC14/Inventory/CMHolsterVisuals.cs
+++ Content.Shared/_RMC14/Inventory/CMHolsterVisuals.cs
@@ -5,7 +5,6 @@ namespace Content.Shared._RMC14.Inventory;
 [Serializable, NetSerializable]
 public enum CMHolsterVisuals
 {
-    Empty,
-    Medium, // TODO: account for the gunslinger belt
+    Empty, // TODO: account for the gunslinger belt
     Full,
 }
