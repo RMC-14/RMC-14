4ba99b5e4568e870327bf89d2275b7d8028d94ab
Verm <vermidiathesnake@gmail.com>
2024-10-28 00:34:00 -0500

    start

---
+++ Content.Shared/_RMC14/Marines/Dogtags/TakeableTagsComponent.cs
@@ -0,0 +1,8 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Marines.Dogtags;
+
+[RegisterComponent, AutoNetworkedField]
+public sealed partial class TakeableTagsComponent : Component
+{
+}
