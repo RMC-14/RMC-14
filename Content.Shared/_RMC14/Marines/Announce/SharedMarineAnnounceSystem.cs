c8820aaac265c6a0d6ef357168822ce9eefc5f81
DrSmugleaf <drsmugleaf@gmail.com>
2024-11-02 15:50:44 -0700

    Fix telephone cord drawing, not being able to open rto pack storage, comms towers being acidable, telephone ui overflowing, groundside console not opening while on cooldown,

--- Content.Shared/_RMC14/Marines/Announce/SharedMarineAnnounceSystem.cs
+++ Content.Shared/_RMC14/Marines/Announce/SharedMarineAnnounceSystem.cs
@@ -4,10 +4,8 @@ using Content.Shared._RMC14.Overwatch;
 using Content.Shared._RMC14.TacticalMap;
 using Content.Shared.Popups;
 using Content.Shared.Radio;
-using Content.Shared.UserInterface;
 using Robust.Shared.Audio;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Timing;
 
 namespace Content.Shared._RMC14.Marines.Announce;
 
@@ -15,35 +13,16 @@ public abstract class SharedMarineAnnounceSystem : EntitySystem
 {
     [Dependency] private readonly SharedPopupSystem _popup = default!;
     [Dependency] private readonly SkillsSystem _skills = default!;
-    [Dependency] private readonly IGameTiming _timing = default!;
     [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
 
     public override void Initialize()
     {
-        SubscribeLocalEvent<MarineCommunicationsComputerComponent, ActivatableUIOpenAttemptEvent>(OnMarineCommunicationsComputerOpenAttempt);
-
         Subs.BuiEvents<MarineCommunicationsComputerComponent>(MarineCommunicationsComputerUI.Key,
             subs =>
             {
                 subs.Event<MarineCommunicationsOpenMapMsg>(OnMarineCommunicationsOpenMapMsg);
                 subs.Event<MarineCommunicationsOverwatchMsg>(OnMarineCommunicationsOverwatchMsg);
             });
-    }
-
-    private void OnMarineCommunicationsComputerOpenAttempt(
-        Entity<MarineCommunicationsComputerComponent> ent,
-        ref ActivatableUIOpenAttemptEvent args
-        )
-    {
-        if (args.Cancelled)
-            return;
-
-        if (_timing.CurTime < ent.Comp.LastAnnouncement + ent.Comp.Cooldown)
-        {
-            // TODO RMC14 localize
-            _popup.PopupClient($"Please allow at least {(int) ent.Comp.Cooldown.TotalSeconds} seconds to pass between announcements", args.User);
-            args.Cancel();
-        }
     }
 
     private void OnMarineCommunicationsOpenMapMsg(Entity<MarineCommunicationsComputerComponent> ent, ref MarineCommunicationsOpenMapMsg args)
