57afe32f0b2f56d27af1b43f499f9a6cbf35cbff
Verm <vermidiathesnake@gmail.com>
2024-11-02 03:29:54 -0500

    Add taking tags, joining tags, and putting them on the slab

---
+++ Content.Shared/_RMC14/Marines/Dogtags/RMCMemorialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Marines.Dogtags;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+
+//Similiar to info tags comp but only stores names (and ghosts? TODO)
+public sealed partial class RMCMemorialComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public List<string> Names = new();
+}
--- Content.Shared/_RMC14/Marines/Dogtags/TakeableTagsComponent.cs
+++ Content.Shared/_RMC14/Marines/Dogtags/TakeableTagsComponent.cs
@@ -1,4 +1,6 @@
 ﻿using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._RMC14.Marines.Dogtags;
 
@@ -7,4 +9,13 @@ public sealed partial class TakeableTagsComponent : Component
 {
     [DataField, AutoNetworkedField]
     public bool TagsTaken = false;
+
+    [DataField]
+    public EntProtoId InfoTag = "RMCInformationDogtag";
+}
+
+[Serializable, NetSerializable]
+public enum DogtagVisuals
+{
+    Taken,
 }
