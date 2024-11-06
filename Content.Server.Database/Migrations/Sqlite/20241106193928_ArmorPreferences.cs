3c6d00751e3e7d8251e701a03a1de0062ca2bc14
Mr. 27 <koolthunder019@gmail.com>
2024-11-06 14:42:23 -0500

    fix

---
+++ Content.Server.Database/Migrations/Sqlite/20241106193928_ArmorPreferences.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class ArmorPreferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "armor_preference",
+                table: "profile",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "armor_preference",
+                table: "profile",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
--- Content.Server.Database/add-migration.ps1
+++ Content.Server.Database/add-migration.ps1
@@ -1,12 +1,5 @@
 ﻿#!/usr/bin/env pwsh
 
-param([String]$name)
 
-if ($name -eq "")
-{
-    Write-Error "must specify migration name"
-    exit
-}
-
-dotnet ef migrations add --context SqliteServerDbContext -o Migrations/Sqlite $name
-dotnet ef migrations add --context PostgresServerDbContext -o Migrations/Postgres $name
+dotnet ef migrations add --context SqliteServerDbContext -o Migrations/Sqlite ArmorPreferences
+dotnet ef migrations add --context PostgresServerDbContext -o Migrations/Postgres ArmorPreferences
