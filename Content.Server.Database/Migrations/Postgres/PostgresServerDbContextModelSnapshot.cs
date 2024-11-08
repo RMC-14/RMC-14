17fa81a8d3362f90de57bc753af75231c1505280
DrSmugleaf <drsmugleaf@gmail.com>
2024-11-07 23:30:23 -0800

    Revert migration

--- Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
+++ Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
@@ -512,6 +512,20 @@ namespace Content.Server.Database.Migrations.Postgres
                     b.ToTable("assigned_user_id", (string)null);
                 });
 
+            modelBuilder.Entity("Content.Server.Database.Blacklist",
+                b =>
+                {
+                    b.Property<Guid>("UserId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("uuid")
+                        .HasColumnName("user_id");
+
+                    b.HasKey("UserId")
+                        .HasName("PK_blacklist");
+
+                    b.ToTable("blacklist", (string) null);
+                });
+
             modelBuilder.Entity("Content.Server.Database.BanTemplate", b =>
                 {
                     b.Property<int>("Id")
@@ -555,19 +569,6 @@ namespace Content.Server.Database.Migrations.Postgres
                         .HasName("PK_ban_template");
 
                     b.ToTable("ban_template", (string)null);
-                });
-
-            modelBuilder.Entity("Content.Server.Database.Blacklist", b =>
-                {
-                    b.Property<Guid>("UserId")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("uuid")
-                        .HasColumnName("user_id");
-
-                    b.HasKey("UserId")
-                        .HasName("PK_blacklist");
-
-                    b.ToTable("blacklist", (string)null);
                 });
 
             modelBuilder.Entity("Content.Server.Database.ConnectionLog", b =>
@@ -794,11 +795,6 @@ namespace Content.Server.Database.Migrations.Postgres
                     b.Property<int>("Age")
                         .HasColumnType("integer")
                         .HasColumnName("age");
-
-                    b.Property<string>("ArmorPreference")
-                        .IsRequired()
-                        .HasColumnType("text")
-                        .HasColumnName("armor_preference");
 
                     b.Property<string>("CharacterName")
                         .IsRequired()
