d2f87d916f62b3a35e8a6d36bfd4355e93cfdb0b
Verm <vermidiathesnake@gmail.com>
2024-11-13 17:24:46 -0600

    Acider Runner

---
+++ Content.Shared/_RMC14/Xenonids/ForTheHive/XenoRespawnComponent.cs
@@ -0,0 +1,24 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RMC14.Xenonids.ForTheHive;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class XenoRespawnComponent : Component
+{
+    [DataField]
+    public EntityUid? Hive;
+
+    [DataField]
+    public TimeSpan RespawnAt;
+
+    [DataField]
+    public bool RespawnAtCorpse = false;
+
+    [DataField]
+    public EntityCoordinates? CorpseLocation;
+
+    [DataField]
+    public EntProtoId Larva = "CMXenoLarva";
+}
