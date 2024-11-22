aa3d3f7beb589c25ebd235235ea40c0b81a77efe
Mr. 27 <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2024-11-21 19:24:22 -0500

    Make parasite only die after it falls off from the host (#4893)
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>

--- Content.Shared/_RMC14/Xenonids/Parasite/SharedXenoParasiteSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Parasite/SharedXenoParasiteSystem.cs
@@ -79,6 +79,7 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
         SubscribeLocalEvent<XenoParasiteComponent, ThrowItemAttemptEvent>(OnParasiteThrowAttempt);
         SubscribeLocalEvent<XenoParasiteComponent, PullAttemptEvent>(OnParasiteTryPull);
         SubscribeLocalEvent<XenoParasiteComponent, GettingPickedUpAttemptEvent>(OnParasiteTryPickup);
+        SubscribeLocalEvent<XenoParasiteComponent, BeforeDamageChangedEvent>(OnParasiteBeforeDamageChanged);
 
         SubscribeLocalEvent<ParasiteSpentComponent, MapInitEvent>(OnParasiteSpentMapInit);
         SubscribeLocalEvent<ParasiteSpentComponent, UpdateMobStateEvent>(OnParasiteSpentUpdateMobState,
@@ -228,6 +229,12 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
         }
     }
 
+    private void OnParasiteBeforeDamageChanged(Entity<XenoParasiteComponent> ent, ref BeforeDamageChangedEvent args)
+    {
+        if (ent.Comp.InfectedVictim != null && !ent.Comp.FellOff) // cannot damage while infecting host
+            args.Cancelled = true;
+    }
+
     protected virtual void ParasiteLeapHit(Entity<XenoParasiteComponent> parasite)
     {
     }
@@ -250,7 +257,6 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
 
     private void OnVictimInfectedMapInit(Entity<VictimInfectedComponent> victim, ref MapInitEvent args)
     {
-        victim.Comp.FallOffAt = _timing.CurTime + victim.Comp.FallOffDelay;
         victim.Comp.BurstAt = _timing.CurTime + victim.Comp.BurstDelay;
     }
 
@@ -328,9 +334,11 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
         return true;
     }
 
-    private bool IsInfectable(EntityUid parasite, EntityUid victim)
+    private bool IsInfectable(Entity<XenoParasiteComponent> parasite, EntityUid victim)
     {
-        return HasComp<InfectableComponent>(victim)
+        return TryComp<InfectableComponent>(victim, out var infected)
+               && parasite.Comp.InfectedVictim == null
+               && !infected.BeingInfected
                && !HasComp<ParasiteSpentComponent>(parasite)
                && !HasComp<VictimInfectedComponent>(victim);
     }
@@ -379,6 +387,9 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
         if (!CanInfectPopup(parasite, victim, parasite, popup, force))
             return false;
 
+        if (!TryComp(victim, out InfectableComponent? infectable))
+            return false;
+
         if (_net.IsServer)
         {
             var pos = _transform.GetWorldPosition(victim);
@@ -402,17 +413,15 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
             return false;
 
         if (_net.IsServer &&
-            TryComp(victim, out InfectableComponent? infectable) &&
             TryComp(victim, out HumanoidAppearanceComponent? appearance) &&
             infectable.Sound.TryGetValue(appearance.Sex, out var sound))
         {
             _audio.PlayPvs(sound, victim);
         }
 
-        var time = _timing.CurTime;
-        var victimComp = EnsureComp<VictimInfectedComponent>(victim);
-        victimComp.AttachedAt = time;
-        victimComp.Hive = _hive.GetHive(parasite.Owner)?.Owner;
+        infectable.BeingInfected = true;
+        Dirty(victim, infectable);
+
         _stun.TryParalyze(victim, parasite.Comp.ParalyzeTime, true);
         _status.TryAddStatusEffect(victim, "Muted", parasite.Comp.ParalyzeTime, true, "Muted");
         _status.TryAddStatusEffect(victim, "TemporaryBlindness", parasite.Comp.ParalyzeTime, true, "TemporaryBlindness");
@@ -420,13 +429,14 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
 
         _inventory.TryEquip(victim, parasite.Owner, "mask", true, true, true);
 
-        // TODO RMC14 also do damage to the parasite
-        EnsureComp<ParasiteSpentComponent>(parasite);
-
         var unremovable = EnsureComp<UnremoveableComponent>(parasite);
         unremovable.DeleteOnDrop = false;
+
+        parasite.Comp.InfectedVictim = victim;
+        parasite.Comp.FallOffAt = _timing.CurTime + parasite.Comp.FallOffDelay;
         Dirty(parasite);
 
+        RemCompDeferred<ParasiteAIComponent>(parasite);
         var ev = new XenoParasiteInfectEvent(victim, parasite.Owner);
         RaiseLocalEvent(victim, ref ev, true);
 
@@ -498,15 +508,35 @@ public abstract partial class SharedXenoParasiteSystem : EntitySystem
             }
         }
 
+        var paraQuery = EntityQueryEnumerator<XenoParasiteComponent>();
+        while (paraQuery.MoveNext(out var uid, out var para))
+        {
+            if (para.FallOffAt < time && !para.FellOff && para.InfectedVictim != null)
+            {
+                var infectedVictim = para.InfectedVictim.Value;
+
+                if (!TryComp(infectedVictim, out InfectableComponent? infectable))
+                    continue;
+
+                para.FellOff = true;
+                Dirty(uid, para);
+
+                _inventory.TryUnequip(infectedVictim, "mask", true, true, true);
+
+                var victimComp = EnsureComp<VictimInfectedComponent>(infectedVictim);
+                victimComp.Hive = _hive.GetHive(uid)?.Owner;
+
+                // TODO RMC14 also do damage to the parasite
+                EnsureComp<ParasiteSpentComponent>(uid);
+
+                infectable.BeingInfected = false;
+                Dirty(infectedVictim, infectable);
+            }
+        }
+
         var query = EntityQueryEnumerator<VictimInfectedComponent, TransformComponent>();
         while (query.MoveNext(out var uid, out var infected, out var xform))
         {
-            if (infected.FallOffAt < time && !infected.FellOff)
-            {
-                infected.FellOff = true;
-                _inventory.TryUnequip(uid, "mask", true, true, true);
-            }
-
             if (infected.BurstAt + infected.AutoBurstTime <= time && infected.SpawnedLarva != null)
             {
                 TryBurst((uid, infected));
