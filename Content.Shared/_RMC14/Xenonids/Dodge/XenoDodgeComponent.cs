c79a78dfc7c50523df845a1c21a009859b4179e4
Verm <vermidiathesnake@gmail.com>
2024-11-24 21:00:47 -0600

    Dodge start

---
+++ Content.Shared/_RMC14/Xenonids/Dodge/XenoDodgeComponent.cs
@@ -0,0 +1,13 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Xenonids.Dodge;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class XenoDodgeComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public int PlasmaCost = 200;
+
+    [DataField, AutoNetworkedField]
+    public TimeSpan Duration = TimeSpan.FromSeconds(7);
+}
