96c0b46f01d694e98cbc19d80df7614d2221d563
CyberTropic <vfalach@gmail.com>
2024-11-25 23:48:16 -0500

    Proof of concept

---
+++ Content.Shared/_RMC14/Repairable/NailgunRepairableComponent.cs
@@ -0,0 +1,16 @@
+﻿using Content.Shared.Stacks;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RMC14.Repairable;
+
+[RegisterComponent]
+[Access(typeof(RMCRepairableSystem))]
+public sealed partial class NailgunRepairableComponent : Component
+{
+    [DataField]
+    public List<string> RepairMaterials = new();
+
+    [DataField]
+    public List<float> RepairValues = new();
+}
