a55e7f4660baaff2eeeca5b154e944f0716b7074
dalush <e725479@gmail.com>
2024-12-02 09:04:39 +0300

    Fix button system
    fixing a bug with buttons.
    
    ALL entities with XenoComponent
    couldn't press buttons.
    now only entities without MarineComponent
    
    Previously, even mice could press buttons.

--- Content.Shared/_RMC14/Doors/CMDoorSystem.cs
+++ Content.Shared/_RMC14/Doors/CMDoorSystem.cs
@@ -13,6 +13,7 @@ using Robust.Shared.Map.Enumerators;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
 using Robust.Shared.Timing;
+using Content.Shared._RMC14.Marines;
 
 namespace Content.Shared._RMC14.Doors;
 
@@ -58,7 +59,7 @@ public sealed class CMDoorSystem : EntitySystem
     private void OnButtonActivateInWorld(Entity<RMCDoorButtonComponent> button, ref ActivateInWorldEvent args)
     {
         var user = args.User;
-        if (HasComp<XenoComponent>(user))
+        if (!HasComp<MarineComponent>(user))
             return;
 
         if (!_accessReader.IsAllowed(user, button))
