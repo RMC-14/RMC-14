2db726ab7a6dd8516399aecf2bcbe85a7d065bb3
blueDev2 <luckywill339@gmail.com>
2024-12-04 16:06:10 -0500

    Supress "Can't infect egg morpher" message

--- Content.Client/_RMC14/Xenonids/Construction/EggMorpher/EggMorpherSystem.cs
+++ Content.Client/_RMC14/Xenonids/Construction/EggMorpher/EggMorpherSystem.cs
@@ -1,4 +1,5 @@
 using Content.Shared._RMC14.Xenonids.Construction.EggMorpher;
+using Content.Shared.Interaction;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,4 +10,20 @@ namespace Content.Client._RMC14.Xenonids.Construction.EggMorpher;
 
 public sealed partial class EggMorpherSystem : SharedEggMorpherSystem
 {
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<EggMorpherComponent, InteractHandEvent>(OnInteractHand);
+        SubscribeLocalEvent<EggMorpherComponent, InteractUsingEvent>(OnInteractUsing);
+    }
+
+    private void OnInteractHand(Entity<EggMorpherComponent> eggMorpher, ref InteractHandEvent args)
+    {
+        args.Handled = true;
+    }
+    private void OnInteractUsing(Entity<EggMorpherComponent> eggMorpher, ref InteractUsingEvent args)
+    {
+        args.Handled = true;
+    }
 }
