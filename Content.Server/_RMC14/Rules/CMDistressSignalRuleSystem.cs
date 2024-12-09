3cae94d5d39211e4d6259bc13b2b587866e0f24d
DrSmugleaf <drsmugleaf@gmail.com>
2024-12-08 17:06:14 -0800

    Rename planet file for consistency

--- Content.Server/_RMC14/Rules/CMDistressSignalRuleSystem.cs
+++ Content.Server/_RMC14/Rules/CMDistressSignalRuleSystem.cs
@@ -1238,7 +1238,7 @@ public sealed class CMDistressSignalRuleSystem : GameRuleSystem<CMDistressSignal
             "trijent" => "Trijent Dam",
             "varadero" => "New Varadero",
             "kutjevo" => "Kutjevo Refinery",
-            "chances_claim" => "LV-522 Chance's Claim",
+            "chances" => "LV-522 Chance's Claim",
             _ => name,
         };
     }
