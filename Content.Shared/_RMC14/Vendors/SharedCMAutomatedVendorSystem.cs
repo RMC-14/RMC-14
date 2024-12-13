5ea36cb426db0bc67be53902cd7a14f143aeba84
DrSmugleaf <drsmugleaf@gmail.com>
2024-12-13 14:04:22 -0800

    Fix not being able to pick up some items dispensed from vendors

--- Content.Shared/_RMC14/Vendors/SharedCMAutomatedVendorSystem.cs
+++ Content.Shared/_RMC14/Vendors/SharedCMAutomatedVendorSystem.cs
@@ -1,5 +1,6 @@
 ﻿using System.Numerics;
 using Content.Shared._RMC14.Inventory;
+using Content.Shared._RMC14.Item;
 using Content.Shared._RMC14.Map;
 using Content.Shared._RMC14.Marines.Squads;
 using Content.Shared._RMC14.Scaling;
@@ -51,6 +52,7 @@ public abstract class SharedCMAutomatedVendorSystem : EntitySystem
 
         SubscribeLocalEvent<RMCRecentlyVendedComponent, GotEquippedHandEvent>(OnRecentlyGotEquipped);
         SubscribeLocalEvent<RMCRecentlyVendedComponent, GotEquippedEvent>(OnRecentlyGotEquipped);
+        SubscribeLocalEvent<RMCRecentlyVendedComponent, ItemCamouflageEvent>(OnRecentlyCamouflage);
 
         Subs.BuiEvents<CMAutomatedVendorComponent>(CMAutomatedVendorUI.Key, subs =>
         {
@@ -146,6 +148,21 @@ public abstract class SharedCMAutomatedVendorSystem : EntitySystem
     private void OnRecentlyGotEquipped<T>(Entity<RMCRecentlyVendedComponent> ent, ref T args)
     {
         RemCompDeferred<WallMountComponent>(ent);
+    }
+
+    private void OnRecentlyCamouflage(Entity<RMCRecentlyVendedComponent> ent, ref ItemCamouflageEvent args)
+    {
+        var recently = EnsureComp<RMCRecentlyVendedComponent>(args.New);
+        foreach (var prevent in ent.Comp.PreventCollide)
+        {
+            recently.PreventCollide.Add(prevent);
+        }
+
+        Dirty(args.New, recently);
+
+        var mount = EnsureComp<WallMountComponent>(args.New);
+        mount.Arc = Angle.FromDegrees(360);
+        Dirty(args.New, mount);
     }
 
     protected virtual void OnVendBui(Entity<CMAutomatedVendorComponent> vendor, ref CMVendorVendBuiMsg args)
