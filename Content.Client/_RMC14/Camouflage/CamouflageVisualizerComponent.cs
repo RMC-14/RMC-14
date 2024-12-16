003d9c2f4fb799017a8cb268ec503233ec9df897
MACMAN2003 <macman2003c@gmail.com>
2024-12-16 14:16:44 -0800

    i give up again
    epic give up major victory

--- Content.Client/_RMC14/Camouflage/CamouflageVisualizerComponent.cs
+++ Content.Client/_RMC14/Camouflage/CamouflageVisualizerComponent.cs
@@ -1,11 +1,14 @@
 using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+using Content.Shared._RMC14.Item;
 
 namespace Content.Client._RMC14.Camouflage;
 
 [RegisterComponent]
 public sealed partial class CamouflageVisualizerComponent : Component
 {
-    // DIY GenericVisualizer
-    [DataField("visuals", required: true)]
-    public Dictionary<Enum, Dictionary<string, Dictionary<string, PrototypeLayerData>>> Visuals = default!;
+    [DataField(required: true), AutoNetworkedField]
+    public Dictionary<CamouflageType, SpriteSpecifier.Rsi> CamouflageVariations = new();
+    [DataField(required: true), AutoNetworkedField]
+    public SpriteSpecifier.Rsi Replace = new();
 }
--- Content.Client/_RMC14/Camouflage/CamouflageVisualizerSystem.cs
+++ Content.Client/_RMC14/Camouflage/CamouflageVisualizerSystem.cs
@@ -1,5 +1,6 @@
-
-
+using Robust.Shared.Utility;
+using Content.Shared._RMC14.Item;
+using Robust.Client.GameObjects;
 
 
 
@@ -8,4 +9,16 @@ namespace Content.Client._RMC14.Camouflage;
 public sealed class CamouflageVisualizerSystem : EntitySystem
 {
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<CamouflageVisualizerComponent, ItemCamouflageEvent>(OnCamoChange);
+    }
+
+    private void OnCamoChange(Entity<CamouflageVisualizerComponent> ent, ref ItemCamouflageEvent args)
+    {
+        if (!TryComp<SpriteComponent>(ent, out var sprite))
+            return;
+
+    }
 }
