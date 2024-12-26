95abc4c78b17f2ec5f528be2e9a3a1a13c406e82
Verm <32827189+Vermidia@users.noreply.github.com>
2024-12-26 17:54:13 -0600

    Xenonid inhands + some stuff missed in the sprite change (#5238)

---
+++ Content.Client/_RMC14/Xenonids/Inhands/XenoInhandsVisualsSystem.cs
@@ -0,0 +1,79 @@
+﻿using Content.Shared._RMC14.Xenonids.Inhands;
+using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
+
+namespace Content.Client._RMC14.Xenonids.Inhands;
+
+public sealed class XenoInhandsVisualsSystem : VisualizerSystem<XenoInhandsComponent>
+{
+    protected override void OnAppearanceChange(EntityUid uid, XenoInhandsComponent component, ref AppearanceChangeEvent args)
+    {
+        var sprite = args.Sprite;
+
+        if (sprite == null)
+            return;
+
+        if (!AppearanceSystem.TryGetData(uid, XenoInhandVisuals.Right, out string right))
+            return;
+
+        if (!AppearanceSystem.TryGetData(uid, XenoInhandVisuals.Left, out string left))
+            return;
+
+        bool downed = false;
+        bool resting = false;
+        bool ovi = false;
+
+        AppearanceSystem.TryGetData(uid, XenoInhandVisuals.Downed, out downed);
+        AppearanceSystem.TryGetData(uid, XenoInhandVisuals.Resting, out resting);
+        AppearanceSystem.TryGetData(uid, XenoInhandVisuals.Ovi, out ovi);
+
+        string name = left;
+        XenoInhandVisualLayers layerDef = XenoInhandVisualLayers.Left;
+
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == 1)
+            {
+                name = right;
+                layerDef = XenoInhandVisualLayers.Right;
+
+            }
+
+            if (!sprite.LayerMapTryGet(layerDef, out var layer))
+                continue;
+
+            if (name == string.Empty)
+            {
+                sprite.LayerSetVisible(layer, false);
+            }
+            else
+            {
+                sprite.LayerSetVisible(layer, true);
+
+                string stateString = $"{component.Prefix}_{name}_{layerDef.ToString().ToLower()}";
+
+
+                if (ovi)
+                    stateString += "_" + component.Ovi;
+                else if (downed)
+                    stateString += "_" + component.Downed;
+                else if (resting)
+                    stateString += "_" + component.Resting;
+
+                RSI? rsi = sprite.LayerGetActualRSI(layerDef);
+
+                if (rsi == null)
+                    continue;
+
+                rsi.TryGetState(stateString, out var state);
+
+                if (state != null)
+                {
+                    sprite.LayerSetState(layer, stateString);
+                }
+                else
+                    sprite.LayerSetVisible(layer, false);
+            }
+        }
+    }
+}
