29135c5d4a826b21241a98e986e8c3bfbd66a2ba
DrSmugleaf <drsmugleaf@gmail.com>
2025-01-08 22:10:29 -0800

    Fix formatting

--- Content.Shared/_RMC14/Weapons/Ranged/Stacks/GunStacksComponent.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/Stacks/GunStacksComponent.cs
@@ -1,6 +1,7 @@
 using Content.Shared.FixedPoint;
 using Robust.Shared.GameStates;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Utility;
 
 namespace Content.Shared._RMC14.Weapons.Ranged.Stacks;
 
@@ -28,4 +29,25 @@ public sealed partial class GunStacksComponent : Component
 
     [DataField, AutoNetworkedField]
     public int Hits;
+
+    // TODO RMC14
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier.Rsi[] Crosshairs = new SpriteSpecifier.Rsi[]
+    {
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-0.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-1.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-2.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-3.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-4.rsi"), "all"),
+    };
+
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier.Rsi[] FiredCrosshairs = new SpriteSpecifier.Rsi[]
+    {
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-fired-0.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-fired-1.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-fired-2.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-fired-3.rsi"), "all"),
+        new(new ResPath("_RMC14/Interface/MousePointer/XM88/xm88-fired-4.rsi"), "all"),
+    };
 }
