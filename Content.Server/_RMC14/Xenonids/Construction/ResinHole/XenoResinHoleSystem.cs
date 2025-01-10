0ff7f1b428c63b601b1b986a194e779df5e1e17a
DrSmugleaf <drsmugleaf@gmail.com>
2025-01-09 21:07:30 -0800

    Fix acid resin traps triggering through corners in walls

--- Content.Server/_RMC14/Xenonids/Construction/ResinHole/XenoResinHoleSystem.cs
+++ Content.Server/_RMC14/Xenonids/Construction/ResinHole/XenoResinHoleSystem.cs
@@ -1,74 +1,56 @@
 using System.Numerics;
-using Content.Server.Weapons.Ranged.Systems;
+using Content.Shared._RMC14.OnCollide;
 using Content.Shared._RMC14.Xenonids;
 using Content.Shared._RMC14.Xenonids.Acid;
 using Content.Shared._RMC14.Xenonids.Bombard;
 using Content.Shared._RMC14.Xenonids.Construction;
 using Content.Shared._RMC14.Xenonids.Construction.Events;
 using Content.Shared._RMC14.Xenonids.Construction.ResinHole;
-using Content.Shared._RMC14.OnCollide;
-using Content.Shared._RMC14.Xenonids.Egg;
 using Content.Shared._RMC14.Xenonids.Parasite;
 using Content.Shared._RMC14.Xenonids.Plasma;
 using Content.Shared._RMC14.Xenonids.Weeds;
-using Content.Shared.Hands.Components;
-using Content.Shared.Maps;
-using Content.Shared.Actions;
 using Content.Shared.Administration.Logs;
-using Content.Shared.Buckle.Components;
 using Content.Shared.Coordinates.Helpers;
 using Content.Shared.Damage;
 using Content.Shared.Database;
 using Content.Shared.DoAfter;
+using Content.Shared.Examine;
+using Content.Shared.Hands.Components;
 using Content.Shared.Hands.EntitySystems;
 using Content.Shared.Interaction;
-using Content.Shared.Interaction.Events;
+using Content.Shared.Maps;
 using Content.Shared.Popups;
 using Content.Shared.Standing;
 using Content.Shared.StepTrigger.Systems;
 using Content.Shared.Stunnable;
-using Content.Shared.Tag;
 using Robust.Server.Audio;
 using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
 using static Content.Shared.Physics.CollisionGroup;
-using Content.Shared.Examine;
-using Content.Shared.Standing;
 
 namespace Content.Server._RMC14.Xenonids.Construction.ResinHole;
 
-public sealed partial class XenoResinHoleSystem : SharedXenoResinHoleSystem
+public sealed class XenoResinHoleSystem : SharedXenoResinHoleSystem
 {
     [Dependency] private readonly ISharedAdminLogManager _adminLogs = default!;
-    [Dependency] private readonly SharedActionsSystem _action = default!;
     [Dependency] private readonly SharedHandsSystem _hands = default!;
-    [Dependency] private readonly SharedInteractionSystem _interact = default!;
-    [Dependency] private readonly EntityLookupSystem _lookup = default!;
     [Dependency] private readonly SharedTransformSystem _transform = default!;
-    [Dependency] private readonly SharedMapSystem _map = default!;
-    [Dependency] private readonly TagSystem _tags = default!;
     [Dependency] private readonly SharedInteractionSystem _interaction = default!;
     [Dependency] private readonly TurfSystem _turf = default!;
     [Dependency] private readonly XenoPlasmaSystem _xenoPlasma = default!;
     [Dependency] private readonly SharedXenoWeedsSystem _xenoWeeds = default!;
     [Dependency] private readonly SharedStunSystem _stun = default!;
-    [Dependency] private readonly GunSystem _gun = default!;
     [Dependency] private readonly AudioSystem _audio = default!;
     [Dependency] private readonly SharedOnCollideSystem _onCollide = default!;
-    [Dependency] private readonly XenoSystem _xeno = default!;
     [Dependency] private readonly StandingStateSystem _standing = default!;
     [Dependency] private readonly SharedXenoConstructionSystem _xenoConstruct = default!;
     [Dependency] private readonly SharedMapSystem _mapSystem = default!;
     [Dependency] private readonly SharedPhysicsSystem _physics = default!;
 
-    private static readonly ProtoId<TagPrototype> AirlockTag = "Airlock";
-    private static readonly ProtoId<TagPrototype> StructureTag = "Structure";
-
-    private static readonly EntProtoId ResinHolePrototype = "XenoResinHole";
     private EntityQuery<PhysicsComponent> _physicsQuery;
 
     public override void Initialize()
@@ -377,7 +359,9 @@ public sealed partial class XenoResinHoleSystem : SharedXenoResinHoleSystem
                 && !HasComp<VictimInfectedComponent>(args.Tripper);
             return;
         }
-        else if (_mobState.IsDead(args.Tripper) || _standing.IsDown(args.Tripper))
+        else if (_mobState.IsDead(args.Tripper) ||
+                 _standing.IsDown(args.Tripper) ||
+                 !_interaction.InRangeUnobstructed(args.Source, args.Tripper, 1.5f))
         {
             var inRange = EnsureComp<InResinHoleRangeComponent>(args.Tripper);
             if (!inRange.HoleList.Contains(resinHole))
@@ -400,45 +384,7 @@ public sealed partial class XenoResinHoleSystem : SharedXenoResinHoleSystem
 
     private void OnInRangeStand(Entity<InResinHoleRangeComponent> tripper, ref StoodEvent args)
     {
-        for (var i  = tripper.Comp.HoleList.Count - 1; i >= 0; i--)
-        {
-            var resinHole = tripper.Comp.HoleList[i];
-
-            if (!TryComp<XenoResinHoleComponent>(resinHole, out var holeComponent))
-            {
-                tripper.Comp.HoleList.Remove(resinHole);
-                continue;
-            }
-
-            //Continue if trap has emptied or been replaced with a parasite
-            if (holeComponent.TrapPrototype is null || holeComponent.TrapPrototype == XenoResinHoleComponent.ParasitePrototype)
-                continue;
-
-            //Check if each Resin Hole is still colliding with tripper
-            if (!_physicsQuery.TryGetComponent(resinHole, out var physics))
-            {
-                tripper.Comp.HoleList.Remove(resinHole);
-                continue;
-            }
-
-            foreach (var ent in _physics.GetContactingEntities(resinHole, physics))
-            {
-                if (ent != tripper.Owner)
-                    continue;
-
-                ActivateTrap((resinHole, holeComponent));
-                tripper.Comp.HoleList.Remove(resinHole);
-
-                if (tripper.Comp.HoleList.Count == 0)
-                {
-                    RemCompDeferred<InResinHoleRangeComponent>(tripper);
-                }
-                //Only trigger one trap maximum per stand-up
-                return;
-            }
-        }
-
-        RemCompDeferred<InResinHoleRangeComponent>(tripper);
+        UpdateInRange(tripper, true);
     }
 
     private bool CanPlaceResinHole(EntityUid user, EntityCoordinates coords)
@@ -552,6 +498,65 @@ public sealed partial class XenoResinHoleSystem : SharedXenoResinHoleSystem
                 return true;
             default:
                 return false;
+        }
+    }
+
+    private void UpdateInRange(Entity<InResinHoleRangeComponent> tripper, bool stoodUp)
+    {
+        if (_standing.IsDown(tripper))
+            return;
+
+        for (var i = tripper.Comp.HoleList.Count - 1; i >= 0; i--)
+        {
+            var resinHole = tripper.Comp.HoleList[i];
+
+            if (!TryComp<XenoResinHoleComponent>(resinHole, out var holeComponent))
+            {
+                tripper.Comp.HoleList.Remove(resinHole);
+                continue;
+            }
+
+            //Continue if trap has emptied or been replaced with a parasite
+            if (holeComponent.TrapPrototype is null || holeComponent.TrapPrototype == XenoResinHoleComponent.ParasitePrototype)
+                continue;
+
+            //Check if each Resin Hole is still colliding with tripper
+            if (!_physicsQuery.TryGetComponent(resinHole, out var physics))
+            {
+                tripper.Comp.HoleList.Remove(resinHole);
+                continue;
+            }
+
+            foreach (var ent in _physics.GetContactingEntities(resinHole, physics))
+            {
+                if (ent != tripper.Owner)
+                    continue;
+
+                if (!_interaction.InRangeUnobstructed(resinHole, ent, 1.5f))
+                    continue;
+
+                ActivateTrap((resinHole, holeComponent));
+                tripper.Comp.HoleList.Remove(resinHole);
+
+                if (tripper.Comp.HoleList.Count == 0)
+                {
+                    RemCompDeferred<InResinHoleRangeComponent>(tripper);
+                }
+                //Only trigger one trap maximum per stand-up
+                return;
+            }
+        }
+
+        if (stoodUp || tripper.Comp.HoleList.Count == 0)
+            RemCompDeferred<InResinHoleRangeComponent>(tripper);
+    }
+
+    public override void Update(float frameTime)
+    {
+        var inRangeQuery = EntityQueryEnumerator<InResinHoleRangeComponent>();
+        while (inRangeQuery.MoveNext(out var uid, out var comp))
+        {
+            UpdateInRange((uid, comp), false);
         }
     }
 }
