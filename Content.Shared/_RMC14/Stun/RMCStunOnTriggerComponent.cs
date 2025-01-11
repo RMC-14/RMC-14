9af27e64c4bc2ba441001f5ffb6282d9d6864ab5
RyanStrudfelt <Ryanstrudfelt@gmail.com>
2025-01-11 20:33:57 +0000

    MP Armory Update + CMP Revolver.
    This update adds the following;
    - Red Alert Gun lockers (m890 tactical, M4SPR, M63)
    - Blue Alert Riot lockers (Riot suits,  riot shotguns, and other riot equipment)
    Both of these lockers require Armory access.
    - M890 Riot Shotgun, (Semi-automatic Tactical shotgun that will only load Beanbag Shells, has an under-barrel grenade launcher)
    - RSh-9 Assault Revolver for the Chief of Military Police, with 9x39 Rounds to load into it.
    - Rubber Magazines & Rounds for ; M54c & M4SPR, M63, M1984 & M77, (SEA's get these in their SEATech vendor too!)
    - New Flash-bangs using the Actual CM13 Flash-bang sprite, and that stun people in a Seven tile radius, with a 27-second stun! These are compatible with the M81 Riot Grenade launcher, but not any other type of launcher.  (Warning! Sunglasses will not save you this time!!!)
    - Flash-bang Boxes, containing 8 flash-bangs each.
    - RiotTech Vendor, Dispenses Riot Equipment.
    - M81 Riot Grenade launcher, This is a break action launcher that will only accept Flashbang grenades, and is coincidentally, the only way to launch them aside from throwing them. (In the future, this will also accept Tear-gas) (Thanks to dummehytteost for helping get the Break-action to work and sanity checking my RSI)
    - Adds RMCStunOnTriggerComponent.cs enabling things to Stun in a radius. (Thanks to NikTheChampiongr for helping me with this!!!)
    
    This update Changes the following;
    - SecTech; Now has RMCGrenadeFlashBang instead of GrenadeFlashBang.
    - SEATech; Now has Rubber magazines and Beanbag Shells as intended.
    - M890 Tactical Shotgun now has a fire-rate of 0.7 instead of 0.5 per second. and is no-longer capable of burst-fire.

--- Content.Shared/_RMC14/Stun/RMCStunOnTriggerComponent.cs
+++ Content.Shared/_RMC14/Stun/RMCStunOnTriggerComponent.cs
@@ -6,6 +6,6 @@ namespace Content.Shared._RMC14.Stun;
 public sealed partial class RMCStunOnTriggerComponent : Component
 {
     [DataField] public float Range = 8.0f;
-    [DataField] public double Duration = 3.0f;
+    [DataField] public double Duration = 27.0f;
     [DataField] public float Probability = 1.0f;
 }
