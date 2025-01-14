9dafd70908b2be8da30947984b2fcf897aff4be7
Mr. 27 <koolthunder019@gmail.com>
2025-01-14 07:40:29 -0500

    Update NightVisionVisorComponent.cs

--- Content.Shared/_RMC14/NightVision/NightVisionVisorComponent.cs
+++ Content.Shared/_RMC14/NightVision/NightVisionVisorComponent.cs
@@ -2,7 +2,7 @@
 
 namespace Content.Shared._RMC14.NightVision;
 
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[RegisterComponent, NetworkedComponent]
 [Access(typeof(SharedNightVisionSystem))]
 public sealed partial class NightVisionVisorComponent : Component
 {
