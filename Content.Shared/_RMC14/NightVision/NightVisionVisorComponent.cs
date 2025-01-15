a46a8cb8a9b221d23b2806ef77b81da8d595e703
Mr. 27 <koolthunder019@gmail.com>
2025-01-15 10:33:51 -0500

    Revert "Update NightVisionVisorComponent.cs"
    This reverts commit 9dafd70908b2be8da30947984b2fcf897aff4be7.

--- Content.Shared/_RMC14/NightVision/NightVisionVisorComponent.cs
+++ Content.Shared/_RMC14/NightVision/NightVisionVisorComponent.cs
@@ -2,7 +2,7 @@
 
 namespace Content.Shared._RMC14.NightVision;
 
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 [Access(typeof(SharedNightVisionSystem))]
 public sealed partial class NightVisionVisorComponent : Component
 {
