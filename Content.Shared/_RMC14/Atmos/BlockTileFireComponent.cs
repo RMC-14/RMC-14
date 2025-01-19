ae72eeb71244979d349c428f18fee46b2eb9942d
DrSmugleaf <drsmugleaf@gmail.com>
2025-01-19 12:46:20 -0800

    Remove redundant AutoGenerateComponentState

--- Content.Shared/_RMC14/Atmos/BlockTileFireComponent.cs
+++ Content.Shared/_RMC14/Atmos/BlockTileFireComponent.cs
@@ -2,6 +2,6 @@
 
 namespace Content.Shared._RMC14.Atmos;
 
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[RegisterComponent, NetworkedComponent]
 [Access(typeof(SharedRMCFlammableSystem))]
 public sealed partial class BlockTileFireComponent : Component;
