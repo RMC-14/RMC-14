0cefd8089c26ec9d903bcd80362f387406512616
Timemaster99 <57200767+Timemaster99@users.noreply.github.com>
2025-02-07 02:31:49 -0500

    Fix diff

--- Content.Shared/_RMC14/Evacuation/SharedEvacuationSystem.cs
+++ Content.Shared/_RMC14/Evacuation/SharedEvacuationSystem.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+﻿using System.Linq;
 using System.Numerics;
 using System.Text;
 using Content.Shared._RMC14.Areas;
@@ -553,7 +553,7 @@ public abstract class SharedEvacuationSystem : EntitySystem
                     if (offAreas.Length == 0)
                         marineAnnounce += " All fueling areas operational.";
                     else
-                        marineAnnounce += $" To increase speed, restore power to the following areas: {offAreas}";
+                        marineAnnounce += $"To increase speed, restore power to the following areas: {offAreas}";
 
                     return marineAnnounce;
                 }
@@ -599,7 +599,7 @@ public abstract class SharedEvacuationSystem : EntitySystem
                     if (offAreas.Length == 0)
                         marineAnnounce += " All fueling areas operational.";
                     else
-                        marineAnnounce += $"To increase speed, restore power to the following areas: {offAreas}";
+                        marineAnnounce += $" To increase speed, restore power to the following areas: {offAreas}";
 
                     _marineAnnounce.AnnounceARES(null, marineAnnounce);
 
