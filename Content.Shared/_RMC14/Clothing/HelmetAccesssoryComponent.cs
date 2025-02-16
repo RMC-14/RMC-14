b86e1f8d6e4e17858275b1a83ec762eb20033ac2
Mr. 27 <koolthunder019@gmail.com>
2025-02-16 13:11:48 -0500

    inital

---
+++ Content.Shared/_RMC14/Clothing/HelmetAccesssoryComponent.cs
@@ -0,0 +1,17 @@
+﻿using Content.Shared.Inventory;
+using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
+
+namespace Content.Shared._RMC14.Clothing;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(HelmetAccessoriesSystem))]
+public sealed partial class HelmetAccessoryComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public SpriteSpecifier.Rsi Rsi;
+
+    // todo add toggle sprites
+    // [DataField, AutoNetworkedField]
+    // public SpriteSpecifier.Rsi? ToggledSprite;
+}
---
+++ Content.Shared/_RMC14/Clothing/HelmetAccesssoryHolderComponent.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Clothing;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(HelmetAccessoriesSystem))]
+public sealed partial class HelmetAccessoryHolderComponent : Component {}
+
+public enum HelmetAccessoryLayers
+{
+    Helmet
+}
