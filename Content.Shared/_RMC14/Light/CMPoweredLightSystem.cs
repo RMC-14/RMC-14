8e21f8782cc673a57de4f3c24cd64050c3a8506a
noctis <99278536+noctyrnal@users.noreply.github.com>
2025-02-22 10:00:30 +0200

    Hybrisa (#5518)
    Co-authored-by: DrSmugleaf <drsmugleaf@gmail.com>

--- Content.Shared/_RMC14/Light/CMPoweredLightSystem.cs
+++ Content.Shared/_RMC14/Light/CMPoweredLightSystem.cs
@@ -1,15 +1,19 @@
 ﻿using Content.Shared._RMC14.Xenonids;
+using Content.Shared.Interaction.Events;
 using Content.Shared.Popups;
 
 namespace Content.Shared._RMC14.Light;
 
 public sealed class CMPoweredLightSystem : EntitySystem
 {
+    [Dependency] private readonly SharedPointLightSystem _pointLight = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
 
     public override void Initialize()
     {
         SubscribeLocalEvent<LightBurnHandAttemptEvent>(OnLightBurnHandAttempt);
+
+        SubscribeLocalEvent<PreventAttackLightOffComponent, GettingAttackedAttemptEvent>(OnPreventAttackLightOffAttackedAttempt);
     }
 
     private void OnLightBurnHandAttempt(ref LightBurnHandAttemptEvent ev)
@@ -17,5 +21,17 @@ public sealed class CMPoweredLightSystem : EntitySystem
         ev.Cancelled = true;
         if (!HasComp<XenoComponent>(ev.User))
             _popup.PopupClient(Loc.GetString("cm-light-failed"), ev.Light, ev.User);
+    }
+
+    private void OnPreventAttackLightOffAttackedAttempt(Entity<PreventAttackLightOffComponent> ent, ref GettingAttackedAttemptEvent args)
+    {
+        if (args.Cancelled)
+            return;
+
+        if (!_pointLight.TryGetLight(ent, out var pointLight) ||
+            !pointLight.Enabled)
+        {
+            args.Cancelled = true;
+        }
     }
 }
