5ee2cb16d40a4d34bfc8091e345a17390d9cd44e
DrSmugleaf <drsmugleaf@gmail.com>
2025-02-28 17:09:53 -0800

    Thunderdome atmos, thunderdome ert area lights, fix barriers to delete only, landing pad floors

--- Content.Shared/_RMC14/CrashLand/DeleteCrashLandableOnTouchComponent.cs
+++ Content.Shared/_RMC14/CrashLand/DeleteCrashLandableOnTouchComponent.cs
@@ -4,4 +4,4 @@ namespace Content.Shared._RMC14.CrashLand;
 
 [RegisterComponent, NetworkedComponent]
 [Access(typeof(CrashLandSystem))]
-public sealed partial class CrashLandOnTouchComponent : Component;
+public sealed partial class DeleteCrashLandableOnTouchComponent : Component;
