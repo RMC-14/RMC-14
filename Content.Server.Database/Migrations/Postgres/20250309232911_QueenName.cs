1ad8332360db7bae107f3b69234e177f0b7b806e
Binbag <124107163+Binbagg@users.noreply.github.com>
2025-03-09 23:34:21 +0000

    WHAT IS A MIGRATIONNNNNN

--- Content.Server.Database/Migrations/Postgres/20250309232911_QueenName.cs
+++ Content.Server.Database/Migrations/Postgres/20250309232911_QueenName.cs
@@ -1,22 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿#nullable disable
 
-#nullable disable
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Content.Server.Database.Migrations.Postgres
 {
     /// <inheritdoc />
-    public partial class QueenName : Migration
+    public partial class PlaytimePerks : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<bool>(
+                name: "queen_name",
+                table: "profile",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "queen_name",
+                table: "profile");
         }
     }
 }
