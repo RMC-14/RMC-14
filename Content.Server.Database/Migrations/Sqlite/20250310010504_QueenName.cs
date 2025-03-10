658e4d98d66cd5c06a758341fc168b98b9fc9fdb
Binbag <tbrisby333@gmail.com>
2025-03-10 01:06:15 +0000

    vc

---
+++ Content.Server.Database/Migrations/Sqlite/20250310010504_QueenName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class QueenName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "queen_name",
+                table: "profile",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "queen_name",
+                table: "profile");
+        }
+    }
+}
