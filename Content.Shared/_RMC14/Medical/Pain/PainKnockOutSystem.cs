926b7f42268f9123227b4288562121b559901731
N0lim <0s7n888884@gmail.com>
2025-03-20 12:44:46 +0300

    Damage on devour in pain krit added

--- Content.Shared/_RMC14/Medical/Pain/PainKnockOutSystem.cs
+++ Content.Shared/_RMC14/Medical/Pain/PainKnockOutSystem.cs
@@ -1,6 +1,12 @@
 using Content.Shared.StatusEffect;
 using Content.Shared.Stunnable;
 using Content.Shared._RMC14.StatusEffect;
+using Content.Shared.Throwing;
+using Robust.Shared.Prototypes;
+using Content.Shared.Damage;
+using Content.Shared.Damage.Prototypes;
+using Content.Shared.Chat;
+using Content.Shared._RMC14.Xenonids.Devour;
 
 namespace Content.Shared._RMC14.Medical.Pain;
 
@@ -8,13 +14,20 @@ public sealed class PainKnockOutSystem : EntitySystem
 {
     [Dependency] private readonly SharedStunSystem _stun = default!;
     [Dependency] private readonly StatusEffectsSystem _status = default!;
+    [Dependency] private readonly DamageableSystem _damage = default!;
+    [Dependency] private readonly SharedSuicideSystem _suicide = default!;
 
     [ValidatePrototypeId<StatusEffectPrototype>]
     private const string PainKnockOutKey = "PainKnockOut";
+
+    private static readonly ProtoId<DamageTypePrototype> AsphyxiationType = "Asphyxiation";
+
     public override void Initialize()
     {
         SubscribeLocalEvent<PainKnockOutComponent, RMCStatusEffectTimeEvent>(OnUpdate);
+        SubscribeLocalEvent<PainKnockOutComponent, XenoDevouredEvent>(OnDevour);
     }
+
     private void OnUpdate(EntityUid uid, PainKnockOutComponent component, RMCStatusEffectTimeEvent args)
     {
         if (args.Key != PainKnockOutKey)
@@ -25,5 +38,16 @@ public sealed class PainKnockOutSystem : EntitySystem
         _stun.TryParalyze(uid, time, true);
         _status.TryAddStatusEffect(uid, "Muted", time, true, "Muted");
         _status.TryAddStatusEffect(uid, "TemporaryBlindness", time, true, "TemporaryBlindness");
+    }
+
+    private void OnDevour(Entity<PainKnockOutComponent> ent, ref XenoDevouredEvent args)
+    {
+        if (HasComp<DamageableComponent>(ent))
+            OxyKill(ent);
+    }
+
+    private void OxyKill(Entity<DamageableComponent> ent)
+    {
+        _suicide.ApplyLethalDamage(ent, AsphyxiationType);
     }
 }
