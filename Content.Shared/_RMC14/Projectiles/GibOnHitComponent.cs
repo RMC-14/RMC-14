8ddd5b425c329047cc13d8a9208e77a9f4be9e08
Otito <oplaton2303@gmail.com>
2025-03-23 02:32:48 -0300

    GibOnHitComponent created

---
+++ Content.Shared/_RMC14/Projectiles/GibOnHitComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Projectiles;
+
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class GibOnHitComponent : Component
+{
+
+}
