d426938266af9c26ad43b79f408d4a573de35b5f
DrSmugleaf <drsmugleaf@gmail.com>
2025-03-29 02:22:24 -0700

    Fix alpha overwatch console not being accessible with a table in the way

---
+++ Content.Shared/_RMC14/Interaction/IgnoreInteractionRangeComponent.cs
@@ -0,0 +1,12 @@
+﻿using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Interaction;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(RMCInteractionSystem))]
+public sealed partial class IgnoreInteractionRangeComponent : Component
+{
+    [DataField(required: true), AutoNetworkedField]
+    public EntityWhitelist? Whitelist;
+}
