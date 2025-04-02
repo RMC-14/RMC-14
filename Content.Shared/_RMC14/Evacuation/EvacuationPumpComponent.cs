db30dcab77d174bcf7e7c1ee57cd013b5a99d819
exin <me@exin.xyz>
2025-04-02 17:48:51 -0500

    feat(evacuation): Add fuel pump ambience

--- Content.Shared/_RMC14/Evacuation/EvacuationPumpComponent.cs
+++ Content.Shared/_RMC14/Evacuation/EvacuationPumpComponent.cs
@@ -1,11 +1,16 @@
-﻿using Robust.Shared.GameStates;
+﻿using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared._RMC14.Evacuation;
 
 [RegisterComponent, NetworkedComponent]
 [Access(typeof(SharedEvacuationSystem))]
-public sealed partial class EvacuationPumpComponent : Component;
+public sealed partial class EvacuationPumpComponent : Component
+{
+    [DataField("sound", required: true)]
+    public SoundSpecifier Sound = default!;
+}
 
 [Serializable, NetSerializable]
 public enum EvacuationPumpLayers
