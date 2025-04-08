30de4d9c9c1950c8f58ddc5c9db1cfff4d5f58a2
CyberTropic <vfalach@gmail.com>
2025-04-08 12:26:26 -0400

    a

---
+++ Content.Shared/_RMC14/Weather/RMCWeatherCycleComponent.cs
@@ -0,0 +1,13 @@
+﻿using Content.Shared._RMC14.OrbitalCannon;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._RMC14.Weather;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(RMCWeatherSystem))]
+public sealed partial class RMCWeatherCycleComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public EntProtoId<RMCWeatherEventComponent> WeatherEvent;
+}
---
+++ Content.Shared/_RMC14/Weather/RMCWeatherEventComponent.cs
@@ -0,0 +1,6 @@
+﻿namespace Content.Shared._RMC14.Weather;
+
+public sealed partial class RMCWeatherEventComponent : Component
+{
+    public string Name;
+}
