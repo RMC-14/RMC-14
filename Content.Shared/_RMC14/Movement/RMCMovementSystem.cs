72f8552acde653cd8727515972ebf34ac3bf66c7
DrSmugleaf <drsmugleaf@gmail.com>
2025-04-15 23:57:13 -0700

    Add resting with Shift+R

---
+++ Content.Shared/_RMC14/Movement/RMCMovementSystem.cs
@@ -0,0 +1,36 @@
+﻿using Content.Shared.Movement.Components;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics.Systems;
+
+namespace Content.Shared._RMC14.Movement;
+
+public sealed class RMCMovementSystem : EntitySystem
+{
+    [Dependency] private readonly FixtureSystem _fixture = default!;
+
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<RMCMobCollisionComponent, MapInitEvent>(OnMobCollisionMapInit);
+    }
+
+    private void OnMobCollisionMapInit(Entity<RMCMobCollisionComponent> ent, ref MapInitEvent args)
+    {
+        if (TryComp(ent, out MobCollisionComponent? collision))
+        {
+            collision.FixtureId = ent.Comp.FixtureId;
+            DirtyField(ent, collision, nameof(MobCollisionComponent.FixtureId));
+        }
+
+        if (!TryComp<PhysicsComponent>(ent, out var body))
+            return;
+
+        _fixture.TryCreateFixture(
+            ent,
+            ent.Comp.FixtureShape,
+            ent.Comp.FixtureId,
+            hard: false,
+            collisionMask: (int) ent.Comp.FixtureLayer,
+            body: body
+        );
+    }
+}
