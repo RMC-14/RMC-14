ffc46c1eefb334d9cff16c2b7a8fce66f8282471
DrSmugleaf <drsmugleaf@gmail.com>
2025-04-17 00:31:19 -0700

    Add royal smart gun operator

---
+++ Content.Server/_RMC14/Humanoid/RMCJobSpawnerComponent.cs
@@ -0,0 +1,15 @@
+﻿using Content.Shared.Roles;
+using Robust.Shared.Prototypes;
+
+namespace Content.Server._RMC14.Humanoid;
+
+[RegisterComponent]
+[Access(typeof(RMCHumanoidSystem))]
+public sealed partial class RMCJobSpawnerComponent : Component
+{
+    [DataField(required: true)]
+    public ProtoId<JobPrototype>? Job;
+
+    [DataField, AutoNetworkedField]
+    public bool Loadout = true;
+}
