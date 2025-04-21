d3c2cdd05fa422872323db6f1bb0fb49acb5a7a1
Dutch-VanDerLinde <koolthunder019@gmail.com>
2025-04-21 16:37:37 -0400

    fixes

--- Content.Shared/_RMC14/Damage/ObstacleSlamming/RMCObstacleSlamImmuneComponent.cs
+++ Content.Shared/_RMC14/Damage/ObstacleSlamming/RMCObstacleSlamImmuneComponent.cs
@@ -4,22 +4,15 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 namespace Content.Shared._RMC14.Damage.ObstacleSlamming;
 
 /// <summary>
-/// Gives a mob extra damage or stun on obstacle slam when this component is available.
-/// Useful for things like xeno abilities which stun when an ability causes obstacle slams.
+/// Makes a mob immune to obstacle slamming for a certain period of time.
 /// </summary>
 [Access(typeof(RMCObstacleSlammingSystem))]
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class RMCObstacleSlamBonusEffectsComponent : Component
+public sealed partial class RMCObstacleSlamImmuneComponent : Component
 {
     [DataField, AutoNetworkedField]
     public TimeSpan ExpireIn = TimeSpan.FromSeconds(1);
 
     [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField]
     public TimeSpan? ExpireAt;
-
-    [DataField, AutoNetworkedField]
-    public TimeSpan Stun = TimeSpan.FromSeconds(0);
-
-    [DataField, AutoNetworkedField]
-    public TimeSpan Slow = TimeSpan.FromSeconds(0);
 }
