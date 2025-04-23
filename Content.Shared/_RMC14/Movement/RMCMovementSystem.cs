d2ef6d0134f6363949b2d584c17d69c6b9671880
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2025-04-22 17:02:27 -0700

    Fix 4 performance issues, general fixes (#6244)

--- Content.Shared/_RMC14/Movement/RMCMovementSystem.cs
+++ Content.Shared/_RMC14/Movement/RMCMovementSystem.cs
@@ -1,4 +1,7 @@
-﻿using Content.Shared.Movement.Components;
+﻿using Content.Shared.Mobs;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.Movement.Components;
+using Robust.Shared.Network;
 using Robust.Shared.Physics.Components;
 using Robust.Shared.Physics.Systems;
 
@@ -7,10 +10,13 @@ namespace Content.Shared._RMC14.Movement;
 public sealed class RMCMovementSystem : EntitySystem
 {
     [Dependency] private readonly FixtureSystem _fixture = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly INetManager _net = default!;
 
     public override void Initialize()
     {
         SubscribeLocalEvent<RMCMobCollisionComponent, MapInitEvent>(OnMobCollisionMapInit);
+        SubscribeLocalEvent<RMCMobCollisionComponent, MobStateChangedEvent>(OnMobCollisionMobStateChanged);
     }
 
     private void OnMobCollisionMapInit(Entity<RMCMobCollisionComponent> ent, ref MapInitEvent args)
@@ -21,17 +27,41 @@ public sealed class RMCMovementSystem : EntitySystem
             DirtyField(ent, collision, nameof(MobCollisionComponent.FixtureId));
         }
 
+        if (_mobState.IsDead(ent))
+            return;
+
         if (!TryComp<PhysicsComponent>(ent, out var body))
+            return;
+
+        CreateMobCollisionFixture((ent, ent, body));
+    }
+
+    private void CreateMobCollisionFixture(Entity<RMCMobCollisionComponent, PhysicsComponent?> ent)
+    {
+        if (!Resolve(ent, ref ent.Comp2, false))
             return;
 
         _fixture.TryCreateFixture(
             ent,
-            ent.Comp.FixtureShape,
-            ent.Comp.FixtureId,
+            ent.Comp1.FixtureShape,
+            ent.Comp1.FixtureId,
             hard: false,
-            collisionLayer: (int) ent.Comp.FixtureLayer,
-            collisionMask: (int) ent.Comp.FixtureLayer,
-            body: body
+            collisionLayer: (int) ent.Comp1.FixtureLayer,
+            collisionMask: (int) ent.Comp1.FixtureLayer,
+            body: ent
         );
+    }
+
+    private void OnMobCollisionMobStateChanged(Entity<RMCMobCollisionComponent> ent, ref MobStateChangedEvent args)
+    {
+        switch (args.NewMobState)
+        {
+            case MobState.Alive:
+                CreateMobCollisionFixture(ent);
+                break;
+            case MobState.Dead:
+                _fixture.DestroyFixture(ent, ent.Comp.FixtureId);
+                break;
+        }
     }
 }
