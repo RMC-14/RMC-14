84fb1dfa706e6867107e5f50bbc2af772718512f
gandalf <gandalf@mordor.main>
2025-04-25 17:09:55 -0500

    Fire sentries offset correctly (most of the time)

---
+++ Content.Shared/_RMC14/Projectiles/RMCFireProjectileComponent.cs
@@ -0,0 +1,6 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Projectiles;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class RMCFireProjectileComponent : Component;
