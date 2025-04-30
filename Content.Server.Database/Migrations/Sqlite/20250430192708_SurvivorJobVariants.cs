758e27feb851235744a7e47b1c7cc139feffa800
Dutch-VanDerLinde <koolthunder019@gmail.com>
2025-04-30 15:31:24 -0400

    9

--- Content.Server.Database/Migrations/Sqlite/20250430192708_SurvivorJobVariants.cs
+++ Content.Server.Database/Migrations/Sqlite/20250430192708_SurvivorJobVariants.cs
@@ -5,25 +5,18 @@
 namespace Content.Server.Database.Migrations.Sqlite
 {
     /// <inheritdoc />
-    public partial class JobVariants : Migration
+    public partial class SurvivorJobVariants : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "preferred_variant",
-                table: "job",
-                type: "TEXT",
-                nullable: false,
-                defaultValue: "");
+
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "preferred_variant",
-                table: "job");
+
         }
     }
 }
