6672be51704fe4807135d8a2f8d5d00b250345b6
Dutch-VanDerLinde <koolthunder019@gmail.com>
2025-04-30 17:54:57 -0400

    j

---
+++ Content.Server.Database/Migrations/Postgres/20250430215238_SurvivorJobVariants.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class SurvivorJobVariants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "preferred_variant",
+                table: "job",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "preferred_variant",
+                table: "job",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "");
+        }
+    }
+}
--- Content.Server.Database/add-migration.ps1
+++ Content.Server.Database/add-migration.ps1
@@ -1,4 +1,12 @@
 ﻿#!/usr/bin/env pwsh
 
-dotnet ef migrations add --context SqliteServerDbContext -o Migrations/Sqlite "SurvivorJobVariants"
-dotnet ef migrations add --context PostgresServerDbContext -o Migrations/Postgres "SurvivorJobVariants"
+param([String]$name)
+
+if ($name -eq "")
+{
+    Write-Error "must specify migration name"
+    exit
+}
+
+dotnet ef migrations add --context SqliteServerDbContext -o Migrations/Sqlite $name
+dotnet ef migrations add --context PostgresServerDbContext -o Migrations/Postgres $name
