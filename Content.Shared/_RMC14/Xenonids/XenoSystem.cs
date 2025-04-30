56d38e2c95cf87908f746cb843f3c3cb23ff7323
Vermidia <vermidiathesnake@gmail.com>
2025-04-30 18:38:06 -0500

    Fix bleedout happening on fire/on weeds/as larva

--- Content.Shared/_RMC14/Xenonids/XenoSystem.cs
+++ Content.Shared/_RMC14/Xenonids/XenoSystem.cs
@@ -334,7 +334,7 @@ public sealed class XenoSystem : EntitySystem
 
     private void OnXenoRegenBeforeCritDamage(Entity<XenoRegenComponent> ent, ref DamageStateCritBeforeDamageEvent args)
     {
-        if (!_rmcFlammable.IsOnFire(ent.Owner) || (!ent.Comp.HealOffWeeds && !_weeds.IsOnWeeds(ent.Owner)))
+        if (!_rmcFlammable.IsOnFire(ent.Owner) && !ent.Comp.HealOffWeeds && !_weeds.IsOnWeeds(ent.Owner))
             return;
 
         //Don't take bleedout damage on fire or on weeds
