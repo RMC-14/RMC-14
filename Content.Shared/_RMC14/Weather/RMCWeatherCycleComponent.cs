f407148ee45d06ae00da0d88bf543cc24fbf0871
CyberTropic <CyberTropic@users.noreply.github.com>
2025-05-02 13:46:01 -0400

    Update RMCWeatherCycleComponent.cs

--- Content.Shared/_RMC14/Weather/RMCWeatherCycleComponent.cs
+++ Content.Shared/_RMC14/Weather/RMCWeatherCycleComponent.cs
@@ -9,11 +9,11 @@ namespace Content.Shared._RMC14.Weather;
 [Access(typeof(RMCWeatherSystem))]
 public sealed partial class RMCWeatherCycleComponent : Component
 {
-    [DataField, AutoNetworkedField]
-    public List<RMCWeatherEventComponent> WeatherEvents = new();
+    [DataField]
+    public List<RMCWeatherEvent> WeatherEvents = new();
 
-    [DataField, AutoNetworkedField]
-    public RMCWeatherEventComponent? CurrentEvent;
+    [DataField]
+    public RMCWeatherEvent? CurrentEvent;
 
     [DataField, AutoNetworkedField]
     public TimeSpan MinTimeBetweenEvents;
@@ -24,31 +24,31 @@ public sealed partial class RMCWeatherCycleComponent : Component
     [DataField, AutoNetworkedField]
     public TimeSpan LastEventCooldown;
 
-    [DataDefinition]
-    [Serializable, NetSerializable]
-    public partial struct RMCWeatherEventComponent()
-    {
-        [DataField, AutoNetworkedField]
-        public string Name = "rmcWeatherEvent";
+}
 
-        [DataField, AutoNetworkedField]
-        public TimeSpan Duration;
+[DataDefinition, Serializable, NetSerializable]
+public sealed partial class RMCWeatherEvent
+{
+    [DataField]
+    public string Name = "rmcWeatherEvent";
 
-        [DataField, AutoNetworkedField]
-        public ProtoId<WeatherPrototype> WeatherType;
+    [DataField]
+    public TimeSpan Duration;
 
-        [DataField, AutoNetworkedField]
-        public float LightningChance = 0.0f;
+    [DataField]
+    public ProtoId<WeatherPrototype> WeatherType;
 
-        [DataField, AutoNetworkedField]
-        public TimeSpan LightningDuration = TimeSpan.FromSeconds(2);
+    [DataField]
+    public float LightningChance = 1.0f;
 
-        [DataField, AutoNetworkedField]
-        public TimeSpan LightningCooldown = TimeSpan.FromSeconds(5);
+    [DataField]
+    public TimeSpan LightningDuration = TimeSpan.FromSeconds(2);
 
-        [DataField, AutoNetworkedField]
-        public List<string> LightningEffects = new(){"RMCColorSequenceLightningSharpPeak", "RMCColorSequenceLightningFlicker"};
-    }
+    [DataField]
+    public TimeSpan LightningCooldown = TimeSpan.FromSeconds(5);
+
+    [DataField]
+    public List<string> LightningEffects = new(){"RMCColorSequenceLightningSharpPeak", "RMCColorSequenceLightningFlicker"};
 }
 
 
