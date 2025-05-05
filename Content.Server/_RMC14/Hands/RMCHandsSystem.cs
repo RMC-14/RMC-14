9c73c4e7f4c838b251199ef000c4fd0512ae34f6
Dutch-VanDerLinde <koolthunder019@gmail.com>
2025-05-05 17:00:09 -0400

    add disarming and gun discharging

---
+++ Content.Server/_RMC14/Hands/RMCHandsSystem.cs
@@ -0,0 +1,14 @@
+﻿using Content.Server.Hands.Systems;
+using Robust.Shared.Map;
+
+namespace Content.Shared._RMC14.Hands;
+
+public sealed class RMCHandsSystem : SharedRMCHandsSystem
+{
+    [Dependency] private readonly HandsSystem _hands = default!;
+
+    public override bool ThrowHeldItem(EntityUid player, EntityCoordinates coordinates, float minDistance = 0.1f)
+    {
+        return _hands.ThrowHeldItem(player, coordinates, minDistance);
+    }
+}
