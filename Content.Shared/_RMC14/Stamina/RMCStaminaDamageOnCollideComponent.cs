61e0213fdb50692301fc719748a2cb5efb5d5861
Vermidia <vermidiathesnake@gmail.com>
2025-05-09 22:52:02 -0500

    Forgor register

--- Content.Shared/_RMC14/Stamina/RMCStaminaDamageOnCollideComponent.cs
+++ Content.Shared/_RMC14/Stamina/RMCStaminaDamageOnCollideComponent.cs
@@ -1,5 +1,8 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared._RMC14.Stamina;
 
+[RegisterComponent, NetworkedComponent]
 public sealed partial class RMCStaminaDamageOnCollideComponent : Component
 {
     [DataField]
