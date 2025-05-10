05404c8086a435f3f32574959d23eb6fb75a1731
Dutch-VanDerLinde <koolthunder019@gmail.com>
2025-05-09 22:50:47 -0400

    Update XenoEviscerateSystem.cs

--- Content.Shared/_RMC14/Xenonids/Eviscerate/XenoEviscerateSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Eviscerate/XenoEviscerateSystem.cs
@@ -13,7 +13,6 @@ using Content.Shared.Stunnable;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
-using Robust.Shared.Timing;
 
 namespace Content.Shared._RMC14.Xenonids.Eviscerate;
 
