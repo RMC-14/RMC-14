8af0623437b8ceb3182703b55150868ed81dec89
0tito <147736056+0tito@users.noreply.github.com>
2025-05-12 10:05:34 -0300

    Update Content.Shared/_RMC14/Xenonids/Plasma/XenoRemoteTransferPlasmaActionEvent.cs
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>

--- Content.Shared/_RMC14/Xenonids/Plasma/XenoRemoteTransferPlasmaActionEvent.cs
+++ Content.Shared/_RMC14/Xenonids/Plasma/XenoRemoteTransferPlasmaActionEvent.cs
@@ -2,7 +2,4 @@ using Content.Shared.Actions;
 
 namespace Content.Shared._RMC14.Xenonids.Plasma;
 
-public sealed partial class XenoRemoteTransferPlasmaActionEvent : EntityTargetActionEvent
-{
-
-}
+public sealed partial class XenoRemoteTransferPlasmaActionEvent : EntityTargetActionEvent;
