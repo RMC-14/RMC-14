29953ba1aede6ec459290bef05bcf9056a43638a
N0lim <0s7n888884@gmail.com>
2025-05-13 13:12:16 +0300

    Namespace formatting fixed

--- Content.Shared/_RMC14/Medical/Pain/PainComponent.cs
+++ Content.Shared/_RMC14/Medical/Pain/PainComponent.cs
@@ -4,80 +4,79 @@ using Content.Shared.EntityEffects;
 using Content.Shared.Alert;
 using Robust.Shared.Prototypes;
 
-namespace Content.Shared._RMC14.Medical.Pain
+namespace Content.Shared._RMC14.Medical.Pain;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class PainComponent : Component
 {
-    [RegisterComponent, NetworkedComponent]
-    public sealed partial class PainComponent : Component
+    /// <summary>
+    /// Pain value derived from overall damage to the body without modificators.
+    /// </summary>
+    [ViewVariables]
+    public FixedPoint2 CurrentPain = FixedPoint2.Zero;
+
+    /// <summary>
+    /// Pain value with all modificators and limited to 100.
+    /// </summary>
+    [ViewVariables]
+    public FixedPoint2 CurrentPainPercentage = FixedPoint2.Zero;
+
+    [ViewVariables]
+    public int CurrentPainLevel = 0;
+    [DataField]
+    public TimeSpan PainLevelUpdateRate = TimeSpan.FromSeconds(2);
+
+    [ViewVariables]
+    public TimeSpan LastPainLevelUpdateTime = new(0);
+
+    [DataField]
+    public TimeSpan EffectUpdateRate = TimeSpan.FromSeconds(1);
+
+    [ViewVariables]
+    public TimeSpan NextEffectUpdateTime = new(0);
+
+    [ViewVariables, Access(typeof(PainSystem))]
+    public List<PainModificator> PainModificators = [];
+
+    [DataField]
+    public FixedPoint2 PainReductionDecreaceRate = FixedPoint2.New(0.25);
+
+    [DataField]
+    public FixedPoint2 BrutePainMultiplier = FixedPoint2.New(1);
+
+    [DataField]
+    public FixedPoint2 BurnPainMultiplier = FixedPoint2.New(1.2);
+
+    [DataField]
+    public FixedPoint2 ToxinPainMultiplier = FixedPoint2.New(1.5);
+
+    [DataField]
+    public FixedPoint2 AirlossPainMultiplier = FixedPoint2.Zero;
+
+    [DataField(required: true, serverOnly: true)]
+    public List<EntityEffect> PainLevels = [];
+
+    [DataField]
+    public ProtoId<AlertPrototype> Alert = "Pain";
+}
+
+[DataDefinition]
+public sealed partial class PainModificator
+{
+    public TimeSpan Duration;
+    public FixedPoint2 EffectStrength;
+    public PainModificatorType Type;
+
+    public PainModificator(TimeSpan duration, FixedPoint2 strength, PainModificatorType type)
     {
-        /// <summary>
-        /// Pain value derived from overall damage to the body without modificators.
-        /// </summary>
-        [ViewVariables]
-        public FixedPoint2 CurrentPain = FixedPoint2.Zero;
-
-        /// <summary>
-        /// Pain value with all modificators and limited to 100.
-        /// </summary>
-        [ViewVariables]
-        public FixedPoint2 CurrentPainPercentage = FixedPoint2.Zero;
-
-        [ViewVariables]
-        public int CurrentPainLevel = 0;
-        [DataField]
-        public TimeSpan PainLevelUpdateRate = TimeSpan.FromSeconds(2);
-
-        [ViewVariables]
-        public TimeSpan LastPainLevelUpdateTime = new(0);
-
-        [DataField]
-        public TimeSpan EffectUpdateRate = TimeSpan.FromSeconds(1);
-
-        [ViewVariables]
-        public TimeSpan NextEffectUpdateTime = new(0);
-
-        [ViewVariables, Access(typeof(PainSystem))]
-        public List<PainModificator> PainModificators = [];
-
-        [DataField]
-        public FixedPoint2 PainReductionDecreaceRate = FixedPoint2.New(0.25);
-
-        [DataField]
-        public FixedPoint2 BrutePainMultiplier = FixedPoint2.New(1);
-
-        [DataField]
-        public FixedPoint2 BurnPainMultiplier = FixedPoint2.New(1.2);
-
-        [DataField]
-        public FixedPoint2 ToxinPainMultiplier = FixedPoint2.New(1.5);
-
-        [DataField]
-        public FixedPoint2 AirlossPainMultiplier = FixedPoint2.Zero;
-
-        [DataField(required: true, serverOnly: true)]
-        public List<EntityEffect> PainLevels = [];
-
-        [DataField]
-        public ProtoId<AlertPrototype> Alert = "Pain";
+        Duration = duration;
+        EffectStrength = strength;
+        Type = type;
     }
+}
 
-    [DataDefinition]
-    public sealed partial class PainModificator
-    {
-        public TimeSpan Duration;
-        public FixedPoint2 EffectStrength;
-        public PainModificatorType Type;
-
-        public PainModificator(TimeSpan duration, FixedPoint2 strength, PainModificatorType type)
-        {
-            Duration = duration;
-            EffectStrength = strength;
-            Type = type;
-        }
-    }
-
-    public enum PainModificatorType : byte
-    {
-        PainReduction,
-        PainIncrease,
-    }
+public enum PainModificatorType : byte
+{
+    PainReduction,
+    PainIncrease,
 }
