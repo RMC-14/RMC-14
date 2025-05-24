10e0386a0e6d7e8002c102ace149875f995f3624
chavonadelal <chavonadelal@mail.ru>
2025-05-24 23:36:29 +0300

    Revert changes in SpeechSoundSystem

--- Content.Server/Speech/SpeechNoiseSystem.cs
+++ Content.Server/Speech/SpeechNoiseSystem.cs
@@ -7,7 +7,6 @@ using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
 using Robust.Shared.Random;
-using Content.Shared._RMC14.Megaphone;
 
 namespace Content.Server.Speech
 {
@@ -73,11 +72,7 @@ namespace Content.Server.Speech
 
             var sound = GetSpeechSound((uid, component), args.Message);
             component.LastTimeSoundPlayed = currentTime;
-
-            if (HasComp<RMCMegaphoneUserComponent>(uid)) // RMC14 Megaphone allows you to hear without being able to see the sound source
-                _audio.PlayEntity(sound, Filter.Broadcast(), uid, recordReplay: true);
-            else
-                _audio.PlayPvs(sound, uid);
+            _audio.PlayPvs(sound, uid);
         }
     }
 }
