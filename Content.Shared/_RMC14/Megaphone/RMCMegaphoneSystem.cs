f4fef90a41eda6bd00aa29d6dd76107f7324b68e
chavonadelal <chavonadelal@mail.ru>
2025-06-01 03:03:30 +0300

    Increase character limit for megaphone messages

--- Content.Shared/_RMC14/Megaphone/RMCMegaphoneSystem.cs
+++ Content.Shared/_RMC14/Megaphone/RMCMegaphoneSystem.cs
@@ -22,7 +22,7 @@ public sealed class RMCMegaphoneSystem : EntitySystem
         args.Handled = true;
 
         var ev = new MegaphoneInputEvent(GetNetEntity(args.User));
-        _dialog.OpenInput(args.User, "Enter a message for the megaphone:", ev, largeInput: false, characterLimit: 100);
+        _dialog.OpenInput(args.User, "Enter a message for the megaphone:", ev, largeInput: false, characterLimit: 150);
     }
 
     private void OnExamined(Entity<RMCMegaphoneComponent> ent, ref ExaminedEvent args)
