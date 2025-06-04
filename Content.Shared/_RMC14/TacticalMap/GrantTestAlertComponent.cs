6a9e3330b91e990337cf028db5a059b32940501b
Özgür Akça <doguhanozgurakca@gmail.com>
2025-06-04 23:15:07 +0300

    inital

---
+++ Content.Shared/_RMC14/TacticalMap/GrantTestAlertComponent.cs
@@ -0,0 +1,12 @@
+using Content.Shared.Inventory;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.TacticalMap;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SharedTacticalMapSystem))]
+public sealed partial class GrantTestAlertComponent : Component, IClothingSlots
+{
+    [DataField, AutoNetworkedField]
+    public SlotFlags Slots { get; set; } = SlotFlags.EARS;
+}
