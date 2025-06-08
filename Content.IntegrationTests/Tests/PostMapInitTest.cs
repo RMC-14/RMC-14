749ef750fa1a74b02c1433be9a434d99102d5172
Noctis <thbanham@gmail.com>
2025-06-08 22:41:08 +0100

    fix

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -88,7 +88,7 @@ namespace Content.IntegrationTests.Tests
             "Savannah",
             "Almayer",
             "RMCAdminFax",
-            "Golden Arrow",
+            "GoldenArrow",
         };
 
         /// <summary>
