86f2cffd8f27dd0a735d8f3cadfed66ac9f0e1e6
Özgür Akça <doguhanozgurakca@gmail.com>
2025-06-09 20:09:56 +0300

    fixed the sound and properly added the opening sound

--- Content.Client/_RMC14/SpecialBox/AegisCrateSystem.cs
+++ Content.Client/_RMC14/SpecialBox/AegisCrateSystem.cs
@@ -1,6 +1,8 @@
 using Content.Shared._RMC14.AegisCrate;
 using Robust.Client.Animations;
+using Robust.Client.Audio;
 using Robust.Client.GameObjects;
+using Robust.Shared.Audio;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Timing;
 using System;
@@ -12,6 +14,8 @@ public sealed class AegisCrateSystem : EntitySystem
     private const string AnimationKey = "AegisCrateOpenAnim";
     private Animation? _openingAnimation;
 
+    [Dependency] private readonly AudioSystem _audio = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -22,7 +26,7 @@ public sealed class AegisCrateSystem : EntitySystem
 
         _openingAnimation = new Animation
         {
-            Length = TimeSpan.FromSeconds(1.2),
+            Length = TimeSpan.FromSeconds(1.46), // 0.96 + 0.5 = 1.46s
             AnimationTracks =
             {
                 new AnimationTrackSpriteFlick
@@ -31,7 +35,7 @@ public sealed class AegisCrateSystem : EntitySystem
                     KeyFrames =
                     {
                         new AnimationTrackSpriteFlick.KeyFrame("aegis_crate_opening", 0f),
-                        new AnimationTrackSpriteFlick.KeyFrame("aegis_crate_open", 1.2f)
+                        new AnimationTrackSpriteFlick.KeyFrame("aegis_crate_open", 1.46f)
                     }
                 }
             }
@@ -54,14 +58,16 @@ public sealed class AegisCrateSystem : EntitySystem
 
     private void OnStateChanged(EntityUid uid, AegisCrateComponent comp)
     {
-        if (comp.State == AegisCrateState.Opening)
+        if (comp.State == AegisCrateState.Opening && comp.OpenSound != null)
         {
+            _audio.PlayPvs(comp.OpenSound, uid, AudioParams.Default);
             var animPlayer = EntityManager.System<AnimationPlayerSystem>();
             if (!animPlayer.HasRunningAnimation(uid, AnimationKey))
             {
                 animPlayer.Play(uid, _openingAnimation!, AnimationKey);
             }
         }
+
     }
 
     private void OnAnimationCompleted(EntityUid uid, AegisCrateComponent comp, AnimationCompletedEvent args)
