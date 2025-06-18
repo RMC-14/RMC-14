defbcd48e13428801702f58c77119629309c8258
Arykh <181794112+Arykh@users.noreply.github.com>
2025-06-18 18:41:45 -0400

    uhh

--- Content.Shared/_RMC14/Xenonids/Rest/XenoRestActionEvent.cs
+++ Content.Shared/_RMC14/Xenonids/Rest/XenoRestActionEvent.cs
@@ -2,5 +2,4 @@
 
 namespace Content.Shared._RMC14.Xenonids.Rest;
 
-[ByRefEvent]
 public sealed partial class XenoRestActionEvent : InstantActionEvent;
--- Content.Shared/_RMC14/Xenonids/Rest/XenoRestBindInputSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Rest/XenoRestBindInputSystem.cs
@@ -1,10 +1,16 @@
 using Content.Shared._RMC14.Input;
+using Content.Shared.Actions;
 using Robust.Shared.Input.Binding;
+using Robust.Shared.Timing;
 
 namespace Content.Shared._RMC14.Xenonids.Rest;
 
 public sealed class XenoRestBindInputSystem : EntitySystem
 {
+    [Dependency] private readonly SharedActionsSystem _actions = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly IEntityManager _entMan = default!;
+
     public override void Initialize()
     {
         base.Initialize();
@@ -16,8 +22,22 @@ public sealed class XenoRestBindInputSystem : EntitySystem
                         var ent = session?.AttachedEntity;
                         if (ent != null && HasComp<XenoComponent>(ent.Value))
                         {
-                            var ev = new XenoRestActionEvent();
-                            RaiseLocalEvent(ent.Value, ref ev);
+                            const string restActionPrototypeId = "ActionXenoRest";
+
+                            if (TryFindActionByPrototype(ent.Value,
+                                    restActionPrototypeId,
+                                    out var actionId,
+                                    out var comp))
+                            {
+                                _actions.PerformAction(
+                                    ent.Value,
+                                    null,
+                                    actionId,
+                                    comp,
+                                    null,
+                                    _timing.CurTime
+                                );
+                            }
                         }
                     },
                     handle: false
@@ -29,5 +49,25 @@ public sealed class XenoRestBindInputSystem : EntitySystem
     {
         base.Shutdown();
         CommandBinds.Unregister<XenoRestBindInputSystem>();
+    }
+
+    private bool TryFindActionByPrototype(EntityUid owner,
+        string prototypeId,
+        out EntityUid actionId,
+        out BaseActionComponent comp)
+    {
+        foreach (var (actId, actComp) in _actions.GetActions(owner))
+        {
+            var meta = _entMan.GetComponent<MetaDataComponent>(actId);
+            if (meta.EntityPrototype != null && meta.EntityPrototype.ID == prototypeId)
+            {
+                actionId = actId;
+                comp = actComp;
+                return true;
+            }
+        }
+        actionId = default;
+        comp = default!;
+        return false;
     }
 }
