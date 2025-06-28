588aae7674ec33ecadfaf2a1962413b8cd07c0be
noctis <99278536+noctyrnal@users.noreply.github.com>
2025-06-28 18:06:35 +0100

    Update Content.Shared/_RMC14/Holiday/RMCCustomHolidayComponent.cs
    Co-authored-by: Mr. 27 <45323883+Dutch-VanDerLinde@users.noreply.github.com>

--- Content.Shared/_RMC14/Holiday/RMCCustomHolidayComponent.cs
+++ Content.Shared/_RMC14/Holiday/RMCCustomHolidayComponent.cs
@@ -5,9 +5,18 @@ namespace Content.Shared._RMC14.RMCCustomHoliday;
 [Prototype]
 public sealed class CustomHolidayPrototype : IPrototype
 {
-    [IdDataField] public string ID { get; private set; } = default!;
-    [DataField] public string Name { get; private set; } = default!;
-    [DataField] public int BeginDay { get; private set; }
-    [DataField] public string BeginMonth { get; private set; } = default!;
-    [DataField] public string Description { get; private set; } = default!;
+    [IdDataField] 
+    public string ID { get; private set; } = default!;
+    
+    [DataField] 
+    public string Name { get; private set; } = default!;
+    
+    [DataField] 
+    public int BeginDay { get; private set; }
+    
+    [DataField] 
+    public string BeginMonth { get; private set; } = default!;
+    
+    [DataField] 
+    public string Description { get; private set; } = default!;
 }
