ff18c9d7fdfad50d48fbd5f70996cc74c2ba7500
John 117 <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2025-07-06 13:15:23 -0400

    k

--- Content.Shared/_RMC14/Xenonids/Construction/SharedXenoConstructionSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Construction/SharedXenoConstructionSystem.cs
@@ -661,7 +661,7 @@ public sealed class SharedXenoConstructionSystem : EntitySystem
     {
         var user = args.User;
         var plasmaLeft = xenoStructure.Comp.PlasmaCost - xenoStructure.Comp.StoredPlasma;
-        if (!TryComp(user, out XenoConstructionComponent? xeno) ||
+        if (!TryComp(user, out XenoCanAddPlasmaToConstructComponent? xeno) ||
             plasmaLeft < FixedPoint2.Zero ||
             !TryComp(xenoStructure, out TransformComponent? xenoStructureTransform) ||
             !TryComp(user, out XenoPlasmaComponent? plasma) ||
@@ -677,7 +677,7 @@ public sealed class SharedXenoConstructionSystem : EntitySystem
             return;
         }
 
-        if (!InRangePopup(user, xenoStructureTransform.Coordinates, xeno.OrderConstructionRange.Float()))
+        if (!InRangePopup(user, xenoStructureTransform.Coordinates, xeno.Range.Float()))
             return;
 
         if (plasma.Plasma < 1)
---
+++ Content.Shared/_RMC14/Xenonids/Construction/XenoCanAddPlasmaToConstructComponent.cs
@@ -0,0 +1,13 @@
+using Content.Shared._RMC14.Xenonids.Construction.ResinWhisper;
+using Content.Shared.FixedPoint;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Xenonids.Construction;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
+[Access(typeof(SharedXenoConstructionSystem), typeof(ResinWhispererSystem))]
+public sealed partial class XenoCanAddPlasmaToConstructComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public FixedPoint2 Range = 1.75;
+}
