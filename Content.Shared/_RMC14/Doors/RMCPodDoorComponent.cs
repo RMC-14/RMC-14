377d7865f73a222ab81f301e753995115afa2597
Noctis <thbanham@gmail.com>
2025-07-07 19:57:23 +0100

    Update RMCPodDoorComponent.cs

--- Content.Shared/_RMC14/Doors/RMCPodDoorComponent.cs
+++ Content.Shared/_RMC14/Doors/RMCPodDoorComponent.cs
@@ -4,6 +4,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._RMC14.Doors;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(CMDoorSystem))]
 public sealed partial class RMCPodDoorComponent : Component
 {
     [DataField, AutoNetworkedField]
