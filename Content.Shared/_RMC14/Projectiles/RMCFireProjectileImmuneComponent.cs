bd31557ca23d3544b75e64fe412760f7d9387827
Magicocity1 <146776935+Magicocity1@users.noreply.github.com>
2025-07-07 16:50:33 -0500

    Delete Content.Shared/_RMC14/Projectiles/RMCFireProjectileImmuneComponent.cs

--- Content.Shared/_RMC14/Projectiles/RMCFireProjectileImmuneComponent.cs
+++
@@ -1,6 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._RMC14.Projectiles;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class RMCFireProjectileImmuneComponent : Component;
