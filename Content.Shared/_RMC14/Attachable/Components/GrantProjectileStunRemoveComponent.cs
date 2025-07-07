5384dd2049458bdff0af9ef38202d516af0ef246
FarmWizard <portalpwned@gmail.com>
2025-07-07 15:29:46 -0400

    Update Content.Shared/_RMC14/Attachable/Components/GrantProjectileStunRemoveComponent.cs
    Fix to missing partial in component.
    
    Co-authored-by: Arendian <137322659+Arendian@users.noreply.github.com>

--- Content.Shared/_RMC14/Attachable/Components/GrantProjectileStunRemoveComponent.cs
+++ Content.Shared/_RMC14/Attachable/Components/GrantProjectileStunRemoveComponent.cs
@@ -4,6 +4,6 @@ namespace Content.Shared._RMC14.Attachable.Components;
 
 [RegisterComponent] [NetworkedComponent]
 
-public sealed class GrantProjectileStunRemoveComponent : Component
+public sealed partial class GrantProjectileStunRemoveComponent : Component
 {
 }
