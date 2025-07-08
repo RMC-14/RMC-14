44e258b458aa729c3d5f8f81f428101c54982ad3
DrSmugleaf <drsmugleaf@gmail.com>
2025-07-07 18:05:47 -0700

    Predict the health scanner UI

---
+++ Content.Server/_RMC14/Temperature/RMCTemperatureSystem.cs
@@ -0,0 +1,19 @@
+﻿using Content.Server.Temperature.Components;
+using Content.Shared._RMC14.Temperature;
+
+namespace Content.Server._RMC14.Temperature;
+
+public sealed class RMCTemperatureSystem : SharedRMCTemperatureSystem
+{
+    public override bool TryGetCurrentTemperature(EntityUid uid, out float temperature)
+    {
+        if (!TryComp(uid, out TemperatureComponent? temperatureComp))
+        {
+            temperature = 0;
+            return true;
+        }
+
+        temperature = temperatureComp.CurrentTemperature;
+        return false;
+    }
+}
---
+++ Content.Shared/_RMC14/Temperature/SharedRMCTemperatureSystem.cs
@@ -0,0 +1,14 @@
+﻿using Content.Shared.Atmos;
+using Content.Shared.Temperature;
+
+namespace Content.Shared._RMC14.Temperature;
+
+public abstract class SharedRMCTemperatureSystem : EntitySystem
+{
+    public virtual bool TryGetCurrentTemperature(EntityUid uid, out float temperature)
+    {
+        // TODO RMC14
+        temperature = TemperatureHelpers.CelsiusToKelvin(Atmospherics.NormalBodyTemperature);
+        return true;
+    }
+}
