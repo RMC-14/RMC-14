aa0c328543787c6434515ec119baaf6ad9fdb025
SurfinNinja1 <donivanbiggs@gmail.com>
2025-07-08 21:42:14 -0600

    updated processor UI

--- Content.Client/_RMC14/UserInterface/FoodProcessorUI/ProcessorBoundUserInterface.cs
+++ Content.Client/_RMC14/UserInterface/FoodProcessorUI/ProcessorBoundUserInterface.cs
@@ -1,9 +1,15 @@
+using Content.Client._RMC14.UserInterface.FoodProcessorUI;
 using Content.Shared._RMC14.Kitchen.Components;
 using Content.Shared.Chemistry.Reagent;
+using Content.Shared.Kitchen.Components;
 using JetBrains.Annotations;
+using Robust.Client.GameObjects;
+using Robust.Client.Graphics;
 using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Timing;
 
-namespace Content.Client._RMC14.UserInterface.FoodProcessorUI
+namespace Content.Client.Kitchen.UI
 {
     [UsedImplicitly]
     public sealed class ProcessorBoundUserInterface : BoundUserInterface
@@ -26,8 +32,12 @@ namespace Content.Client._RMC14.UserInterface.FoodProcessorUI
             base.Open();
             _menu = this.CreateWindow<ProcessorMenu>();
             _menu.StartButton.OnPressed += _ => SendPredictedMessage(new ProcessorStartCookMessage());
+            _menu.EjectButton.OnPressed += _ => SendPredictedMessage(new ProcessorEjectMessage());
+            _menu.IngredientsList.OnItemSelected += args =>
+            {
+                SendPredictedMessage(new ProcessorEjectSolidIndexedMessage(EntMan.GetNetEntity(_solids[args.ItemIndex])));
+            };
 
-            SendPredictedMessage(new ProcessorSelectCookTimeMessage(5, 0));
         }
 
         protected override void UpdateState(BoundUserInterfaceState state)
@@ -39,17 +49,57 @@ namespace Content.Client._RMC14.UserInterface.FoodProcessorUI
             }
 
             _menu.IsBusy = cState.IsProcessorBusy;
-            _menu.CurrentCooktimeEnd = cState.CurrentCookTimeEnd;
 
             _menu.ToggleBusyDisableOverlayPanel(cState.IsProcessorBusy || cState.ContainedSolids.Length == 0);
+            // TODO move this to a component state and ensure the net ids.
+            RefreshContentsDisplay(EntMan.GetEntityArray(cState.ContainedSolids));
 
-            //Set the cook time info label
-            var cookTime = cState.ActiveButtonIndex == 0
-                ? Loc.GetString("processor-menu-instant-button")
-                : cState.CurrentCookTime.ToString();
+            //Set the "micowave light" ui color to indicate if the processor is busy or not
+            if (cState.IsProcessorBusy && cState.ContainedSolids.Length > 0)
+            {
+                _menu.IngredientsPanel.PanelOverride = new StyleBoxFlat { BackgroundColor = Color.FromHex("#947300") };
+            }
+            else
+            {
+                _menu.IngredientsPanel.PanelOverride = new StyleBoxFlat { BackgroundColor = Color.FromHex("#1B1B1E") };
+            }
+        }
 
-            _menu.StartButton.Disabled = cState.IsProcessorBusy || cState.ContainedSolids.Length == 0;
+        private void RefreshContentsDisplay(EntityUid[] containedSolids)
+        {
+            _reagents.Clear();
 
+            if (_menu == null) return;
+
+            _solids.Clear();
+            _menu.IngredientsList.Clear();
+            foreach (var entity in containedSolids)
+            {
+                if (EntMan.Deleted(entity))
+                {
+                    return;
+                }
+
+                // TODO just use sprite view
+
+                Texture? texture;
+                if (EntMan.TryGetComponent<IconComponent>(entity, out var iconComponent))
+                {
+                    texture = EntMan.System<SpriteSystem>().GetIcon(iconComponent);
+                }
+                else if (EntMan.TryGetComponent<SpriteComponent>(entity, out var spriteComponent))
+                {
+                    texture = spriteComponent.Icon?.Default;
+                }
+                else
+                {
+                    continue;
+                }
+
+                var solidItem = _menu.IngredientsList.AddItem(EntMan.GetComponent<MetaDataComponent>(entity).EntityName, texture);
+                var solidIndex = _menu.IngredientsList.IndexOf(solidItem);
+                _solids.Add(solidIndex, entity);
+            }
         }
     }
 }
