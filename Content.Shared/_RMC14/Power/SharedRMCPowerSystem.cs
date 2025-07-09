d8afe1d588f751278f36ebcf955cdb8c13a325cf
Dutch_VanDerLinde <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2025-07-09 00:55:47 -0400

    inital

--- Content.Shared/_RMC14/Power/SharedRMCPowerSystem.cs
+++ Content.Shared/_RMC14/Power/SharedRMCPowerSystem.cs
@@ -55,6 +55,7 @@ public abstract class SharedRMCPowerSystem : EntitySystem
     private EntityQuery<RMCApcComponent> _apcQuery;
     private EntityQuery<AppearanceComponent> _appearanceQuery;
     private EntityQuery<RMCAreaPowerComponent> _areaPowerQuery;
+    private EntityQuery<AreaComponent> _areaQuery;
     private EntityQuery<RMCPowerReceiverComponent> _powerReceiverQuery;
 
     public override void Initialize()
@@ -62,6 +63,7 @@ public abstract class SharedRMCPowerSystem : EntitySystem
         _apcQuery = GetEntityQuery<RMCApcComponent>();
         _appearanceQuery = GetEntityQuery<AppearanceComponent>();
         _areaPowerQuery = GetEntityQuery<RMCAreaPowerComponent>();
+        _areaQuery = GetEntityQuery<AreaComponent>();
         _powerReceiverQuery = GetEntityQuery<RMCPowerReceiverComponent>();
 
         SubscribeLocalEvent<RMCApcComponent, MapInitEvent>(OnApcUpdate);
@@ -739,6 +741,9 @@ public abstract class SharedRMCPowerSystem : EntitySystem
     {
         if (!_areaPowerQuery.Resolve(area, ref area.Comp, false))
             return false;
+
+        if (_areaQuery.TryComp(area, out var areaComponent) && areaComponent.AlwaysPowered)
+            return true;
 
         foreach (var apcId in area.Comp.Apcs)
         {
