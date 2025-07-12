0b6e431e8efd8e15864e7a9dd7805cee539a9977
Özgür Akça <doguhanozgurakca@gmail.com>
2025-07-12 10:09:11 +0300

    Update Content.Client/Fax/UI/FaxWindow.xaml.cs
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>

--- Content.Client/Fax/UI/FaxWindow.xaml.cs
+++ Content.Client/Fax/UI/FaxWindow.xaml.cs
@@ -36,7 +36,7 @@ public sealed partial class FaxWindow : DefaultWindow
         SendButton.OnPressed += _ => SendButtonPressed?.Invoke();
         RefreshButton.OnPressed += _ => RefreshButtonPressed?.Invoke();
         PeerSelector.OnItemSelected += args =>
-            PeerSelected?.Invoke((string)args.Button.GetItemMetadata(args.Id)!);
+            PeerSelected?.Invoke((string) args.Button.GetItemMetadata(args.Id)!);
     }
 
     public void UpdateState(FaxUiState state)
