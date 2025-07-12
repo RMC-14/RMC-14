e08adde77aa593e1a0792b33c3a70d098b94f20b
Özgür Akça <doguhanozgurakca@gmail.com>
2025-07-12 13:29:38 +0300

    fix final

--- Content.Server/_RMC14/Fax/RMCFaxSystem.cs
+++ Content.Server/_RMC14/Fax/RMCFaxSystem.cs
@@ -2,8 +2,8 @@ using Content.Server.Fax;
 using Content.Shared.Administration.Logs;
 using Content.Shared.Fax;
 using Content.Shared.Fax.Components;
+using Content.Shared.Fax.Systems;
 using Content.Shared._RMC14.Fax;
-using Content.Shared._RMC14.Fax.Systems;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Paper;
 using Content.Shared.Labels.Components;
@@ -15,34 +15,22 @@ namespace Content.Server._RMC14.Fax;
 
 public sealed class RMCFaxSystem : EntitySystem
 {
-    [Dependency] private readonly RMCFaxecuteSystem _faxecute = default!;
     [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
     [Dependency] private readonly FaxSystem _faxSystem = default!;
+    [Dependency] private readonly FaxecuteSystem _faxecute = default!;
 
     public override void Initialize()
     {
         base.Initialize();
         
         SubscribeLocalEvent<FaxMachineComponent, RMCFaxCopyMultipleMessage>(OnCopyMultipleButtonPressed);
-        
-
-    }
-
-    public bool TryFaxecuteMob(EntityUid uid, FaxMachineComponent component)
-    {
-        if (HasComp<MobStateComponent>(component.PaperSlot.Item))
-        {
-            _faxecute.Faxecute(uid, component);
-            return true;
-        }
-        return false;
     }
 
     private void OnCopyMultipleButtonPressed(EntityUid uid, FaxMachineComponent component, RMCFaxCopyMultipleMessage args)
     {
         if (HasComp<MobStateComponent>(component.PaperSlot.Item))
         {
-            _faxecute.Faxecute(uid, component); // when button pressed it will hurt the mob.
+            _faxecute.Faxecute(uid, component);
         }
         else
         {
@@ -66,7 +54,6 @@ public sealed class RMCFaxSystem : EntitySystem
         TryComp<LabelComponent>(sendEntity, out var labelComponent);
         TryComp<NameModifierComponent>(sendEntity, out var nameMod);
 
-        // TODO: See comment in upstream 'Send()' about not being able to copy whole entities
         var printout = new FaxPrintout(paper.Content,
                                        nameMod?.BaseName ?? metadata.EntityName,
                                        labelComponent?.CurrentLabel,
@@ -87,7 +74,7 @@ public sealed class RMCFaxSystem : EntitySystem
 
         _adminLogger.Add(LogType.Action,
             LogImpact.Low,
-            $"RMC user added copy x{args.Copies} job to \"{component.FaxName}\" {ToPrettyString(uid):tool} " +
+            $"User added copy x{args.Copies} job to \"{component.FaxName}\" {ToPrettyString(uid):tool} " +
             $"of {ToPrettyString(sendEntity):subject}: {printout.Content}");
     }
 
