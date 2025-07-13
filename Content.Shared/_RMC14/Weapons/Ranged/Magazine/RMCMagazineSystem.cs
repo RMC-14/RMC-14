55a3c261a47831af860c1c4c3e53efa4b1d8f6fd
Dutch_VanDerLinde <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2025-07-13 16:17:28 -0400

    yeah

--- Content.Shared/_RMC14/Weapons/Ranged/Magazine/RMCMagazineSystem.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/Magazine/RMCMagazineSystem.cs
@@ -1,5 +1,3 @@
-using System.Linq;
-using Content.Shared.Weapons.Ranged.Components;
 using Content.Shared.Weapons.Ranged.Events;
 using Content.Shared.Weapons.Ranged.Systems;
 using Robust.Shared.Containers;
@@ -9,7 +7,6 @@ namespace Content.Shared._RMC14.Weapons.Ranged.Magazine;
 public sealed class RMCMagazineSystem : EntitySystem
 {
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-    [Dependency] private readonly SharedContainerSystem _container = default!;
 
     public override void Initialize()
     {
