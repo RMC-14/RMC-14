d04b4d083bb17f6b10f1f95a72ada82bcbf2bcb3
N0lim <0s7n888884@gmail.com>
2025-07-17 12:00:32 +0300

    PainComponent networking added

--- Content.Shared/_RMC14/Medical/Pain/PainComponent.cs
+++ Content.Shared/_RMC14/Medical/Pain/PainComponent.cs
@@ -3,64 +3,67 @@ using Content.Shared.FixedPoint;
 using Content.Shared.EntityEffects;
 using Content.Shared.Alert;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 
 namespace Content.Shared._RMC14.Medical.Pain;
 
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState(fieldDeltas: true), AutoGenerateComponentPause]
 public sealed partial class PainComponent : Component
 {
     /// <summary>
     /// Pain value derived from overall damage to the body without modificators.
     /// </summary>
-    [ViewVariables]
+    [ViewVariables, AutoNetworkedField]
     public FixedPoint2 CurrentPain = FixedPoint2.Zero;
 
     /// <summary>
     /// Pain value with all modificators and limited to 100.
     /// </summary>
-    [ViewVariables]
+    [ViewVariables, AutoNetworkedField]
     public FixedPoint2 CurrentPainPercentage = FixedPoint2.Zero;
 
-    [ViewVariables]
+    [ViewVariables, AutoNetworkedField]
     public int CurrentPainLevel = 0;
-    [DataField]
+    [DataField, AutoNetworkedField]
     public TimeSpan PainLevelUpdateRate = TimeSpan.FromSeconds(2);
 
-    [ViewVariables]
+    [ViewVariables, AutoNetworkedField]
     public TimeSpan LastPainLevelUpdateTime = new(0);
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public TimeSpan EffectUpdateRate = TimeSpan.FromSeconds(1);
 
-    [ViewVariables]
+    [DataField(customTypeSerializer: typeof(TimeOffsetSerializer)), AutoNetworkedField]
+    [AutoPausedField]
     public TimeSpan NextEffectUpdateTime = new(0);
 
-    [ViewVariables, Access(typeof(PainSystem))]
+    [ViewVariables, Access(typeof(PainSystem)), AutoNetworkedField]
     public List<PainModificator> PainModificators = [];
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public FixedPoint2 PainReductionDecreaceRate = FixedPoint2.New(0.25);
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public FixedPoint2 BrutePainMultiplier = FixedPoint2.New(1);
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public FixedPoint2 BurnPainMultiplier = FixedPoint2.New(1.2);
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public FixedPoint2 ToxinPainMultiplier = FixedPoint2.New(1.5);
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public FixedPoint2 AirlossPainMultiplier = FixedPoint2.Zero;
 
-    [DataField(required: true, serverOnly: true)]
+    [DataField(required: true, serverOnly: true), AutoNetworkedField]
     public List<EntityEffect> PainLevels = [];
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public ProtoId<AlertPrototype> Alert = "Pain";
 }
 
-[DataDefinition]
+[DataDefinition, Serializable, NetSerializable]
 public sealed partial class PainModificator
 {
     public TimeSpan Duration;
