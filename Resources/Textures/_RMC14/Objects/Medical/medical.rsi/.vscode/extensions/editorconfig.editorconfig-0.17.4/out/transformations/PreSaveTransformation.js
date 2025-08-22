255e696777a1b2632ebad49186931f7c4880bf5f
GitHubUser53123 <110841413+GitHubUser53123@users.noreply.github.com>
2025-08-22 19:03:15 +0400

    help

--- Resources/Textures/_RMC14/Objects/Medical/medical.rsi/.vscode/extensions/editorconfig.editorconfig-0.17.4/out/transformations/PreSaveTransformation.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PreSaveTransformation = void 0;
-class PreSaveTransformation {
-}
-exports.PreSaveTransformation = PreSaveTransformation;
-//# sourceMappingURL=PreSaveTransformation.js.map
--- Resources/Textures/_RMC14/Objects/Medical/medical.rsi/.vscode/extensions/ms-dotnettools.csdevkit-1.41.11-win32-x64/components/servicehub/README.md
+++
@@ -1,4 +0,0 @@
-This directory contains ServiceHub host configuration files
-because the ones that are included in the inserted host NPM packages
-are inappropriate for use by a xplat extension that uses a dynamically
-placed .NET runtime.
