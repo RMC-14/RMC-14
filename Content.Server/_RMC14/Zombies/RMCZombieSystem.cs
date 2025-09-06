f448152622870efc5d74b50025dd313ac2beb985
GitHubUser53123 <110841413+GitHubUser53123@users.noreply.github.com>
2025-09-06 18:08:05 +0400

    a

--- Content.Server/_RMC14/Zombies/RMCZombieSystem.cs
+++ Content.Server/_RMC14/Zombies/RMCZombieSystem.cs
@@ -55,8 +55,7 @@ public sealed partial class RMCZombieSystem : EntitySystem
                 }
             };
             zombieComponent.PassiveHealingCritMultiplier = 1.5f;
-            zombieComponent.ZombieMovementSpeedDebuff = 0.5f; // rnmc change - used to be 0.8
-        };
+            zombieComponent.ZombieMovementSpeedDebuff = 0.80f;
 
         var accentType = "RMCZombie";
         if (TryComp<ZombieAccentOverrideComponent>(target, out var accent))
