ad9481416b8a846dc286220b1c40db1abb75996a
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2025-09-06 01:50:53 -0700

    Update Content.Shared/_RMC14/Camera/RMCStaticZoomLevelComponent.cs

--- Content.Shared/_RMC14/Camera/RMCStaticZoomLevelComponent.cs
+++ Content.Shared/_RMC14/Camera/RMCStaticZoomLevelComponent.cs
@@ -1,6 +1,8 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared._RMC14.Camera;
 
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class RMCStaticZoomLevelComponent : Component
 {
 
