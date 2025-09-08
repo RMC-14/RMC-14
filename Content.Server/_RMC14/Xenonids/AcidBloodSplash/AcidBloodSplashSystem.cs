d250d0ee44ab6d6705c74a384f50ff1b89151b4a
N0lim <0s7n888884@gmail.com>
2025-09-08 07:10:47 +0300

    slash changed to acid

--- Content.Server/_RMC14/Xenonids/AcidBloodSplash/AcidBloodSplashSystem.cs
+++ Content.Server/_RMC14/Xenonids/AcidBloodSplash/AcidBloodSplashSystem.cs
@@ -86,7 +86,7 @@ public sealed class AcidBloodSplashSystem : EntitySystem
             if (_random.NextFloat() > hitProbability)
                 continue;
 
-            _damageable.TryChangeDamage(target, comp.Damage, origin: uid, tool: uid);
+            _damageable.TryChangeDamage(target, _xeno.TryApplyXenoAcidDamageMultiplier(target, comp.Damage), origin: uid, tool: uid);
             comp.NextSplashAvailable = _timing.CurTime + comp.SplashCooldown;
             i++;
 
