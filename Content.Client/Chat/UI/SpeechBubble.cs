594b718278d6c96ce99d0a0ec3765934f0967b35
BramvanZijp <56019239+BramvanZijp@users.noreply.github.com>
2025-09-13 15:53:08 +0200

    Apply requested changes.

--- Content.Client/Chat/UI/SpeechBubble.cs
+++ Content.Client/Chat/UI/SpeechBubble.cs
@@ -251,9 +251,7 @@ namespace Content.Client.Chat.UI
             {
                 if (message.SpeechStyleClass != null)
                     speechStyleClass = message.SpeechStyleClass;
-                else if (entityManager.HasComponent<SquadLeaderComponent>(senderUid) || entityManager.HasComponent<HiveLeaderComponent>(senderUid))
-                    speechStyleClass = "commanderSpeech";
-                else if (entityManager.HasComponent<XenoWordQueenComponent>(senderUid) || entityManager.HasComponent<InnateCommandSpeechComponent>(senderUid))
+                else if (entityManager.HasComponent<SquadLeaderComponent>(senderUid) || entityManager.HasComponent<HiveLeaderComponent>(senderUid) || entityManager.HasComponent<InnateCommandSpeechComponent>(senderUid))
                     speechStyleClass = "commanderSpeech";
             }
 
