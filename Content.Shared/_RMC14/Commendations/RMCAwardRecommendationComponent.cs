4707b2d41bf8128dc62b4805ca5df5a988e205ea
chavonadelal <chavonadelal@mail.ru>
2025-09-29 18:41:13 +0300

    Aded RMCAwardRecommendationComponent

---
+++ Content.Shared/_RMC14/Commendations/RMCAwardRecommendationComponent.cs
@@ -0,0 +1,32 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Commendations;
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(SharedCommendationSystem))]
+public sealed partial class RMCAwardRecommendationComponent : Component
+{
+    /// <summary>
+    /// How many recommendations this entity can give out.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int MaxRecommendations = 2;
+
+    /// <summary>
+    /// How many recommendations this entity has given out.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public int RecommendationsGiven;
+
+    /// <summary>
+    /// Whether to not block the ability to recommend awards, preventing <c>CanRecommend</c> = false.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool CanAlwaysRecommend;
+
+    /// <summary>
+    /// Whether this entity can recommend awards.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public bool CanRecommend = true;
+}
