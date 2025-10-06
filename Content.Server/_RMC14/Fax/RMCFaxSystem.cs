764ba7d4eee659beef4e2a6912b25f917f8d8946
Özgür Akça <doguhanozgurakca@gmail.com>
2025-10-06 12:15:21 +0300

    loop

--- Content.Server/_RMC14/Fax/RMCFaxSystem.cs
+++ Content.Server/_RMC14/Fax/RMCFaxSystem.cs
@@ -1,21 +1,14 @@
 using Content.Server.Fax;
-using Content.Shared.Administration.Logs;
 using Content.Shared.Fax;
 using Content.Shared.Fax.Components;
-using Content.Shared.Fax.Systems;
 using Content.Shared._RMC14.Fax;
 using Content.Shared.Mobs.Components;
-using Content.Shared.Paper;
-using Content.Shared.Labels.Components;
-using Content.Shared.NameModifier.Components;
-using Content.Shared.Database;
 
 namespace Content.Server._RMC14.Fax;
 
 
 public sealed class RMCFaxSystem : EntitySystem
 {
-    [Dependency] private readonly ISharedAdminLogManager _adminLogger = default!;
     [Dependency] private readonly FaxSystem _faxSystem = default!;
     [Dependency] private readonly FaxecuteSystem _faxecute = default!;
 
@@ -34,46 +27,12 @@ public sealed class RMCFaxSystem : EntitySystem
         }
         else
         {
-            CopyMultiple((uid, component), args);
+            // Call the existing Copy method multiple times instead of reimplementing the logic
+            for (int i = 0; i < args.Copies; i++)
+            {
+                _faxSystem.Copy(uid, component, new FaxCopyMessage());
+            }
         }
-    }
-
-    private void CopyMultiple(Entity<FaxMachineComponent> ent, RMCFaxCopyMultipleMessage args)
-    {
-        if (ent.Comp.SendTimeoutRemaining > 0)
-            return;
-
-        var sendEntity = ent.Comp.PaperSlot.Item;
-        if (sendEntity == null)
-            return;
-
-        if (!TryComp(sendEntity, out MetaDataComponent? metadata) ||
-            !TryComp<PaperComponent>(sendEntity, out var paper))
-            return;
-
-        TryComp<LabelComponent>(sendEntity, out var labelComponent);
-        TryComp<NameModifierComponent>(sendEntity, out var nameMod);
-
-        var printout = new FaxPrintout(paper.Content,
-                                       nameMod?.BaseName ?? metadata.EntityName,
-                                       labelComponent?.CurrentLabel,
-                                       metadata.EntityPrototype?.ID ?? ent.Comp.PrintPaperId,
-                                       paper.StampState,
-                                       paper.StampedBy,
-                                       paper.EditingDisabled);
-
-        for (int i = 0; i < args.Copies; i++)
-        {
-            ent.Comp.PrintingQueue.Enqueue(printout);
-        }
-        ent.Comp.SendTimeoutRemaining += ent.Comp.SendTimeout;
-
-        _faxSystem.UpdateUserInterface(ent, ent.Comp);
-
-        _adminLogger.Add(LogType.Action,
-            LogImpact.Low,
-            $"User added copy x{args.Copies} job to \"{ent.Comp.FaxName}\" {ToPrettyString(ent):tool} " +
-            $"of {ToPrettyString(sendEntity):subject}: {printout.Content}");
     }
 
 
