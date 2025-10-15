849e00c1a882ed34977b967f2d6964cd10587cbe
Arykh <181794112+Arykh@users.noreply.github.com>
2025-10-15 02:12:39 -0400

    :(

--- Content.Shared/_RMC14/Chemistry/Effects/RMCChemicalEffect.cs
+++ Content.Shared/_RMC14/Chemistry/Effects/RMCChemicalEffect.cs
@@ -10,15 +10,11 @@ public abstract partial class RMCChemicalEffect : EntityEffect
     [DataField]
     public float Potency;
 
-    public float ReagentBoost;
-
-    public float PotencyBoost => Potency + ReagentBoost;
-
     /// <summary>
     ///     The value that should be used in actual calculations for chemical effect
     ///     Halved since potency is halved before being used
     /// </summary>
-    public float ActualPotency => PotencyBoost * 0.5f;
+    public float ActualPotency => Potency * 0.5f;
 
     // Halved again since chemicals tick every second in SS14, not every 2
     public float PotencyPerSecond => ActualPotency * 0.5f;
@@ -33,14 +29,6 @@ public abstract partial class RMCChemicalEffect : EntityEffect
 
         var damageable = args.EntityManager.System<DamageableSystem>();
         var scale = reagentArgs.Scale;
-
-        // Calculate reagent-wide boost from all boost provider effects
-        var reagentBoostValue = CalculateReagentWideBoost(reagentArgs);
-
-        // Apply the boost to this effect's potency based on whether this effect allows self-boosting
-        var allowSelfBoost = this is IReagentBooster { BoostSelf: false };
-        ReagentBoost = allowSelfBoost ? reagentBoostValue : this is IReagentBooster ? 0f : reagentBoostValue;
-
         var scaledPotency = PotencyPerSecond * scale;
         Tick(damageable, scaledPotency, reagentArgs);
 
@@ -53,23 +41,6 @@ public abstract partial class RMCChemicalEffect : EntityEffect
 
         if (reagent.CriticalOverdose != null && totalQuantity >= reagent.CriticalOverdose)
             TickCriticalOverdose(damageable, scaledPotency, reagentArgs);
-    }
-
-    private static float CalculateReagentWideBoost(EntityEffectReagentArgs args)
-    {
-        if (args.Reagent?.Metabolisms == null)
-            return 0f;
-
-        var totalBoost = 0f;
-
-        foreach (var effect in args.Reagent.Metabolisms.Values.SelectMany(metabolism => metabolism.Effects))
-        {
-            if (effect is IReagentBooster boostProvider)
-            {
-                totalBoost += boostProvider.CalculateBoost(args);
-            }
-        }
-        return totalBoost;
     }
 
     protected virtual void Tick(DamageableSystem damageable, FixedPoint2 potency, EntityEffectReagentArgs args)
