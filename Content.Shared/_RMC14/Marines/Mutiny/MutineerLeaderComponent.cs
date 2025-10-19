ecb46da08a80f9949c99ec3a9b50325baef8e908
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2025-10-19 15:03:05 -0700

    Update Content.Shared/_RMC14/Marines/Mutiny/MutineerLeaderComponent.cs

--- Content.Shared/_RMC14/Marines/Mutiny/MutineerLeaderComponent.cs
+++ Content.Shared/_RMC14/Marines/Mutiny/MutineerLeaderComponent.cs
@@ -7,12 +7,12 @@ namespace Content.Shared._RMC14.Marines.Mutiny;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class MutineerLeaderComponent : Component
 {
-    [DataField("icon"), AutoNetworkedField]
+    [DataField, AutoNetworkedField]
     public SpriteSpecifier Icon = new SpriteSpecifier.Rsi(new ResPath("_RMC14/Interface/cm_job_icons.rsi"), "hudmutineerleader");
 
-    [DataField("recruitAction"), AutoNetworkedField]
+    [DataField, AutoNetworkedField]
     public EntProtoId RecruitAction = "ActionMutineerRecruit";
 
-    [DataField("recruitActionEntity"), AutoNetworkedField]
+    [DataField, AutoNetworkedField]
     public EntityUid? RecruitActionEntity;
 }
