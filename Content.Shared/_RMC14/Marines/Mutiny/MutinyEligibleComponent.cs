6b41435cbf68dbf8f63dd0ab2fd393c584a0e1ca
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2025-10-19 15:03:11 -0700

    Update Content.Shared/_RMC14/Marines/Mutiny/MutinyEligibleComponent.cs

--- Content.Shared/_RMC14/Marines/Mutiny/MutinyEligibleComponent.cs
+++ Content.Shared/_RMC14/Marines/Mutiny/MutinyEligibleComponent.cs
@@ -6,7 +6,5 @@ namespace Content.Shared._RMC14.Marines.Mutiny;
 ///     Marks a character as ineligible for mutiny recruitment.
 /// </summary>
 [RegisterComponent, NetworkedComponent]
-public sealed partial class MutinyEligibleComponent : Component
-{
-}
+public sealed partial class MutinyEligibleComponent : Component;
 
