fe8a7fa059deaaf0df82362a21e3548f9a39a0df
Ignaz Kraft <ignaz.k@live.de>
2025-11-05 23:59:07 +0100

    make implosions ignore ghosts

--- Content.Shared/_RMC14/Explosion/Implosion/RMCImplosionSystem.cs
+++ Content.Shared/_RMC14/Explosion/Implosion/RMCImplosionSystem.cs
@@ -1,4 +1,5 @@
 using Content.Shared._RMC14.Stun;
+using Content.Shared.Ghost;
 using Robust.Shared.Map;
 
 namespace Content.Shared._RMC14.Explosion.Implosion;
@@ -8,11 +9,22 @@ public sealed class RMCImplosionSystem : EntitySystem
     [Dependency] private readonly RMCSizeStunSystem _sizeStun = default!;
     [Dependency] private readonly EntityLookupSystem _entityLookup = default!;
 
+    private EntityQuery<GhostComponent> _ghostQuery;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        _ghostQuery = GetEntityQuery<GhostComponent>();
+    }
+
     public void Implode(RMCImplosion implosion, MapCoordinates origin)
     {
         var nearbyEntities = _entityLookup.GetEntitiesInRange(origin, implosion.PullRange, LookupFlags.Uncontained);
         foreach (var entity in nearbyEntities)
         {
+            if (_ghostQuery.HasComp(entity))
+                continue;
+
             _sizeStun.KnockBack(entity, origin, -implosion.PullDistance, -implosion.PullDistance, implosion.PullSpeed, implosion.IgnoreSize);
         }
     }
