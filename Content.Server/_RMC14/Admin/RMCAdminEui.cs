e53d1b05b146bf020e6193952e1006f5511053cf
Noctis <thbanham@gmail.com>
2025-11-06 04:30:09 +0000

    give up

--- Content.Server/_RMC14/Admin/RMCAdminEui.cs
+++ Content.Server/_RMC14/Admin/RMCAdminEui.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using Content.Server._RMC14.Xenonids.Hive;
 using Content.Server.Administration;
-using Content.Server.Administration.Logs;
 using Content.Server.Administration.Managers;
 using Content.Server.Chat.Managers;
 using Content.Server.EUI;
@@ -28,7 +27,6 @@ namespace Content.Server._RMC14.Admin;
 public sealed class RMCAdminEui : BaseEui
 {
     [Dependency] private readonly IAdminManager _admin = default!;
-    [Dependency] private readonly IAdminLogManager _adminLog = default!;
     [Dependency] private readonly IComponentFactory _compFactory = default!;
     [Dependency] private readonly IEntityManager _entities = default!;
     [Dependency] private readonly IReflectionManager _reflection = default!;
@@ -279,10 +277,7 @@ public sealed class RMCAdminEui : BaseEui
                     _entities.RemoveComponent<HiveMemberComponent>(target.Value);
 
                     var playerName = actor.PlayerSession?.Name ?? "unknown";
-                    var adminmessage = Loc.GetString("rmc-xeno-banish", ("target", playerName));
-
-                    _chatManager.SendAdminAlert(adminmessage);
-                    _adminLog.Add(LogType.RMCAdminCommandLogging, LogImpact.Extreme, adminmessage);
+                    _chatManager.SendAdminAlert(Loc.GetString("rmc-xeno-banish", ("target", playerName)));
                 }
 
                 break;
