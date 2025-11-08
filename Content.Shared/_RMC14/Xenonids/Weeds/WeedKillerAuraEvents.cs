42ace65d091018067bbefcf61301cce01cf44749
Özgür Akça <doguhanozgurakca@gmail.com>
2025-11-08 13:38:40 +0300

    add weedkiller aura

---
+++ Content.Shared/_RMC14/Xenonids/Weeds/WeedKillerAuraEvents.cs
@@ -0,0 +1,9 @@
+namespace Content.Shared._RMC14.Xenonids.Weeds;
+
+/// <summary>
+/// Raised by WeedKillerAuraSystem when an aura expires naturally.
+/// Listeners can react (e.g., schedule corruption checks).
+/// </summary>
+public readonly struct WeedKillerAuraExpiredEvent
+{
+}
