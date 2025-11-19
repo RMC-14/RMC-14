9a92bee6cbbc53e416db2883bd49bb5c12267965
Arykh <181794112+Arykh@users.noreply.github.com>
2025-11-19 00:26:27 -0500

    buh

--- Content.Shared/_RMC14/Chemistry/ChemMaster/RMCPillBottleTransferComponent.cs
+++ Content.Shared/_RMC14/Chemistry/ChemMaster/RMCPillBottleTransferComponent.cs
@@ -4,7 +4,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._RMC14.Chemistry.ChemMaster;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed class RMCPillBottleTransferComponent : Component
+public sealed partial class RMCPillBottleTransferComponent : Component
 {
     [DataField, AutoNetworkedField]
     public float TimePerBottle = 0.3f; // time_to_empty = 3
