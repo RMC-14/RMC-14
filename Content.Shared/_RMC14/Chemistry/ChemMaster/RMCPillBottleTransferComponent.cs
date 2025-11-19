b739ea706b7b19c91f34dda47af52e879a3efbdb
Arykh <181794112+Arykh@users.noreply.github.com>
2025-11-19 00:10:50 -0500

    doggo
    Co-authored-by: Aurallianz <74161272+Aurallianz@users.noreply.github.com>

--- Content.Shared/_RMC14/Chemistry/ChemMaster/RMCPillBottleTransferComponent.cs
+++ Content.Shared/_RMC14/Chemistry/ChemMaster/RMCPillBottleTransferComponent.cs
@@ -6,9 +6,9 @@ namespace Content.Shared._RMC14.Chemistry.ChemMaster;
 [RegisterComponent, NetworkedComponent]
 public sealed partial class RMCPillBottleTransferComponent : Component
 {
-    [DataField]
+    [DataField, AutoNetworkedField]
     public float TimePerBottle = 0.3f; // time_to_empty = 3
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public SoundSpecifier? InsertPillBottleSound = new SoundPathSpecifier("/Audio/Weapons/Guns/MagIn/revolver_magin.ogg");
 }
