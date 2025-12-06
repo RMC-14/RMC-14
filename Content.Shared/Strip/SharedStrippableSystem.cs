5eb7575913a8ab470b6927c6ef6025985c70d2f8
YO0teacher <hungisaiah@icloud.com>
2025-12-05 17:32:53 -0700

    Police 1 lets you strip dogtags
    Anyone with police 1 can strip dogtags from other people. Meaning admin ghosts can strip dogtags too. Created a bool to toggle this feature in case RMCUnstrippableComponent is used for something other than dogtags in the future.

--- Content.Shared/Strip/SharedStrippableSystem.cs
+++ Content.Shared/Strip/SharedStrippableSystem.cs
@@ -45,7 +45,8 @@ public abstract class SharedStrippableSystem : EntitySystem
     // RMC14
     [Dependency] private readonly SkillsSystem _skills = default!;
 
-    private static readonly EntProtoId<SkillDefinitionComponent> MultiStripSkill = "RMCSkillPolice";
+    private static readonly EntProtoId<SkillDefinitionComponent> Skill = "RMCSkillPolice";
+    public const int MinimumStripSkillLevel = 1;
     private const int MultiStripSkillLevel = 2;
 
     public override void Initialize()
@@ -287,10 +288,15 @@ public abstract class SharedStrippableSystem : EntitySystem
             return false;
         }
 
-        if (HasComp<RMCUnstrippableComponent>(slotItem))
+        if (TryComp<RMCUnstrippableComponent>(slotItem, out var unstrippableItem))
         {
-            _popupSystem.PopupCursor(Loc.GetString("rmc-unstrippable", ("item", slotItem), ("owner", Identity.Entity(target, EntityManager))));
-            return false;
+            if (unstrippableItem.PoliceCanStrip && _skills.HasSkill(user, Skill, MinimumStripSkillLevel))
+                return true;
+            else
+            {
+                _popupSystem.PopupCursor(Loc.GetString("rmc-unstrippable", ("item", slotItem), ("owner", Identity.Entity(target, EntityManager))));
+                return false;
+            }
         }
 
         return true;
@@ -345,7 +351,7 @@ public abstract class SharedStrippableSystem : EntitySystem
             BreakOnMove = true,
             NeedHand = true,
             BreakOnHandChange = false, // Allow simultaneously removing multiple items.
-            DuplicateCondition = _skills.HasSkill(user, MultiStripSkill, MultiStripSkillLevel) ? DuplicateConditions.SameTool : DuplicateConditions.SameEvent, // RMC14
+            DuplicateCondition = _skills.HasSkill(user, Skill, MultiStripSkillLevel) ? DuplicateConditions.SameTool : DuplicateConditions.SameEvent, // RMC14
             ForceVisible = user != target,
         };
 
@@ -561,7 +567,7 @@ public abstract class SharedStrippableSystem : EntitySystem
             BreakOnMove = true,
             NeedHand = true,
             BreakOnHandChange = false, // Allow simultaneously removing multiple items.
-            DuplicateCondition = _skills.HasSkill(user.Owner, MultiStripSkill, MultiStripSkillLevel) ? DuplicateConditions.SameTool : DuplicateConditions.SameEvent, // RMC14
+            DuplicateCondition = _skills.HasSkill(user.Owner, Skill, MultiStripSkillLevel) ? DuplicateConditions.SameTool : DuplicateConditions.SameEvent, // RMC14
             ForceVisible = user != target,
         };
 
