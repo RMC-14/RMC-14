94115fd3ed77dd15ed374addbf0919461d5f3ccb
DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>
2025-12-14 14:40:50 -0800

    Update Content.Shared/_RMC14/Xenonids/Hive/AutoAssignHiveComponent.cs

--- Content.Shared/_RMC14/Xenonids/Hive/AutoAssignHiveComponent.cs
+++ Content.Shared/_RMC14/Xenonids/Hive/AutoAssignHiveComponent.cs
@@ -5,7 +5,6 @@ namespace Content.Shared._RMC14.Xenonids.Hive;
 /// <summary>
 /// Given a hive's name, attempts to auto assign itself to said hive
 /// </summary>
-
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 [Access(typeof(SharedXenoHiveSystem))]
 public sealed partial class AutoAssignHiveComponent : Component
