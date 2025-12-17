3f1c4b72731b7bd2bcd0183cd458fce570856e5a
Scyarah <sarah@gegner.me>
2025-12-17 12:15:29 +0100

    Changed the xeno rank 6 name from prime to apex

--- Content.Shared/_RMC14/Xenonids/Name/XenoRankNamesComponent.cs
+++ Content.Shared/_RMC14/Xenonids/Name/XenoRankNamesComponent.cs
@@ -13,6 +13,6 @@ public sealed partial class XenoRankNamesComponent : Component
         {3, "rmc-xeno-elder"},
         {4, "rmc-xeno-ancient"},
         {5, "rmc-xeno-prime"},
-        {6, "rmc-xeno-prime"},
+        {6, "rmc-xeno-apex"},
     };
 }
