59d388a4af1abf98e349a8b798fd928a724110e2
SharkSnake98 <sharksnake87@gmail.com>
2025-12-19 17:56:11 -0500

    If someone has any coding knowledge please god help me I've never touched C# before and it's like being hit with hammers HELP!!! I have literally copied a bunch of code from the fax machine and none of this is my own work I have no idea what I'm doing

--- Content.Server/_RMC14/Photocopier/PhotocopierSystem.cs
+++ Content.Server/_RMC14/Photocopier/PhotocopierSystem.cs
@@ -1,10 +1,16 @@
 ﻿using Content.Server.Power.Components;
-using Content.Shared.DeviceNetwork.Events;
+using Content.Shared.Database;
 using Content.Shared.Fax;
+using Content.Shared.Fax.Components;
 using Content.Shared.Interaction;
+using Content.Shared.Labels.Components;
+using Content.Shared.Mobs.Components;
+using Content.Shared.NameModifier.Components;
+using Content.Shared.Paper;
 using Content.Shared.Power;
 using Content.Shared.UserInterface;
 using Robust.Shared.Containers;
+using Content.Shared._RMC14.Photocopier;
 
 namespace Content.Server._RMC14.Photocopier;
 
@@ -27,7 +33,6 @@ public sealed class PhotocopierSystem : EntitySystem
         SubscribeLocalEvent<PhotocopierComponent, EntInsertedIntoContainerMessage>(OnItemSlotChanged);
         SubscribeLocalEvent<PhotocopierComponent, EntRemovedFromContainerMessage>(OnItemSlotChanged);
         SubscribeLocalEvent<PhotocopierComponent, PowerChangedEvent>(OnPowerChanged);
-        SubscribeLocalEvent<PhotocopierComponent, DeviceNetworkPacketEvent>(OnPacketReceived);
 
         // Interaction
         SubscribeLocalEvent<PhotocopierComponent, InteractUsingEvent>(OnInteractUsing);
@@ -48,7 +53,6 @@ public sealed class PhotocopierSystem : EntitySystem
                 continue;
 
             ProcessPrintingAnimation(uid, frameTime, fax);
-            ProcessInsertingAnimation(uid, frameTime, fax);
             ProcessSendingTimeout(uid, frameTime, fax);
         }
     }
@@ -75,5 +79,179 @@ public sealed class PhotocopierSystem : EntitySystem
             comp.PrintingTimeRemaining = comp.PrintingTime;
             _audioSystem.PlayPvs(comp.PrintSound, uid);
         }
+    }
+ private void OnComponentInit(EntityUid uid, FaxMachineComponent component, ComponentInit args)
+    {
+        _itemSlotsSystem.AddItemSlot(uid, PaperSlotId, component.PaperSlot);
+        UpdateAppearance(uid, component);
+    }
+
+    private void OnComponentRemove(EntityUid uid, FaxMachineComponent component, ComponentRemove args)
+    {
+        _itemSlotsSystem.RemoveItemSlot(uid, component.PaperSlot);
+    }
+
+    private void OnMapInit(EntityUid uid, FaxMachineComponent component, MapInitEvent args)
+    {
+        // Load all faxes on map in cache each other to prevent taking same name by user created fax
+        Refresh(uid, component);
+    }
+
+    private void OnItemSlotChanged(EntityUid uid, FaxMachineComponent component, ContainerModifiedMessage args)
+    {
+        if (!component.Initialized)
+            return;
+
+        if (args.Container.ID != component.PaperSlot.ID)
+            return;
+
+        var isPaperInserted = component.PaperSlot.Item.HasValue;
+        if (isPaperInserted)
+        {
+            component.InsertingTimeRemaining = component.InsertionTime;
+            _itemSlotsSystem.SetLock(uid, component.PaperSlot, true);
+        }
+
+        UpdateUserInterface(uid, component);
+    }
+
+    private void OnPowerChanged(EntityUid uid, PhotocopierComponent component, ref PowerChangedEvent args)
+    {
+        var isInsertInterrupted = !args.Powered && component.InsertingTimeRemaining > 0;
+        if (isInsertInterrupted)
+        {
+            component.InsertingTimeRemaining = 0f; // Reset animation
+
+            // Drop from slot because animation did not play completely
+            _itemSlotsSystem.SetLock(uid, component.PaperSlot, false);
+            _itemSlotsSystem.TryEject(uid, component.PaperSlot, null, out var _, true);
+        }
+
+        var isPrintInterrupted = !args.Powered && component.PrintingTimeRemaining > 0;
+        if (isPrintInterrupted)
+        {
+            component.PrintingTimeRemaining = 0f; // Reset animation
+        }
+
+        if (isInsertInterrupted || isPrintInterrupted)
+            UpdateAppearance(uid, component);
+
+        _itemSlotsSystem.SetLock(uid, component.PaperSlot, !args.Powered); // Lock slot when power is off
+    }
+
+    private void OnToggleInterface(EntityUid uid, FaxMachineComponent component, AfterActivatableUIOpenEvent args)
+    {
+        UpdateUserInterface(uid, component);
+    }
+
+    private void OnFileButtonPressed(EntityUid uid, FaxMachineComponent component, FaxFileMessage args)
+    {
+        args.Label = args.Label?[..Math.Min(args.Label.Length, FaxFileMessageValidation.MaxLabelSize)];
+        args.Content = args.Content[..Math.Min(args.Content.Length, FaxFileMessageValidation.MaxContentSize)];
+        PrintFile(uid, component, args);
+    }
+
+    private void OnCopyButtonPressed(EntityUid uid, FaxMachineComponent component, FaxCopyMessage args)
+    {
+        if (HasComp<MobStateComponent>(component.PaperSlot.Item))
+            _faxecute.Faxecute(uid, component); // when button pressed it will hurt the mob.
+        else
+            Copy(uid, component, args);
+    }
+    private void UpdateAppearance(EntityUid uid, FaxMachineComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        if (TryComp<FaxableObjectComponent>(component.PaperSlot.Item, out var faxable))
+            component.InsertingState = faxable.InsertingState;
+
+
+        if (component.InsertingTimeRemaining > 0)
+        {
+            _appearanceSystem.SetData(uid, FaxMachineVisuals.VisualState, FaxMachineVisualState.Inserting);
+            Dirty(uid, component);
+        }
+        else if (component.PrintingTimeRemaining > 0)
+            _appearanceSystem.SetData(uid, FaxMachineVisuals.VisualState, FaxMachineVisualState.Printing);
+        else
+            _appearanceSystem.SetData(uid, FaxMachineVisuals.VisualState, FaxMachineVisualState.Normal);
+    }
+    private void UpdateUserInterface(EntityUid uid, PhotocopierComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        var isPaperInserted = component.PaperSlot.Item != null; ;
+        var canCopy = isPaperInserted &&
+                      component.SendTimeoutRemaining <= 0 &&
+                      component.InsertingTimeRemaining <= 0;
+        var state = new FaxUiState( canCopy, isPaperInserted );
+        _userInterface.SetUiState(uid, FaxUiKey.Key, state);
+    }
+    public void PrintFile(EntityUid uid, PhotocopierComponent component, FaxFileMessage args)
+    {
+        var prototype = args.OfficePaper ? component.PrintOfficePaperId : component.PrintPaperId;
+
+        var name = Loc.GetString("fax-machine-printed-paper-name");
+
+        var printout = new FaxPrintout(args.Content, name, args.Label, prototype);
+        component.PrintingQueue.Enqueue(printout);
+        component.SendTimeoutRemaining += component.SendTimeout;
+
+        UpdateUserInterface(uid, component);
+
+        // Unfortunately, since a paper entity does not yet exist, we have to emulate what LabelSystem will do.
+        var nameWithLabel = (args.Label is { } label) ? $"{name} ({label})" : name;
+        _adminLogger.Add(LogType.Action,
+            LogImpact.Low,
+            $"{ToPrettyString(args.Actor):actor} " +
+            $"added print job to \"{component.FaxName}\" {ToPrettyString(uid):tool} " +
+            $"of {nameWithLabel}: {args.Content}");
+    }
+    /// <summary>
+    ///     Copies the paper in the fax. A timeout is set after copying,
+    /// </summary>
+    public void Copy(EntityUid uid,PhotocopierComponent? component, FaxCopyMessage args)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        if (component.SendTimeoutRemaining > 0)
+            return;
+
+        var sendEntity = component.PaperSlot.Item;
+        if (sendEntity == null)
+            return;
+
+        if (!TryComp(sendEntity, out MetaDataComponent? metadata) ||
+            !TryComp<PaperComponent>(sendEntity, out var paper))
+            return;
+
+        TryComp<LabelComponent>(sendEntity, out var labelComponent);
+        TryComp<NameModifierComponent>(sendEntity, out var nameMod);
+
+        // TODO: See comment in 'Send()' about not being able to copy whole entities
+        var printout = new FaxPrintout(paper.Content,
+                                       nameMod?.BaseName ?? metadata.EntityName,
+                                       labelComponent?.CurrentLabel,
+                                       metadata.EntityPrototype?.ID ?? component.PrintPaperId,
+                                       paper.StampState,
+                                       paper.StampedBy,
+                                       paper.EditingDisabled);
+
+        component.PrintingQueue.Enqueue(printout);
+        component.SendTimeoutRemaining += component.SendTimeout;
+
+        // Don't play component.SendSound - it clashes with the printing sound, which
+        // will start immediately.
+
+        UpdateUserInterface(uid, component);
+
+        _adminLogger.Add(LogType.Action,
+            LogImpact.Low,
+            $"{ToPrettyString(args.Actor):actor} " +
+            $"added copy job to \"{component.FaxName}\" {ToPrettyString(uid):tool} " +
+            $"of {ToPrettyString(sendEntity):subject}: {printout.Content}");
     }
 }
--- Content.Shared/_RMC14/Photocopier/PhotocopierComponent.cs
+++ Content.Shared/_RMC14/Photocopier/PhotocopierComponent.cs
@@ -1,10 +1,121 @@
-﻿namespace Content.Shared._RMC14.Photocopier;
+﻿using Content.Shared.Containers.ItemSlots;
+using Content.Shared.Paper;
+using Robust.Shared.Audio;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Shared._RMC14.Photocopier;
 
 /// <summary>
 /// This is used for...
 /// </summary>
-[RegisterComponent]
-public sealed partial class PhotocopierComponent : Component
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class FaxMachineComponent : Component
 {
-    
+
+    /// <summary>
+    /// Contains the item to be sent, assumes it's paper...
+    /// </summary>
+    [DataField(required: true)]
+    public ItemSlot PaperSlot = new();
+
+    /// <summary>
+    /// Sound to play when fax printing new message
+    /// </summary>
+    [DataField]
+    public SoundSpecifier PrintSound = new SoundPathSpecifier("/Audio/Machines/printer.ogg");
+
+    /// <summary>
+    /// Sound to play when fax successfully send message
+    /// </summary>
+    [DataField]
+    public SoundSpecifier SendSound = new SoundPathSpecifier("/Audio/Machines/high_tech_confirm.ogg");
+
+    /// <summary>
+    /// Print queue of the incoming message
+    /// </summary>
+    [ViewVariables]
+    [DataField]
+    public Queue<FaxPrintout> PrintingQueue { get; private set; } = new();
+
+    /// <summary>
+    /// Message sending timeout
+    /// </summary>
+    [ViewVariables]
+    [DataField]
+    public float SendTimeoutRemaining;
+
+    /// <summary>
+    /// Message sending timeout
+    /// </summary>
+    [ViewVariables]
+    [DataField]
+    public float SendTimeout = 5f;
+
+    /// <summary>
+    /// Remaining time of printing animation
+    /// </summary>
+    [DataField]
+    public float PrintingTimeRemaining;
+
+    /// <summary>
+    /// How long the printing animation will play
+    /// </summary>
+    [ViewVariables]
+    public float PrintingTime = 2.3f;
+
+    /// <summary>
+    ///     The prototype ID to use for faxed or copied entities if we can't get one from
+    ///     the paper entity for whatever reason.
+    /// </summary>
+    [DataField]
+    public EntProtoId PrintPaperId = "Paper";
+
+    /// <summary>
+    ///     The prototype ID to use for faxed or copied entities if we can't get one from
+    ///     the paper entity for whatever reason of the Office type.
+    /// </summary>
+    [DataField]
+    public EntProtoId PrintOfficePaperId = "PaperOffice";
+}
+
+[DataDefinition]
+public sealed partial class FaxPrintout
+{
+    [DataField(required: true)]
+    public string Name { get; private set; } = default!;
+
+    [DataField]
+    public string? Label { get; private set; }
+
+    [DataField(required: true)]
+    public string Content { get; private set; } = default!;
+
+    [DataField(customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>), required: true)]
+    public string PrototypeId { get; private set; } = default!;
+
+    [DataField("stampState")]
+    public string? StampState { get; private set; }
+
+    [DataField("stampedBy")]
+    public List<StampDisplayInfo> StampedBy { get; private set; } = new();
+
+    [DataField]
+    public bool Locked { get; private set; }
+
+    private FaxPrintout()
+    {
+    }
+
+    public FaxPrintout(string content, string name, string? label = null, string? prototypeId = null, string? stampState = null, List<StampDisplayInfo>? stampedBy = null, bool locked = false)
+    {
+        Content = content;
+        Name = name;
+        Label = label;
+        PrototypeId = prototypeId ?? "";
+        StampState = stampState;
+        StampedBy = stampedBy ?? new List<StampDisplayInfo>();
+        Locked = locked;
+    }
 }
