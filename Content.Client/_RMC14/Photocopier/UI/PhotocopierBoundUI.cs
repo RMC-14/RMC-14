4a57f275b3befaa544b73db76ff95fed138a5b89
SharkSnake98 <sharksnake87@gmail.com>
2025-12-19 22:51:34 -0500

    Updated systems

---
+++ Content.Client/_RMC14/Photocopier/UI/PhotocopierBoundUI.cs
@@ -0,0 +1,93 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Content.Shared._RMC14.Photocopier;
+using Content.Shared._RMC14.Photocopier.Events;
+using Content.Shared.Fax;
+using JetBrains.Annotations;
+using Robust.Client.GameObjects;
+using Robust.Client.UserInterface;
+
+namespace Content.Client._RMC14.Photocopier.UI;
+
+[UsedImplicitly]
+public sealed class PhotocopierBoundUI : BoundUserInterface
+{
+    [Dependency] private readonly IFileDialogManager _fileDialogManager = default!;
+
+    [ViewVariables]
+    private PhotocopierWindow? _window;
+
+    private bool _dialogIsOpen = false;
+
+    public PhotocopierBoundUI(EntityUid owner, Enum uiKey) : base(owner, uiKey)
+    {
+    }
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _window = this.CreateWindow<PhotocopierWindow>();
+    }
+
+    private async void OnFileButtonPressed()
+    {
+        if (_dialogIsOpen)
+            return;
+
+        _dialogIsOpen = true;
+        var filters = new FileDialogFilters(new FileDialogFilters.Group("txt"));
+        await using var file = await _fileDialogManager.OpenFile(filters);
+        _dialogIsOpen = false;
+
+        if (_window == null || _window.Disposed || file == null)
+        {
+            return;
+        }
+
+        using var reader = new StreamReader(file);
+
+        var firstLine = await reader.ReadLineAsync();
+        string? label = null;
+        var content = await reader.ReadToEndAsync();
+
+        if (firstLine is { })
+        {
+            if (firstLine.StartsWith('#'))
+            {
+                label = firstLine[1..].Trim();
+            }
+            else
+            {
+                content = firstLine + "\n" + content;
+            }
+        }
+
+        SendMessage(new FaxFileMessage(
+            label?[..Math.Min(label.Length, FaxFileMessageValidation.MaxLabelSize)],
+            content[..Math.Min(content.Length, FaxFileMessageValidation.MaxContentSize)],
+            _window.OfficePaper));
+    }
+
+    private void OnEjectButtonPressed()
+    {
+       SendMessage(new EjectPaperEvent());
+    }
+
+    private void OnCopyButtonPressed(int copyCount)
+    {
+        var ev = new CopiedPaperEvent();
+        ev.CopyCount = copyCount;
+        SendMessage(ev);
+    }
+
+    protected override void UpdateState(BoundUserInterfaceState state)
+    {
+        base.UpdateState(state);
+
+        if (_window == null || state is not PhotocopierUiState cast)
+            return;
+
+        _window.UpdateState(cast);
+    }
+}
--- Content.Shared/_RMC14/Photocopier/PhotocopierComponent.cs
+++ Content.Shared/_RMC14/Photocopier/PhotocopierComponent.cs
@@ -3,55 +3,28 @@ using Content.Shared.Paper;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Shared._RMC14.Photocopier;
-
 /// <summary>
 /// This is used for...
 /// </summary>
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-public sealed partial class FaxMachineComponent : Component
+[RegisterComponent, NetworkedComponent ]
+public sealed partial class PhotocopierComponent : Component
 {
 
     /// <summary>
     /// Contains the item to be sent, assumes it's paper...
     /// </summary>
     [DataField(required: true)]
-    public ItemSlot PaperSlot = new();
+    public string PaperSlotId = "PhotocopierSlot";
 
     /// <summary>
     /// Sound to play when fax printing new message
     /// </summary>
     [DataField]
     public SoundSpecifier PrintSound = new SoundPathSpecifier("/Audio/Machines/printer.ogg");
-
-    /// <summary>
-    /// Sound to play when fax successfully send message
-    /// </summary>
-    [DataField]
-    public SoundSpecifier SendSound = new SoundPathSpecifier("/Audio/Machines/high_tech_confirm.ogg");
-
-    /// <summary>
-    /// Print queue of the incoming message
-    /// </summary>
-    [ViewVariables]
-    [DataField]
-    public Queue<FaxPrintout> PrintingQueue { get; private set; } = new();
-
-    /// <summary>
-    /// Message sending timeout
-    /// </summary>
-    [ViewVariables]
-    [DataField]
-    public float SendTimeoutRemaining;
-
-    /// <summary>
-    /// Message sending timeout
-    /// </summary>
-    [ViewVariables]
-    [DataField]
-    public float SendTimeout = 5f;
 
     /// <summary>
     /// Remaining time of printing animation
@@ -65,23 +38,22 @@ public sealed partial class FaxMachineComponent : Component
     [ViewVariables]
     public float PrintingTime = 2.3f;
 
+    [ViewVariables]
+    public int PrintingCount = 0;
+
+    [ViewVariables]
+    public CopyPrintout? Printout;
+
     /// <summary>
     ///     The prototype ID to use for faxed or copied entities if we can't get one from
     ///     the paper entity for whatever reason.
     /// </summary>
     [DataField]
-    public EntProtoId PrintPaperId = "Paper";
-
-    /// <summary>
-    ///     The prototype ID to use for faxed or copied entities if we can't get one from
-    ///     the paper entity for whatever reason of the Office type.
-    /// </summary>
-    [DataField]
-    public EntProtoId PrintOfficePaperId = "PaperOffice";
+    public EntProtoId PrintPaperId = "CMPaper";
 }
 
 [DataDefinition]
-public sealed partial class FaxPrintout
+public sealed partial class CopyPrintout
 {
     [DataField(required: true)]
     public string Name { get; private set; } = default!;
@@ -92,9 +64,6 @@ public sealed partial class FaxPrintout
     [DataField(required: true)]
     public string Content { get; private set; } = default!;
 
-    [DataField(customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>), required: true)]
-    public string PrototypeId { get; private set; } = default!;
-
     [DataField("stampState")]
     public string? StampState { get; private set; }
 
@@ -104,16 +73,15 @@ public sealed partial class FaxPrintout
     [DataField]
     public bool Locked { get; private set; }
 
-    private FaxPrintout()
+    private CopyPrintout()
     {
     }
 
-    public FaxPrintout(string content, string name, string? label = null, string? prototypeId = null, string? stampState = null, List<StampDisplayInfo>? stampedBy = null, bool locked = false)
+    public CopyPrintout(string content, string name, string? label = null, string? stampState = null, List<StampDisplayInfo>? stampedBy = null, bool locked = false)
     {
         Content = content;
         Name = name;
         Label = label;
-        PrototypeId = prototypeId ?? "";
         StampState = stampState;
         StampedBy = stampedBy ?? new List<StampDisplayInfo>();
         Locked = locked;
