9adab6b560359ec3dbfd01f6ff6c5e32924f4c8c
Noctis <thbanham@gmail.com>
2025-12-26 16:12:16 +0000

    init

---
+++ Content.Shared/_RMC14/Clothing/ClothingFactionLockIgnoreComponent.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Clothing;
+
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(RMCClothingSystem))]
+public sealed partial class ClothingFactionLockIgnoreComponent : Component
+{
+}
