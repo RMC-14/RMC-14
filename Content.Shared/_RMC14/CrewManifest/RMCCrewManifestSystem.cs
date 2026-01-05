76f25bef5e894b4d689e941a3f6922e59576aaff
Dutch_VanDerLinde <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2026-01-05 14:16:23 -0500

    Update RMCCrewManifestSystem.cs

--- Content.Shared/_RMC14/CrewManifest/RMCCrewManifestSystem.cs
+++ Content.Shared/_RMC14/CrewManifest/RMCCrewManifestSystem.cs
@@ -2,12 +2,14 @@ using Content.Shared._RMC14.Marines;
 using Content.Shared.CrewManifest;
 using Content.Shared.Station.Components;
 using Robust.Shared.Network;
+using Robust.Shared.Player;
 
 namespace Content.Shared._RMC14.CrewManifest;
 
 public sealed class RMCCrewManifestSystem : EntitySystem
 {
     [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly ISharedPlayerManager _playerManager = default!;
 
     public override void Initialize()
     {
@@ -16,15 +18,20 @@ public sealed class RMCCrewManifestSystem : EntitySystem
 
     private void OnUserOpenAlert(Entity<MarineComponent> ent, ref OpenCrewManifestAlertEvent args)
     {
+        if (args.Handled)
+            return;
+
         var stations = EntityQueryEnumerator<StationMemberComponent>();
         while (stations.MoveNext(out var stationId, out var stationComp))
         {
             if (!HasComp<AlmayerComponent>(stationId))
                 continue;
 
+            args.Handled = true;
+
             var stationNetEntity = GetNetEntity(stationComp.Station);
 
-            if (_net.IsClient)
+            if (_net.IsClient && ent.Owner == _playerManager.LocalEntity)
                 RaiseNetworkEvent(new RequestCrewManifestMessage(stationNetEntity));
         }
     }
