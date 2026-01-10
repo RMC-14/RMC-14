c6a62969a50187d77d599dcfe4c22daa1b470395
Neved4 <63655535+Neved4@users.noreply.github.com>
2026-01-10 03:33:25 +0100

    Add RobustToolbox upstream ARM64 fixes

--- RUN_THIS.py
+++ RUN_THIS.py
@@ -7,7 +7,16 @@ import subprocess
 
 version = sys.version_info
 if version.major < 3 or (version.major == 3 and version.minor < 5):
-    print("ERROR: You need at least Python 3.5 to build SS14.")
-    sys.exit(1)
+	print("ERROR: You need at least Python 3.5 to build SS14.")
+	sys.exit(1)
+
+patchPath = "Tools/patches/arm64.patch"
+checkResult = subprocess.run(
+	["git", "apply", "-R", "--check", patchPath],
+	stdout=subprocess.DEVNULL,
+	stderr=subprocess.DEVNULL,
+)
+if checkResult.returncode != 0:
+	subprocess.run(["git", "apply", patchPath], check=True)
 
 subprocess.run([sys.executable, "git_helper.py"], cwd="BuildChecker")
