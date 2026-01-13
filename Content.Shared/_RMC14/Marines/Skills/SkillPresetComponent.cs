73e45d9fedf6b75289631f36ad9d53033fb6ff8c
Dutch_VanDerLinde <45323883+Dutch-VanDerLinde@users.noreply.github.com>
2026-01-13 03:50:20 -0500

    Make skill presets entity prototypes and fixes (#8833)
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>

--- Content.Shared/_RMC14/Marines/Skills/SkillPresetComponent.cs
+++ Content.Shared/_RMC14/Marines/Skills/SkillPresetComponent.cs
@@ -3,13 +3,13 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared._RMC14.Marines.Skills;
 
+/// <summary>
+/// A preset of a group of skills. Useful for standardization of skills across roles that have the same skillset.
+/// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 [Access(typeof(SkillsSystem))]
-public sealed partial class SkillsComponent : Component
+public sealed partial class SkillPresetComponent : Component
 {
-    [DataField, AutoNetworkedField]
+    [DataField, AutoNetworkedField, AlwaysPushInheritance]
     public Dictionary<EntProtoId<SkillDefinitionComponent>, int> Skills = new();
-
-    [DataField, AutoNetworkedField]
-    public EntProtoId<SkillPresetComponent>? Preset;
 }
