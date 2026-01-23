54766184030d0e77ff94508f556cd950cd28be29
chavonadelal <chavonadelal@mail.ru>
2026-01-23 15:50:51 +0300

    desiredType

--- Content.Server/Chat/Systems/ChatSystem.cs
+++ Content.Server/Chat/Systems/ChatSystem.cs
@@ -292,12 +292,12 @@ public sealed partial class ChatSystem : SharedChatSystem
         }
 
         // RMC14: Allow systems to modify the chat type before sending
-        var modifyEv = new Content.Server._RMC14.Chat.Events.RMCChatTypeModifyEvent(source, message, desiredType);
+        var modifyEv = new _RMC14.Chat.Events.RMCChatTypeModifyEvent(source, message, desiredType);
         RaiseLocalEvent(source, ref modifyEv);
-        var finalType = modifyEv.ModifiedType.HasValue ? modifyEv.ModifiedType.Value : desiredType;
+        desiredType = modifyEv.ModifiedType.HasValue ? modifyEv.ModifiedType.Value : desiredType;
 
         // Otherwise, send whatever type.
-        switch (finalType) // RMC14
+        switch (desiredType)
         {
             case InGameICChatType.Speak:
                 SendEntitySpeak(source, message, range, nameOverride, hideLog, ignoreActionBlocker);
