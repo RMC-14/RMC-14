d204ede43227fd7494fa7fdcb1d113566221e380
Thereisabear <snorklefinns@gmail.com>
2026-02-08 17:11:22 +0800

    Update SharedMeleeWeaponSystem.cs

--- Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
+++ Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
@@ -766,7 +766,7 @@ public abstract class SharedMeleeWeaponSystem : EntitySystem
         // RMC14: Checks and returns early for when attacking too fast and target is technically not there
         if (appliedDamage.GetTotal() > FixedPoint2.Zero && targets.Count > 0)
         {
-            if (!TryComp<TransformComponent>(targets[0], out var targetXform))
+            if (!TryComp(targets[0], out TransformComponent? targetXform))
                 return true;
             DoDamageEffect(targets, user, targetXform);
         }
