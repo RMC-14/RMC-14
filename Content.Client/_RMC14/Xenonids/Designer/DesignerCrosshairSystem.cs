1c780bca34832bc8a75d8fbe9d6fbe7db713ed28
LKarzon <leith.karzon@gmail.com>
2026-02-09 23:53:10 -0600

    Updated design mark overlay-color reflects node choice

--- Content.Client/_RMC14/Xenonids/Designer/DesignerCrosshairSystem.cs
+++ Content.Client/_RMC14/Xenonids/Designer/DesignerCrosshairSystem.cs
@@ -7,6 +7,7 @@ using Robust.Client.Input;
 using Robust.Client.Player;
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.CustomControls;
+using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -16,10 +17,12 @@ namespace Content.Client._RMC14.Xenonids.Designer;
 public sealed class DesignerCrosshairSystem : EntitySystem
 {
     [Dependency] private readonly IClyde _clyde = default!;
+    [Dependency] private readonly IComponentFactory _compFactory = default!;
     [Dependency] private readonly IEyeManager _eye = default!;
     [Dependency] private readonly IInputManager _input = default!;
     [Dependency] private readonly IOverlayManager _overlay = default!;
     [Dependency] private readonly IPlayerManager _player = default!;
+    [Dependency] private readonly IPrototypeManager _prototype = default!;
     [Dependency] private readonly IUserInterfaceManager _ui = default!;
 
     private ICursor? _transparentCursor;
@@ -62,6 +65,9 @@ public sealed class DesignerCrosshairSystem : EntitySystem
         if (!TryComp(player, out DesignerStrainComponent? designer))
             return null;
 
+        if (!TryComp(player, out XenoConstructionComponent? construction))
+            return null;
+
         var actionController = _ui.GetUIController<ActionUIController>();
         if (actionController.SelectingTargetFor is not { } selectedActionId)
             return null;
@@ -69,6 +75,23 @@ public sealed class DesignerCrosshairSystem : EntitySystem
         if (!HasComp<XenoConstructionActionComponent>(selectedActionId))
             return null;
 
-        return designer.BuildDoorNodes ? crosshair.DoorRsi : crosshair.WallRsi;
+        // Resolve the node type from the current build choice prototype.
+        var nodeType = DesignNodeType.Construct;
+        if (construction.BuildChoice is { } buildChoice &&
+            _prototype.TryIndex(buildChoice, out var proto) &&
+            proto.TryGetComponent(out DesignNodeComponent? nodeComp, _compFactory))
+        {
+            nodeType = nodeComp.NodeType;
+        }
+
+        var isDoor = designer.BuildDoorNodes;
+
+        return nodeType switch
+        {
+            DesignNodeType.Optimized => isDoor ? crosshair.OptimizedDoorRsi : crosshair.OptimizedWallRsi,
+            DesignNodeType.Flexible => isDoor ? crosshair.FlexibleDoorRsi : crosshair.FlexibleWallRsi,
+            DesignNodeType.Construct => isDoor ? crosshair.ConstructDoorRsi : crosshair.ConstructWallRsi,
+            _ => null,
+        };
     }
 }
