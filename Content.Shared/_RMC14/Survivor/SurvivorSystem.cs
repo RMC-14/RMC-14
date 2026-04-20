d2d78d114461cc75790ce4e8bde3f223af068f1d
Vixting <vixting@gmail.com>
2026-04-20 09:32:59 +0100

    a

--- Content.Shared/_RMC14/Survivor/SurvivorSystem.cs
+++ Content.Shared/_RMC14/Survivor/SurvivorSystem.cs
@@ -1,15 +1,12 @@
 using Content.Shared._RMC14.Armor;
 using Content.Shared._RMC14.EntityPreset;
 using Content.Shared.GameTicking;
-using Robust.Shared.Prototypes;
 
 namespace Content.Shared._RMC14.Survivor;
 
 public sealed class SurvivorSystem : EntitySystem
 {
-    [Dependency] private readonly IComponentFactory _compFactory = default!;
-    [Dependency] private readonly IPrototypeManager _prototypes = default!;
-    [Dependency] private readonly RMCRandomizedPresetSystem _preset = default!;
+    [Dependency] private readonly EntityPresetSystem _preset = default!;
 
     public override void Initialize()
     {
@@ -18,14 +15,6 @@ public sealed class SurvivorSystem : EntitySystem
 
     private void OnPresetPlayerSpawnComplete(Entity<EquipSurvivorPresetComponent> ent, ref PlayerSpawnCompleteEvent args)
     {
-        ApplyPreset(ent, ent.Comp.Preset);
-    }
-
-    private void ApplyPreset(EntityUid mob, EntProtoId<EntityPresetComponent> preset)
-    {
-        if (!preset.TryGet(out var comp, _prototypes, _compFactory))
-            return;
-
-        _preset.ApplyPreset(mob, comp);
+        _preset.ApplyPreset(ent, ent.Comp.Preset);
     }
 }
