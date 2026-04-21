d2ab789acea644da741741af1a2b2456ecf21f79
Arendian <137322659+Arendian@users.noreply.github.com>
2026-04-21 19:41:01 +0200

    Humanoid imaginary friends take the appearance of someones SEA character.

--- Content.Server/_RMC14/Mentor/ImaginaryFriend/ImaginaryFriendSystem.cs
+++ Content.Server/_RMC14/Mentor/ImaginaryFriend/ImaginaryFriendSystem.cs
@@ -1,9 +1,22 @@
+using System.Linq;
+using Content.Server.Humanoid;
 using Content.Server.Mind;
+using Content.Server.Popups;
+using Content.Server.Preferences.Managers;
+using Content.Server.Station.Systems;
 using Content.Shared._RMC14.Mentor.ImaginaryFriend;
 using Content.Shared._RMC14.Xenonids;
+using Content.Shared.Clothing;
 using Content.Shared.Eye;
 using Content.Shared.Ghost;
+using Content.Shared.Humanoid;
+using Content.Shared.Inventory;
+using Content.Shared.Popups;
+using Content.Shared.Preferences;
+using Content.Shared.Preferences.Loadouts;
+using Content.Shared.Roles;
 using Robust.Server.GameObjects;
+using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._RMC14.Mentor.ImaginaryFriend;
@@ -12,12 +25,22 @@ public sealed class ImaginaryFriendSystem : SharedImaginaryFriendSystem
 {
     [Dependency] private readonly AppearanceSystem _appearance = default!;
     [Dependency] private readonly EyeSystem _eye = default!;
+    [Dependency] private readonly HumanoidAppearanceSystem _humanoid = default!;
+    [Dependency] private readonly InventorySystem _inventory = default!;
+    [Dependency] private readonly MetaDataSystem _metaData = default!;
     [Dependency] private readonly MindSystem _mind = default!;
+    [Dependency] private readonly PopupSystem _popup = default!;
+    [Dependency] private readonly IServerPreferencesManager _preferencesManager = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly StationSpawningSystem _stationSpawning = default!;
     [Dependency] private readonly TransformSystem _transform = default!;
     [Dependency] private readonly VisibilitySystem _visibility = default!;
 
-    public static readonly EntProtoId ImaginaryFriendPrototype = "RMCImaginaryFriend";
-    public static readonly EntProtoId XenoImaginaryFriendPrototype = "RMCImaginaryFriendXeno";
+    private static readonly EntProtoId ImaginaryFriendPrototype = "RMCImaginaryFriendHumanoid";
+    private static readonly EntProtoId XenoImaginaryFriendPrototype = "RMCImaginaryFriendXeno";
+
+    private static readonly ProtoId<StartingGearPrototype> XenoImaginaryFriendGear = "RMCMobXippyGear";
+    private static readonly ProtoId<JobPrototype> ImaginaryFriendJobPrototype = "CMSeniorEnlistedAdvisor";
 
     public override void Initialize()
     {
@@ -77,14 +100,78 @@ public sealed class ImaginaryFriendSystem : SharedImaginaryFriendSystem
             return;
 
         if (EnsureComp<HasImaginaryFriendComponent>(imaginer, out var hasFriend))
+        {
+            _popup.PopupClient(Loc.GetString("rmc-mentor-imaginary-friend-already-has-friend"), newFriend, PopupType.SmallCaution);
             return;
+        }
 
         var targetIsXeno = HasComp<XenoComponent>(imaginer);
-        var coordinates = _transform.GetMoverCoordinates(imaginer);
+        var coordinates = _transform.GetMoverCoordinates(newFriend);
         var prototype = targetIsXeno ? XenoImaginaryFriendPrototype : ImaginaryFriendPrototype;
 
         var friend = EntityManager.SpawnEntity(prototype, coordinates);
         _transform.AttachToGridOrMap(friend, Transform(friend));
+
+        if (!targetIsXeno && TryComp(newFriend, out ActorComponent? actor))
+        {
+            var characters = _preferencesManager.GetPreferences(actor.PlayerSession.UserId).Characters;
+            foreach (var (_, profile) in characters)
+            {
+                if (profile is not HumanoidCharacterProfile humanoid)
+                    continue;
+
+                var jobs = humanoid.JobPriorities;
+                var highJob = jobs.FirstOrDefault(x => x.Value == JobPriority.High).Key;
+
+                if (highJob != ImaginaryFriendJobPrototype)
+                    continue;
+
+                if (TryComp(friend, out HumanoidAppearanceComponent? humanoidAppearance))
+                {
+                    humanoidAppearance.Species = humanoid.Species;
+                    humanoidAppearance.Sex = humanoid.Sex;
+                    humanoidAppearance.Age = humanoid.Age;
+                    humanoidAppearance.Gender = humanoid.Gender;
+                    Dirty(friend, humanoidAppearance);
+                }
+
+                _humanoid.LoadProfile(friend, humanoid);
+                _metaData.SetEntityName(friend, humanoid.Name);
+
+                if (_prototypeManager.TryIndex(highJob, out var jobProto))
+                {
+                    var jobLoadoutId = LoadoutSystem.GetJobPrototype(jobProto.ID);
+
+                    if (_prototypeManager.TryIndex(jobLoadoutId, out RoleLoadoutPrototype? roleProto))
+                    {
+                        humanoid.Loadouts.TryGetValue(jobLoadoutId, out var loadout);
+
+                        if (loadout == null)
+                        {
+                            loadout = new RoleLoadout(jobLoadoutId);
+                            loadout.SetDefault(humanoid, null, _prototypeManager);
+                        }
+
+                        _stationSpawning.EquipRoleLoadout(friend, loadout, roleProto);
+                    }
+
+                    if (jobProto.StartingGear != null)
+                    {
+                        var startingGear = _prototypeManager.Index<StartingGearPrototype>(jobProto.StartingGear);
+                        _stationSpawning.EquipStartingGear(friend, startingGear, raiseEvent: false);
+                    }
+
+                    var ev = new StartingGearEquippedEvent(friend);
+                    RaiseLocalEvent(friend, ref ev);
+                }
+                break;
+            }
+        }
+        else
+        {
+            var startingGear = _prototypeManager.Index(XenoImaginaryFriendGear);
+            _stationSpawning.EquipStartingGear(friend, startingGear, raiseEvent: false);
+        }
 
         _mind.UnVisit(mindId);
         _mind.Visit(mindId, friend, friendMind);
--- Resources/Locale/en-US/_RMC14/mentor-verbs.ftl
+++ Resources/Locale/en-US/_RMC14/mentor-verbs.ftl
@@ -1,1 +1,2 @@
 rmc-mentor-imaginary-friend-verb = Become imaginary friend
+rmc-mentor-imaginary-friend-already-has-friend = This entity already has an imaginary friend.
