4e7ef14ffc3848bfc8b395837084b0f455315112
GrigoriyGalintano <code.grig@mail.ru>
2026-04-23 23:47:04 +0300

    fix

--- Content.Shared/_RMC14/ERT/RMCERTVisuals.cs
+++ Content.Shared/_RMC14/ERT/RMCERTVisuals.cs
@@ -1,8 +1,11 @@
+using Robust.Shared.Serialization;
+
 namespace Content.Shared._RMC14.ERT;
 
 /// <summary>
 /// Appearance keys used by distress beacons to reflect their armed/sent state.
 /// </summary>
+[Serializable, NetSerializable]
 public enum RMCERTDistressBeaconVisuals : byte
 {
     Active,
