d92c066fde654ca77bec100be27b88927386bbe2
Arendian <137322659+Arendian@users.noreply.github.com>
2026-05-11 02:48:43 +0200

    Standards

--- Content.Client/_RMC14/Light/RMCTemporaryDisabledLightComponent.cs
+++ Content.Client/_RMC14/Light/RMCTemporaryDisabledLightComponent.cs
@@ -1,6 +1,7 @@
 namespace Content.Client._RMC14.Light;
 
 [RegisterComponent]
+[Access(typeof(RMCTemporaryDisabledLightSystem))]
 public sealed partial class RMCTemporaryDisabledLightComponent : Component
 {
     [DataField]
