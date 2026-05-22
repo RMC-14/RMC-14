bfc262034d4be1c7adc635e6fc0297a83da11610
Matt Idzik <matt.idzik1@gmail.com>
2026-05-22 00:08:05 -0500

    XenoZoom now increases PVS range too

--- Content.Shared/_RMC14/Xenonids/Zoom/XenoZoomSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Zoom/XenoZoomSystem.cs
@@ -1,7 +1,6 @@
-﻿using System.Numerics;
+using System.Numerics;
 using Content.Shared._RMC14.Actions;
 using Content.Shared._RMC14.Xenonids.Leap;
-using Content.Shared._RMC14.Xenonids.Parasite;
 using Content.Shared._RMC14.Xenonids.Rest;
 using Content.Shared.Actions;
 using Content.Shared.Camera;
@@ -20,12 +19,25 @@ public sealed class XenoZoomSystem : EntitySystem
 
     public override void Initialize()
     {
+        SubscribeLocalEvent<XenoZoomComponent, ComponentStartup>(OnXenoZoomComponentStartup);
+        SubscribeLocalEvent<XenoZoomComponent, ComponentShutdown>(OnXenoZoomComponentShutdown);
         SubscribeLocalEvent<XenoZoomComponent, XenoZoomActionEvent>(OnXenoZoomAction);
         SubscribeLocalEvent<XenoZoomComponent, XenoZoomDoAfterEvent>(OnXenoZoomDoAfter);
         SubscribeLocalEvent<XenoZoomComponent, GetEyeOffsetEvent>(OnXenoZoomGetEyeOffset);
+        SubscribeLocalEvent<XenoZoomComponent, GetEyePvsScaleEvent>(OnXenoZoomGetEyePvsScale);
         SubscribeLocalEvent<XenoZoomComponent, RefreshMovementSpeedModifiersEvent>(OnXenoZoomRefreshSpeed);
         SubscribeLocalEvent<XenoZoomComponent, XenoLeapAttemptEvent>(OnLeapAttempt);
         SubscribeLocalEvent<XenoZoomComponent, XenoRestEvent>(OnRest);
+    }
+
+    private void OnXenoZoomComponentStartup(Entity<XenoZoomComponent> xeno, ref ComponentStartup args)
+    {
+        _contentEye.UpdatePvsScale(xeno);
+    }
+
+    private void OnXenoZoomComponentShutdown(Entity<XenoZoomComponent> xeno, ref ComponentShutdown args)
+    {
+        _contentEye.UpdatePvsScale(xeno);
     }
 
     private void OnXenoZoomAction(Entity<XenoZoomComponent> xeno, ref XenoZoomActionEvent args)
@@ -53,7 +65,7 @@ public sealed class XenoZoomSystem : EntitySystem
         }
         else
         {
-            _contentEye.ResetZoom(xeno);
+            _contentEye.SetZoom(xeno, SharedContentEyeSystem.DefaultZoom);
             xeno.Comp.Offset = Vector2.Zero;
         }
 
@@ -72,7 +84,18 @@ public sealed class XenoZoomSystem : EntitySystem
 
     private void OnXenoZoomGetEyeOffset(Entity<XenoZoomComponent> ent, ref GetEyeOffsetEvent args)
     {
-        args.Offset += ent.Comp.Offset;
+        if (ent.Comp.Running)
+            args.Offset += ent.Comp.Offset;
+    }
+
+    private void OnXenoZoomGetEyePvsScale(Entity<XenoZoomComponent> ent, ref GetEyePvsScaleEvent args)
+    {
+        // Scale is additive, i.e. adding .2 would increase PVS by 20%, so we subtract 1 from the zoom levels first.
+        // TODO RMC14 For now we divide by 2 in order to reduce how much "extra" overhead PVS we have outside of viewport range,
+        // because scale also scales up the default overhead amount, and we don't actually want the overhead scaled.
+        // Dividing by 2 only works for lower zoom levels (under 2). If we get higher zoom levels, come back to this and fix it.
+        if (ent.Comp.Running)
+            args.Scale += Math.Max(0, Math.Max(ent.Comp.Zoom.X - 1, ent.Comp.Zoom.Y - 1)) / 2;
     }
 
     private void OnXenoZoomRefreshSpeed(Entity<XenoZoomComponent> ent, ref RefreshMovementSpeedModifiersEvent args)
