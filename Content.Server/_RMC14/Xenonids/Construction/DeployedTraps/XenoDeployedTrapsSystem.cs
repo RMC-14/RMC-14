0f0a81592f4d520edfb25f0ec80b9ef9bab89e63
MarshesAndMeadows <khin58394@edu.ucl.dk>
2026-06-03 11:38:29 +0200

    trap prediction actually working now, thanks smug, midzik.

--- Content.Server/_RMC14/Xenonids/Construction/DeployedTraps/XenoDeployedTrapsSystem.cs
+++ Content.Server/_RMC14/Xenonids/Construction/DeployedTraps/XenoDeployedTrapsSystem.cs
@@ -1,16 +1,7 @@
 ﻿using Content.Server.Destructible;
-using Content.Shared._RMC14.Marines;
-using Content.Shared._RMC14.Slow;
-using Content.Shared._RMC14.Xenonids;
 using Content.Shared._RMC14.Xenonids.Construction.DeployedTraps;
 using Content.Shared._RMC14.Xenonids.Hive;
 using Content.Shared.Damage;
-using Content.Shared.Mobs.Systems;
-using Content.Shared.StepTrigger.Systems;
-using Content.Shared.Stunnable;
-using Robust.Shared.Audio.Systems;
-using Robust.Shared.Network;
-using Robust.Shared.Timing;
 
 namespace Content.Server._RMC14.Xenonids.Construction.DeployedTraps;
 
@@ -18,12 +9,10 @@ public sealed class XenoDeployedTrapsSystem : EntitySystem
 {
     [Dependency] private readonly DestructibleSystem _destructible = default!;
     [Dependency] private readonly SharedXenoHiveSystem _hive = default!;
-    [Dependency] private readonly MobStateSystem _mobState = default!;
 
     public override void Initialize()
     {
         SubscribeLocalEvent<XenoDeployedTrapsComponent, DamageChangedEvent>(OnTrapDamaged);
-        SubscribeLocalEvent<XenoDeployedTrapsComponent, StepTriggerAttemptEvent>(OnStepTriggerAttempt);
     }
 
     private void OnTrapDamaged(Entity<XenoDeployedTrapsComponent> trap, ref DamageChangedEvent args)
@@ -39,12 +28,5 @@ public sealed class XenoDeployedTrapsSystem : EntitySystem
 
         if (args.Damageable.TotalDamage + args.DamageDelta.GetTotal() > totalHealth)
             QueueDel(trap);
-    }
-
-    private void OnStepTriggerAttempt(Entity<XenoDeployedTrapsComponent> trap, ref StepTriggerAttemptEvent args)
-    {
-        args.Continue = !_hive.FromSameHive(args.Tripper, trap.Owner)
-                        && !_mobState.IsDead(args.Tripper)
-                        && (HasComp<XenoComponent>(args.Tripper) || HasComp<MarineComponent>(args.Tripper));
     }
 }
--- Content.Shared/_RMC14/Xenonids/Construction/DeployedTraps/SharedDeployedTrapsSystem.cs
+++ Content.Shared/_RMC14/Xenonids/Construction/DeployedTraps/SharedDeployedTrapsSystem.cs
@@ -1,7 +1,12 @@
-﻿using Content.Shared._RMC14.Slow;
+﻿using Content.Shared._RMC14.Marines;
+using Content.Shared._RMC14.Slow;
 using Content.Shared._RMC14.Xenonids.Construction.DeployedTraps;
+using Content.Shared._RMC14.Xenonids.Hive;
+using Content.Shared.Fluids;
+using Content.Shared.Mobs.Systems;
 using Content.Shared.StepTrigger.Systems;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Network;
 using Robust.Shared.Timing;
 
 namespace Content.Shared._RMC14.Xenonids.Construction.DeployedTraps;
@@ -11,16 +16,22 @@ public sealed class SharedDeployedTrapsSystem : EntitySystem
     [Dependency] private readonly SharedAudioSystem _audio = default!;
     [Dependency] private readonly RMCSlowSystem _slow = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly SharedXenoHiveSystem _hive = default!;
+    [Dependency] private readonly MobStateSystem _mobState = default!;
+    [Dependency] private readonly INetManager _net = default!;
 
     public override void Initialize()
     {
         SubscribeLocalEvent<XenoDeployedTrapsComponent, StepTriggeredOnEvent>(OnStepTriggered);
+        SubscribeLocalEvent<XenoDeployedTrapsComponent, StepTriggerAttemptEvent>(OnStepTriggerAttempt);
     }
 
     private void OnStepTriggered(Entity<XenoDeployedTrapsComponent> trap, ref StepTriggeredOnEvent args)
     {
         _slow.TryRoot(args.Tripper, trap.Comp.StunDuration, true);
-        _audio.PlayPredicted(trap.Comp.CatchSound, args.Tripper, trap);
+
+        if(!_net.IsClient)
+            _audio.PlayPredicted(trap.Comp.CatchSound, args.Tripper, trap);
 
         var caught = EnsureComp<XenoCaughtInTrapComponent>(args.Tripper);
         caught.ExpireTime = _timing.CurTime + trap.Comp.StunDuration;
@@ -37,5 +48,12 @@ public sealed class SharedDeployedTrapsSystem : EntitySystem
             if (_timing.CurTime >= caught.ExpireTime)
                 RemCompDeferred<XenoCaughtInTrapComponent>(uid);
         }
+    }
+
+    private void OnStepTriggerAttempt(Entity<XenoDeployedTrapsComponent> trap, ref StepTriggerAttemptEvent args)
+    {
+        args.Continue = !_hive.FromSameHive(args.Tripper, trap.Owner)
+                        && !_mobState.IsDead(args.Tripper)
+                        && (HasComp<XenoComponent>(args.Tripper) || HasComp<MarineComponent>(args.Tripper));
     }
 }
