52e1347e85636174b18ac7eadb503f65fb79e081
GrigoriyGalintano <156821788+GrigoriyGalintano@users.noreply.github.com>
2026-06-07 04:58:28 +0300

    Universal recorder (#9716)
    Co-authored-by: DrSmugleaf <10968691+DrSmugleaf@users.noreply.github.com>

---
+++ Content.Shared/_RMC14/UniversalRecorder/RecorderEntry.cs
@@ -0,0 +1,11 @@
+namespace Content.Shared._RMC14.UniversalRecorder;
+
+public readonly record struct RecorderEntry(
+    TimeSpan Timestamp,
+    string SpeakerName,
+    string SpeechVerb,
+    string Text,
+    string FontId,
+    int FontSize,
+    bool Bold
+);
