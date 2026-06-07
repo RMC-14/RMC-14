8d18c9e9fdf3c181eb246ea53c72cb146e1dd3b2
ExponentialWizard <camrinelliott@gmail.com>
2026-06-06 21:01:29 -0700

    rename Client RMCDefibrillatorSystem

--- Content.Client/_RMC14/Medical/Defibrillator/RMCDefibrillatorSystem.cs
+++ Content.Client/_RMC14/Medical/Defibrillator/RMCDefibrillatorSystem.cs
@@ -2,6 +2,6 @@ using Content.Shared._RMC14.Medical.Defibrillator;
 
 namespace Content.Client._RMC14.Medical.Defibrillator;
 
-public sealed class ClientRMCDefibrillatorSystem : SharedRMCDefibrillatorSystem
+public sealed class RMCDefibrillatorSystem : SharedRMCDefibrillatorSystem
 {
 }
