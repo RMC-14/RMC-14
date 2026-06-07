91b4880a7f79acd602f5bc6baad8a6c5b24add09
Vixting <vixting@gmail.com>
2026-06-07 09:58:34 +0100

    fix

--- Content.Shared/_RMC14/Weapons/Ranged/Prediction/SharedGunPredictionSystem.cs
+++ Content.Shared/_RMC14/Weapons/Ranged/Prediction/SharedGunPredictionSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Content.Shared._RMC14.CCVar;
 using Content.Shared.CombatMode;
+using Content.Shared.Weapons.Ranged.Components;
 using Content.Shared.Weapons.Ranged.Systems;
 using Robust.Shared.Configuration;
 using Robust.Shared.Map;
@@ -20,7 +21,7 @@ public abstract class SharedGunPredictionSystem : EntitySystem
         Subs.CVar(_config, RMCCVars.RMCGunPrediction, v => GunPrediction = v, true);
     }
 
-    public List<EntityUid>? ShootRequested(NetEntity netGun, NetCoordinates coordinates, NetEntity? target, List<int>? projectiles, ICommonSession session, bool continuous = false)
+    public List<EntityUid>? ShootRequested(NetEntity netGun, NetCoordinates coordinates, NetEntity? target, List<int>? projectiles, ICommonSession session, bool rearmSemiAuto = false)
     {
         var user = session.AttachedEntity;
 
@@ -38,8 +39,15 @@ public abstract class SharedGunPredictionSystem : EntitySystem
         gun.ShootCoordinates = GetCoordinates(coordinates);
         gun.Target = GetEntity(target);
 #pragma warning restore RA0002
-        if (continuous && !HasComp<GunClickToFireComponent>(ent))
+        var shouldRearmSemiAuto =
+            rearmSemiAuto &&
+            gun.SelectedMode == SelectiveFire.SemiAuto &&
+            !HasComp<GunClickToFireComponent>(ent);
+
+        if (shouldRearmSemiAuto)
             _gun.ResetShotCounter(ent, gun);
-        return _gun.AttemptShoot(user.Value, ent, gun, projectiles, session);
+
+        // Dont snapp the cooldown forward to the current frame on every generated rearm
+        return _gun.AttemptShoot(user.Value, ent, gun, projectiles, session, shouldRearmSemiAuto);
     }
 }
