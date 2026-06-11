a17566bf94bc58d109634ac7baf505c85dcd128a
Arykh <181794112+Arykh@users.noreply.github.com>
2026-06-11 18:33:47 -0400

    Fix getting stuck when exiting a Sleeper, Autodoc, or Body Scanner. (#10072)

--- Content.Shared/_RMC14/Movement/RMCOutsideChamberComponent.cs
+++ Content.Shared/_RMC14/Movement/RMCOutsideChamberComponent.cs
@@ -1,10 +1,10 @@
 using Robust.Shared.GameStates;
 
-namespace Content.Shared._RMC14.Medical.BodyScanner;
+namespace Content.Shared._RMC14.Movement;
 
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-[Access(typeof(SharedBodyScannerSystem))]
-public sealed partial class InsideBodyScannerComponent : Component
+[Access(typeof(RMCMovementSystem))]
+public sealed partial class RMCOutsideChamberComponent : Component
 {
     [DataField, AutoNetworkedField]
     public EntityUid? Chamber;
