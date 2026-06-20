b04c94817649411e3732e85b3cc5b81fcf872d23
Vixting <vixting@gmail.com>
2026-06-21 00:00:44 +0100

    foix

--- Content.Shared/_RMC14/Vehicle/Grid/GridVehicleMoveSystem.Collision.cs
+++ Content.Shared/_RMC14/Vehicle/Grid/GridVehicleMoveSystem.Collision.cs
@@ -129,6 +129,9 @@ public sealed partial class GridVehicleMoverSystem : EntitySystem
 
             if (candidate.CollisionClass == VehicleCollisionClass.SoftMob && candidate.IsXeno)
             {
+                if (candidate.MobState != null && _mobState.IsDead(candidate.Entity, candidate.MobState))
+                    continue;
+
                 var result = HandleSoftXenoCollision(
                     uid,
                     mover,
