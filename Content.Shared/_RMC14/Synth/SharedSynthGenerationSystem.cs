82ebf11371744ad840b5e5a5dd4c537007bec142
Aurallianz <74161272+Aurallianz@users.noreply.github.com>
2026-07-03 13:15:32 -0500

    Update SharedSynthGenerationSystem.cs
    Co-authored-by: Thereisabear <191423197+Thereisabear@users.noreply.github.com>

--- Content.Shared/_RMC14/Synth/SharedSynthGenerationSystem.cs
+++ Content.Shared/_RMC14/Synth/SharedSynthGenerationSystem.cs
@@ -15,7 +15,6 @@ namespace Content.Shared._RMC14.Synth;
 
 public sealed class SharedSynthGenerationSystem : EntitySystem
 {
-    private static readonly EntProtoId<SynthGenerationComponent> DefaultGen= "RMCSynthGenTwo";
 
     [Dependency] private readonly IPrototypeManager _prototype = default!;
     [Dependency] private readonly SharedActionsSystem _actions = default!;
