1acf55369166822c66cf7f694293405557d47be7
Arendian <137322659+Arendian@users.noreply.github.com>
2026-07-06 06:33:48 +0200

    Fix ignored prototypes not being ignored

--- Content.Packaging/ClientPackaging.cs
+++ Content.Packaging/ClientPackaging.cs
@@ -1,15 +1,26 @@
 ﻿using System.Diagnostics;
 using System.IO.Compression;
+using Content.Packaging._RMC14;
 using Robust.Packaging;
 using Robust.Packaging.AssetProcessing;
 using Robust.Packaging.AssetProcessing.Passes;
 using Robust.Packaging.Utility;
+using Robust.Shared.Serialization.Markdown;
+using Robust.Shared.Serialization.Markdown.Sequence;
+using Robust.Shared.Serialization.Markdown.Value;
 using Robust.Shared.Timing;
+using Robust.Shared.Utility;
 
 namespace Content.Packaging;
 
 public static class ClientPackaging
 {
+    //RMC14
+    private const string ResourcesDirectory = "Resources";
+    private const string IgnoredPrototypesDirectory = "IgnoredPrototypes";
+    private const string YamlSearchPattern = "*.yml";
+    //RMC14
+
     /// <summary>
     /// Be advised this can be called from server packaging during a HybridACZ build.
     /// </summary>
@@ -61,13 +72,27 @@ public static class ClientPackaging
         var graph = new RobustClientAssetGraph();
         pass.Dependencies.Add(new AssetPassDependency(graph.Output.Name));
 
+        //RMC14
+        var abstractIgnoredPrototypesPass = new AssetPassAbstractIgnoredPrototypes(
+            ReadIgnoredPrototypePaths(contentDir))
+        {
+            Name = "AbstractIgnoredPrototypesPass",
+        };
+
+        abstractIgnoredPrototypesPass
+            .AddDependency(graph.Input)
+            .AddBefore(graph.PresetPasses);
+
+        graph.PresetPasses.AddDependency(abstractIgnoredPrototypesPass);
+        //RMC14
+
         var dropSvgPass = new AssetPassFilterDrop(f => f.Path.EndsWith(".svg"))
         {
             Name = "DropSvgPass",
         };
         dropSvgPass.AddDependency(graph.Input).AddBefore(graph.PresetPasses);
 
-        AssetGraph.CalculateGraph([pass, dropSvgPass, ..graph.AllPasses], logger);
+        AssetGraph.CalculateGraph([pass, abstractIgnoredPrototypesPass, dropSvgPass, ..graph.AllPasses], logger);
 
         var inputPass = graph.Input;
 
@@ -82,4 +107,46 @@ public static class ClientPackaging
 
         inputPass.InjectFinished();
     }
+
+    //RMC14
+    private static HashSet<string> ReadIgnoredPrototypePaths(string contentDir)
+    {
+        var ignored = new HashSet<string>(StringComparer.Ordinal);
+        var ignoredDir = Path.Combine(contentDir, ResourcesDirectory, IgnoredPrototypesDirectory);
+
+        if (!Directory.Exists(ignoredDir))
+            return ignored;
+
+        foreach (var file in Directory.EnumerateFiles(ignoredDir, YamlSearchPattern))
+        {
+            using var reader = new StreamReader(file, EncodingHelpers.UTF8);
+
+            foreach (var document in DataNodeParser.ParseYamlStream(reader))
+            {
+                if (document.Root is not SequenceDataNode sequence)
+                    continue;
+
+                foreach (var node in sequence.Sequence)
+                {
+                    if (node is not ValueDataNode value)
+                        continue;
+
+                    var path = NormalizeResourcePath(value.Value);
+
+                    if (path.Length != 0)
+                        ignored.Add(path);
+                }
+            }
+        }
+
+        return ignored;
+    }
+
+    private static string NormalizeResourcePath(string path)
+    {
+        return path
+            .Replace('\\', '/')
+            .TrimStart('/');
+    }
+    //RMC14
 }
