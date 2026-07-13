b332c778946fac4d58e5822b796f8284876d7c2e
Thereisabear <191423197+Thereisabear@users.noreply.github.com>
2026-07-13 16:51:44 +0800

    Refactored to use Prototype

--- Content.Shared/_RMC14/Actions/SwappableActionComponent.cs
+++ Content.Shared/_RMC14/Actions/SwappableActionComponent.cs
@@ -1,6 +1,5 @@
-using Content.Shared.Actions;
-using Content.Shared.FixedPoint;
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._RMC14.Actions;
 
@@ -8,21 +7,12 @@ namespace Content.Shared._RMC14.Actions;
 [Access(typeof(SwappableActionSystem))]
 public sealed partial class SwappableActionComponent : Component
 {
+    /// <summary>
+    ///     The action prototype swapped to.
+    /// </summary>
     [DataField, AutoNetworkedField]
-    public string OriginalName = string.Empty;
-
-    [DataField, AutoNetworkedField]
-    public string OriginalDescription = string.Empty;
-
-    [DataField, AutoNetworkedField]
-    public TimeSpan? OriginalUseDelay;
+    public EntProtoId? SwappedActionProto;
 
     [DataField, AutoNetworkedField]
     public bool IsSwapped;
-
-    [DataField, NonSerialized]
-    public BaseActionEvent? SwappedEventTemplate;
-
-    [DataField, NonSerialized]
-    public BaseActionEvent? OriginalEventTemplate;
 }
