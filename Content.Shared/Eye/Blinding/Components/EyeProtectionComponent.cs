a33938cd9336ce040e6aa7995fb9613a37321aad
wheelwrightt <174839295+wheelwrightt@users.noreply.github.com>
2026-07-22 23:55:55 +0200

    goggle

--- Content.Shared/Eye/Blinding/Components/EyeProtectionComponent.cs
+++ Content.Shared/Eye/Blinding/Components/EyeProtectionComponent.cs
@@ -1,15 +1,17 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared.Eye.Blinding.Components;
 
 /// <summary>
 /// For welding masks, sunglasses, etc.
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class EyeProtectionComponent : Component
 {
     /// <summary>
     /// How many seconds to subtract from the status effect. If it's greater than the source
     /// of blindness, do not blind.
     /// </summary>
-    [DataField("protectionTime")]
+    [DataField("protectionTime"), AutoNetworkedField]
     public TimeSpan ProtectionTime = TimeSpan.FromSeconds(10);
 }
