b2539698b3954950ad568ee596e8ba05431111a6
NET <roteadordanet321@gmail.com>
2026-07-30 04:41:26 -0300

    I swear i know how to use github :godo:

--- Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
+++ Content.Shared/Weapons/Melee/SharedMeleeWeaponSystem.cs
@@ -985,7 +985,7 @@ public abstract class SharedMeleeWeaponSystem : EntitySystem
                 return false;
             }
         }
-
+        // RMC14 range disarm distance
         if (!InRange(user, target.Value, _rmcMelee.GetUserDisarmRange(user, target, component), session))
         {
             return false;
