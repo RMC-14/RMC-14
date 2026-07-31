a8bba894c4267117848f07f9c1a6470dac6fb6d0
Thereisabear <191423197+Thereisabear@users.noreply.github.com>
2026-07-31 20:27:30 +0800

    Generation confirmation
    also the ability to specify what generations a synth can choose.
    If not specified defaults to gen 1, 2, 3

--- Content.Shared/_RMC14/Synth/SynthGenerationComponent.cs
+++ Content.Shared/_RMC14/Synth/SynthGenerationComponent.cs
@@ -24,4 +24,15 @@ public sealed partial class SynthGenerationComponent : Component
 
     [DataField]
     public int Priority;
+
+    /// <summary>
+    /// Sets choices, else it defaults to Gens 1,2,3
+    /// </summary>
+    [DataField]
+    public List<EntProtoId<SynthGenerationComponent>> AvailableGenerations = new()
+    {
+        "RMCSynthGenOne",
+        "RMCSynthGenTwo",
+        "RMCSynthGenThree"
+    };
 }
