ceac719d41dff9aced57c3964dc7246015d55068
RerBreab <kostayp@mail.ru>
2026-08-02 22:09:59 +0300

    Fix projectiles, praying to god it didnt break something

--- Content.Shared/Storage/Components/SharedEntityStorageComponent.cs
+++ Content.Shared/Storage/Components/SharedEntityStorageComponent.cs
@@ -22,7 +22,8 @@ public abstract partial class SharedEntityStorageComponent : Component
     public readonly int MasksToRemove = (int) (
         CollisionGroup.MidImpassable |
         CollisionGroup.HighImpassable |
-        CollisionGroup.LowImpassable);
+        CollisionGroup.LowImpassable |
+        CollisionGroup.BulletImpassable);
 
     /// <summary>
     ///     Collision masks that were removed from ANY layer when the storage was opened;
