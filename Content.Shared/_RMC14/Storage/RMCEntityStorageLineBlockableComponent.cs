c3aa420a68ae3261af54eed55e11d97c0c0f4afb
RerBreab <kostayp@mail.ru>
2026-08-03 18:36:14 +0300

    Fix tilesystem (opressor pull, flamethrower and etc)

---
+++ Content.Shared/_RMC14/Storage/RMCEntityStorageLineBlockableComponent.cs
@@ -0,0 +1,8 @@
+using Content.Shared._RMC14.Line;
+using Content.Shared.Whitelist;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._RMC14.Storage;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class RMCEntityStorageLineBlockableComponent : Component;
