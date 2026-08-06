0eed6d1f88c73a6a2b47b922a3109b60fac181e7
SolsticeOfTheWinter <solsticeofthewinter@gmail.com>
2026-08-05 14:36:32 -1000

    Initial

--- Content.Server/Nutrition/EntitySystems/SmokingSystem.cs
+++ Content.Server/Nutrition/EntitySystems/SmokingSystem.cs
@@ -49,6 +49,7 @@ namespace Content.Server.Nutrition.EntitySystems
             InitializeCigars();
             InitializePipes();
             InitializeVapes();
+            InitializeRMC(); // RMC14
         }
 
         private void OnExtinguishEvent(Entity<SmokableComponent> ent, ref ExtinguishEvent args)
