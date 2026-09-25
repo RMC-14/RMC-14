882ca09e3b0137736be6bb020ae02f1a855df599
FreeSpy443 <33325025+FreeSpy443@users.noreply.github.com>
2026-09-25 16:33:04 +0300

    Pushing Altair's Changes
    Adds petsystems and all relevant things for it.

--- Content.Shared.Database/LogType.cs
+++ Content.Shared.Database/LogType.cs
@@ -526,4 +526,5 @@ public enum LogType
     RMCMedalRecommendation = RMCMarineAnnounce + 44,
     RMCAutodocSurgeryAbort = RMCMarineAnnounce + 45,
     RMCXenoPsychic = RMCMarineAnnounce + 46,
+    RMCPetNamed = RMCMarineAnnounce + 48,
 }
